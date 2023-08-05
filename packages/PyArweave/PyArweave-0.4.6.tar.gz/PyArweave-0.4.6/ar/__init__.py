d8122e6fe4ca714baa27152381ed6b23da801b75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:17:24 +0000

    Add package PyArweave/0.4.6/PyArweave-0.4.6.tar.gz

---
+++ packages/PyArweave/PyArweave-0.4.6.tar.gz/PyArweave-0.4.6/ar/__init__.py
@@ -0,0 +1,33 @@
+# This file is part of PyArweave.
+# 
+# PyArweave is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 2 of the License, or (at your option) any later
+# version.
+# 
+# PyArweave is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with
+# PyArweave. If not, see <https://www.gnu.org/licenses/>.
+
+import logging
+
+DEFAULT_API_URL = 'https://arweave.net'
+
+logger = logging.getLogger(__name__)
+
+class ArweaveException(Exception):
+    pass
+
+__all__ = ['Peer', 'Wallet', 'Transaction', 'arql', 'ArweaveException', 'logger', 'ArweaveTransactionException']
+
+ArweaveTransactionException = ArweaveException
+
+from .peer import Peer
+from .wallet import Wallet
+from .transaction import Transaction
+from .arweave_lib import arql
+from .bundle import Bundle, DataItem, ANS104BundleHeader, ANS104DataItemHeader
+
