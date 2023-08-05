552f6d39e290abe495b007be782124c50fdfa07a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:49 +0000

    Add package pandas-tutor/2.0.0.dev0/pandas_tutor-2.0.0.dev0.tar.gz

---
+++ packages/pandas-tutor/pandas_tutor-2.0.0.dev0.tar.gz/pandas_tutor-2.0.0.dev0/setup.py
@@ -0,0 +1,32 @@
+"""
+put an empty __init__.py inside of every directory you want to include in the
+wheel
+
+to build a wheel, run this command, (important: make sure to rm -rf the build
+cache):
+
+rm -rf build/; python setup.py bdist_wheel
+"""
+# type: ignore
+
+from setuptools import setup, find_packages
+
+version = "2.0.0.dev0"
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# note that we don't put requirements.txt into the install_requires list
+# since for the website we'll bundle the dependencies directly into the wheel.
+# other environments will have to run pip install -r requirements.txt manually.
+
+# maybe helpful? https://setuptools.pypa.io/en/latest/userguide/datafiles.html
+setup(
+    name="pandas_tutor",
+    version=version,
+    packages=find_packages(),
+    package_data={
+        "": ["*.golden"]
+    },  # add all test .golden files into package along with .py files
+    include_package_data=True,
+)
