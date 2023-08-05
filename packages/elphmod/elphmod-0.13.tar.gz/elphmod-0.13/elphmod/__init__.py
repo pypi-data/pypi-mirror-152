99cc66fcf95375815456526a360358447032ef7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:57 +0000

    Add package elphmod/0.13/elphmod-0.13.tar.gz

---
+++ packages/elphmod/elphmod-0.13.tar.gz/elphmod-0.13/elphmod/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2017-2022 elphmod Developers
+# This program is free software under the terms of the GNU GPLv3 or later.
+
+"""Load all modules at package import."""
+
+__version__ = '0.13'
+
+from . import MPI
+from . import bravais
+from . import diagrams
+from . import dispersion
+from . import dos
+from . import el
+from . import elel
+from . import eliashberg
+from . import elph
+from . import misc
+from . import occupations
+from . import ph
+from . import plot
