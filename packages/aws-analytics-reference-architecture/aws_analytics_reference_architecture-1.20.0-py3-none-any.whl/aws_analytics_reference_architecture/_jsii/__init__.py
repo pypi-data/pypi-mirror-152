c12015da944b5f955c8df7ce8124125f9433972b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:02 +0000

    Add package aws-analytics-reference-architecture/1.20.0/aws_analytics_reference_architecture-1.20.0-py3-none-any.whl

---
+++ packages/aws-analytics-reference-architecture/aws_analytics_reference_architecture-1.20.0-py3-none-any.whl/aws_analytics_reference_architecture/_jsii/__init__.py
@@ -0,0 +1,53 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.assertions._jsii
+import aws_cdk.aws_athena._jsii
+import aws_cdk.aws_autoscaling._jsii
+import aws_cdk.aws_ec2._jsii
+import aws_cdk.aws_eks._jsii
+import aws_cdk.aws_emr._jsii
+import aws_cdk.aws_emrcontainers._jsii
+import aws_cdk.aws_events._jsii
+import aws_cdk.aws_events_targets._jsii
+import aws_cdk.aws_glue._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_kinesis._jsii
+import aws_cdk.aws_kinesisfirehose._jsii
+import aws_cdk.aws_kinesisfirehose_destinations._jsii
+import aws_cdk.aws_kms._jsii
+import aws_cdk.aws_lakeformation._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_lambda_python._jsii
+import aws_cdk.aws_logs._jsii
+import aws_cdk.aws_redshift._jsii
+import aws_cdk.aws_s3._jsii
+import aws_cdk.aws_s3_assets._jsii
+import aws_cdk.aws_s3_deployment._jsii
+import aws_cdk.aws_secretsmanager._jsii
+import aws_cdk.aws_stepfunctions._jsii
+import aws_cdk.aws_stepfunctions_tasks._jsii
+import aws_cdk.core._jsii
+import aws_cdk.custom_resources._jsii
+import aws_cdk.lambda_layer_awscli._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "aws-analytics-reference-architecture",
+    "1.20.0",
+    __name__[0:-6],
+    "aws-analytics-reference-architecture@1.20.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
