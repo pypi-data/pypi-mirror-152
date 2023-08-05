1b6fa544246df36149e128c2aa93efa2461cafe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:50 +0000

    Add package spinthon/0.2/spinthon-0.2.tar.gz

---
+++ packages/spinthon/spinthon-0.2.tar.gz/spinthon-0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='spinthon',
+      version='0.2',
+      description='A python package intended to simulate spin dynamics',
+      url='http://github.com/bennomeier/spinthon',
+      author='Benno Meier',
+      author_email='meier.benno@gmail.com',
+      license='MIT',
+      packages=['spinthon'],
+      install_requires = [
+          'numpy',
+          'scipy',
+          'spindata'
+          ],
+      zip_safe=False)
