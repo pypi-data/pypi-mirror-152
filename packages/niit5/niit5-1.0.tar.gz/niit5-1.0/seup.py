38d20ec5d47e98707ac7437f06f364b3f0c288dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:03 +0000

    Add package niit5/1.0/niit5-1.0.tar.gz

---
+++ packages/niit5/niit5-1.0.tar.gz/niit5-1.0/seup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='niit5',
+    version='1.0',
+    description='5班测试包',
+    author='niit5',
+    author_email='3242421421@qq.com',
+    py_modules=['niit5.do1','niit5.do2']
+)
