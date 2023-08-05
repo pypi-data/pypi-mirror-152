40a7a85e04a5c81d40c617187225da77a93aaa88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:11 +0000

    Add package lickport-array-interface/2.0.0/lickport_array_interface-2.0.0-py2.py3-none-any.whl

---
+++ packages/lickport-array-interface/lickport_array_interface-2.0.0-py2.py3-none-any.whl/lickport_array_interface/__init__.py
@@ -0,0 +1,17 @@
+'''
+Python interface to the Janelia Dudman lab mouse lickport array.
+'''
+
+# __init__.py is generated automatically from .single-source-of-truth.org
+# File edits may be overwritten!
+from lickport_array_interface.__about__ import (
+    __author__,
+    __copyright__,
+    __email__,
+    __license__,
+    __description__,
+    __url__,
+    __version__,
+)
+
+from .lickport_array_interface import LickportArrayInterface, LickportArrayMetadata
