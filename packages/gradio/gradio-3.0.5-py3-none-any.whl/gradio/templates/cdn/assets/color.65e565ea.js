cea614e5ecc74b05e7fb884d959493fd1bd9df7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:58 +0000

    Add package gradio/3.0.5/gradio-3.0.5-py3-none-any.whl

---
+++ packages/gradio/gradio-3.0.5-py3-none-any.whl/gradio/templates/cdn/assets/color.65e565ea.js
@@ -0,0 +1,1 @@
+import{ai as o}from"./index.6de9505e.js";const t=r=>o[r%o.length];export{t as g};
---
+++ packages/gradio/gradio-3.0.5-py3-none-any.whl/gradio/templates/cdn/assets/index.fd0f8cdc.js
@@ -0,0 +1,1 @@
+import{R as s}from"./index.6de9505e.js";const o=["static"];export{s as Component,o as modes};
