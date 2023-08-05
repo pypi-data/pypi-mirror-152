29616c6681c482ca2dd59416859aae08a7237dcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:08 +0000

    Add package uiwehfbsff/0.0.1/uiwehfbsff-0.0.1.tar.gz

---
+++ packages/uiwehfbsff/uiwehfbsff-0.0.1.tar.gz/uiwehfbsff-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='uiwehfbsff',
+  packages=['uiwehfbsff'],
+  version='0.0.1',
+  description='A very basic calculator',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Cookies',
+  author_email='Cookies@mail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='calculator', 
+  install_requires=[''] 
+)
