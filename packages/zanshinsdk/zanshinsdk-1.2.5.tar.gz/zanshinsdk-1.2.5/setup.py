ea41df1f7ca67af4dc0b5d6d217f889b27de6819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:33 +0000

    Add package zanshinsdk/1.2.5/zanshinsdk-1.2.5.tar.gz

---
+++ packages/zanshinsdk/zanshinsdk-1.2.5.tar.gz/zanshinsdk-1.2.5/setup.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+from os.path import join
+
+from setuptools import setup
+
+exec(open(join('zanshinsdk','version.py')).read())
+
+extras = {
+    'with_boto3': ['boto3>=1.21.21']
+}
+
+setup(
+    name='zanshinsdk',
+    description='Python SDK to access the Tenchi Security Zanshin API v1',
+    long_description=Path('README.rst').read_text(),
+    author='Tenchi Security',
+    author_email='contact@tenchisecurity.com',
+    version=__version__,
+    url='https://github.com/tenchi-security/zanshin-sdk-python',
+    license='Apache Software License',
+    install_requires=['httpx==0.23.0'],
+    tests_require=['pytest==7.1.1', 'moto[all]==3.1.1'],
+    setup_requires=['pytest-runner==6.0.0'],
+    packages=['zanshinsdk'],
+    extras_require=extras,
+    # include_package_data=True,
+    platforms='any',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Development Status :: 5 - Production/Stable',
+        'Natural Language :: English',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Security',
+        'Topic :: Software Development :: Libraries',
+    ]
+)
+# flake8: noqa
