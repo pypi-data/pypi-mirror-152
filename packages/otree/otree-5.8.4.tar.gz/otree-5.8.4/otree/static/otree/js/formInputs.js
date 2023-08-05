77866d838ec0780c2ca0f2e52959cb615ad27ed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:33 +0000

    Add package otree/5.8.4/otree-5.8.4.tar.gz

---
+++ packages/otree/otree-5.8.4.tar.gz/otree-5.8.4/otree/static/otree/js/formInputs.js
@@ -0,0 +1,5 @@
+var formInputs = new Proxy(document.getElementById('form').elements, {
+  set: function (obj, prop, value) {
+    throw new TypeError(`To set the value of a field, you must use .value, for example, formInputs.${prop}.value = ...`);
+  }
+});
