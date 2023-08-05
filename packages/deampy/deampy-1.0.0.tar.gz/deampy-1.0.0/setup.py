29dd628f3e49edbe1f3f87cca3fa05eeee8c5668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:29 +0000

    Add package deampy/1.0.0/deampy-1.0.0.tar.gz

---
+++ packages/deampy/deampy-1.0.0.tar.gz/deampy-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='deampy',
+    version='1.0.0',
+    install_requires=['numpy', 'matplotlib', 'scipy'],
+    packages=['deampy'],
+    url='https://github.com/modeling-health-care-decisions/deampy',
+    license='MIT License',
+    author='Reza Yaesoubi',
+    author_email='reza.yaesoubi@yale.edu',
+    description='Decision analysis in medicine and public health'
+)
