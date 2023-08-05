8fb589f2b1d99c29f245f4bbbea5e658596dfe07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:21 +0000

    Add package MeUtils/2022.5.24.11.27.33/MeUtils-2022.5.24.11.27.33.tar.gz

---
+++ packages/MeUtils/MeUtils-2022.5.24.11.27.33.tar.gz/MeUtils-2022.5.24.11.27.33/meutils/str_utils/__init__.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : MeUtils.
+# @File         : __init__.py
+# @Time         : 2022/5/12 下午2:02
+# @Author       : yuanjie
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 
+
+
+from meutils.pipe import *
+from meutils.request_utils.crawler import Crawler
+
+"""todo
+url 拼接
+
+"""
+
+
+def hump_str(string="a_b", pattern='_'):
+    """驼峰式转换"""
+    reg = re.compile(pattern)
+    _ = reg.sub('', string.title())
+    return _.replace(_[0], _[0].lower())
+
+
+def str_replace(s: str, dic: dict):
+    """多值替换
+        str_replace('abcd', {'a': '8', 'd': '88'})
+    """
+    return s.translate(str.maketrans(dic))
+
+
+def unquote(s='%E6%9C%80%E6%96%B0%E6%9C%8D%E5%8A%A1'):
+    """http字符串解码"""
+    from urllib import parse
+
+    return parse.unquote(s)
+
+
+@lru_cache()
+def arabic2chinese(arabic=123):
+    c = Crawler(f'https://szjrzzwdxje.bmcx.com/{arabic}__szjrzzwdxje')
+    return c.xpath('//span//text()')[-3:-1]
+
+
+@lru_cache(1024)
+def json_loads(s):
+    try:
+        return json.loads(s.replace("'", '"'))
+    except Exception as e:
+        logger.warning(e)
+        return eval(s)
+
+
+if __name__ == '__main__':
+    print(str_replace('abcd', {'a': '8', 'd': '88'}))
+    print(unquote())
+    print(arabic2chinese())
---
+++ packages/MeUtils/MeUtils-2022.5.24.11.27.33.tar.gz/MeUtils-2022.5.24.11.27.33/pypi.sh
@@ -0,0 +1,12 @@
+##!/usr/bin/env bash
+#python setup.py sdist bdist_wheel && twine upload ./dist/*
+#
+#pip install ./dist/*.whl -U
+#rm -rf ./build ./dist ./*.egg* ./.eggs
+#exit
+
+#!/usr/bin/env bash
+python setup.py sdist bdist_wheel && twine upload ./dist/*
+pip install ./dist/*.whl -U
+rm -rf ./build/ ./dist/ ./*.egg-info/ ./.eggs/
+exit
