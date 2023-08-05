ee37c1c271437227e4b3fdb341f7c112b1e52e1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:17:07 +0000

    Add package grndvc/0.0.1/grndvc-0.0.1.tar.gz

---
+++ packages/grndvc/grndvc-0.0.1.tar.gz/grndvc-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="grndvc",
+    version="0.0.1",
+    author="dannm2",
+    author_email="mdan226@gmail.com",
+    description="Sort description",
+    long_description="Full description",
+    long_description_content_type="text/markdown",
+    # url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
