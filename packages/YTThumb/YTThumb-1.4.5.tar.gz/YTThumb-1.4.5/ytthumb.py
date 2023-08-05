69f0faf9ab08d31d273f3bb6142859329610e039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:17:20 +0000

    Add package YTThumb/1.4.5/YTThumb-1.4.5.tar.gz

---
+++ packages/YTThumb/YTThumb-1.4.5.tar.gz/YTThumb-1.4.5/ytthumb.py
@@ -0,0 +1,83 @@
+import requests
+
+
+def extract_id(video):
+    
+    # extract the id
+    if ("youtube.com" in video) and ("/" in video) and ("=" in video):
+        id = video.split("=")[-1]
+    elif ("youtu.be" in video) and ("/" in video):
+        id = video.split("/")[-1]
+    else:
+        id = video
+    return id
+
+
+def qualities(json=True):
+    '''
+    json:
+      True - for return qualities and full form as json (default)
+      False - for return only qualities as list
+    '''
+    json_qualities = {
+        "sd": "Standard Quality",
+        "mq": "Medium Quality",
+        "hq": "High Quality",
+        "maxres": "Maximum Resolution"
+    }
+    if not json:
+        qualities = []
+        for i in json_qualities:
+            qualities.append(i)
+    else:
+        qualities = json_qualities
+    return qualities
+
+
+def thumbnail(video, quality='sd'):
+    '''
+    video:
+      id - video id
+      link - video link
+    quality:
+      sd - Standard Quality
+      mq - Medium Quality 
+      hq - High Quality
+      maxres - Maximum Resolution 
+    '''
+    
+    # extract the id
+    video_id = extract_id(video)
+    
+    # check the qualities
+    if quality not in qualities():
+        quality = 'sd'
+    
+    # thumbnail link
+    thumbnail = f"https://img.youtube.com/vi/{video_id}/{quality}default.jpg"
+    return thumbnail
+
+
+def download_thumbnail(video, name='thumbnail.jpg', quality='sd'):
+    '''
+    video:
+      id - video id
+      link - video link
+    name:
+      thumbnail name
+    quality:
+      sd - Standard Quality
+      mq - Medium Quality 
+      hq - High Quality
+      maxres - Maximum Resolution 
+    '''
+    
+    # thumbnail link
+    thumbnail = requests.get(thumbnail(video, quality)).content
+    
+    # save thumbnail
+    with open(name, "wb") as file:
+        file.write(thumbnail)
+    
+    # return name
+    return name
