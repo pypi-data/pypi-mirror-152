713e2fc5a80ce6553e9676462686284d17049f61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:35 +0000

    Add package cbcintacctsdk/0.0.13/cbcintacctsdk-0.0.13.tar.gz

---
+++ packages/cbcintacctsdk/cbcintacctsdk-0.0.13.tar.gz/cbcintacctsdk-0.0.13/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='cbcintacctsdk',
+    packages=['cbcintacctsdk'],
+    version='0.0.13',
+    # package_dir={'': '*'},
+    # install_requires=['*']
+)
