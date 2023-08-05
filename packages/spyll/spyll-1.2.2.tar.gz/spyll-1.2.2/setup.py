dbd1026fc8195329ade9ca84cd45c898799f9603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:02 +0000

    Add package spyll/1.2.2/spyll-1.2.2.tar.gz

---
+++ packages/spyll/spyll-1.2.2.tar.gz/spyll-1.2.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="spyll",
+    version="1.2.2",
+    author="Dango Labs",
+    author_email="info@dangolabs.com",
+    description="Spyll is an open source Python package that aims to make text file handling more streamlined. Works with Python versions 3.6+",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://dangolabs.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+            ],
+)
