990d98eb0bbca176a851f959c58f6ce5221d5908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:02 +0000

    Add package spyll/1.2.2/spyll-1.2.2-py3-none-any.whl

---
+++ packages/spyll/spyll-1.2.2-py3-none-any.whl/spyll/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from .spyll import *
+__author__ = "Dango Labs"
+__copyright__ = "Copyright (c) 2019 pTinosq"
+__credits__ = [""]
+__version__ = "1.2.2"
+__maintainer__ = "pTinosq"
+__email__ = "info@dangolabs.com"
+__status__ = "Release"
