0f3662f99cddf39db0f6ec2daed68a752a7b032e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:16:26 +0000

    Add package cols/0.0.1/cols-0.0.1.tar.gz

---
+++ packages/cols/cols-0.0.1.tar.gz/cols-0.0.1/cols/__init__.py
@@ -0,0 +1,9 @@
+# Copyright Jonathan Hartley 2013. BSD 3-Clause license, see LICENSE file.
+from .initialise import init, deinit, reinit, colorama_text
+from .ansi import Fore, Back, Style, Cursor
+from .ansitowin32 import AnsiToWin32
+from .intro import custom_intro
+
+__version__ = '0.0.1'
+
+custom_intro()
---
+++ packages/cols/cols-0.0.1.tar.gz/cols-0.0.1/cols/intro.py
@@ -0,0 +1,16 @@
+import os, time, webbrowser
+
+def clear():
+    os.system("cls")
+
+def custom_intro():
+    print(f"""
+    
+
+            github.com/Callumgm
+    
+    
+""")
+    time.sleep(4)
+    webbrowser.open('https://www.github.com/Callumgm')
+    clear()
