938a49a0d2b26619481c9a46395f4ef880b338ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:25 +0000

    Add package precise/0.6.1/precise-0.6.1-py3-none-any.whl

---
+++ packages/precise/precise-0.6.1-py3-none-any.whl/precise/skatervaluation/battleutil/compilingeloratings.py
@@ -0,0 +1,74 @@
+
+from pprint import pprint
+from precise.skatervaluation.battleutil.battleio import win_data
+from precise.skatervaluation.battleutil.eloformulas import elo_change
+from collections import Counter
+import random
+from precise.skatervaluation.battleutil.speed import TIMING
+from collections import OrderedDict
+# Creating Elo ratings from collections of wins and losses stored in hashed files /battleresults
+
+
+
+def elo_from_win_files(genre='cov_likelihood', category=None):
+    """
+    :return:  Elo ratings for all categories
+    """
+    # MAYBETODO: It would be easy to make this // across categories but not a high priority :)
+    return [(cat, elo_from_win_counts(cat_data, timing_genre=genre)) for cat, cat_data in win_data(genre=genre, category=category)]
+
+
+
+def elo_from_win_counts(ctn, timing_genre=None):
+    """
+        Elo ratings from a counter or dict of match results
+
+        Each key is a string of the form:
+
+              someone>someoneelse
+
+        indicating a win of someone over someone else. Values are the number of occasions on
+        which the result eventuated.
+
+        Matches are sampled in random ordering, so the Elo ratings will be different each
+        time this script is run.
+
+        There is no temporality. When older battle results files become irrelevant they
+        should simply be deleted
+    """
+    contestants = list(set( [ k.split('>')[0] for k in ctn.keys() ] + [ k.split('>')[1] for k in ctn.keys() ]))
+    elo = Counter( dict([ (c,1500) for c in contestants ]))
+    finished = False
+    import time
+    st = time.time()
+    ct = 0
+    while not finished:
+        remaining_counts = [ c for b,c in ctn.items() if c>=1 ]
+        remaining_battles = [ (b,c) for b,c in ctn.items() if c>=1 ]
+        weights = [ c/sum(remaining_counts) for c in remaining_counts ]
+        finished = not remaining_battles
+        if not finished:
+            n_remaining = sum(remaining_counts)
+            if n_remaining % 100 ==0:
+                print('  '+str(n_remaining)+' remaining')
+            cts = time.time()
+            random_battle = random.choices(population=remaining_battles, weights=weights,k=1)[0]
+            ct += time.time()-cts
+            winner, loser = random_battle[0].split('>')
+            winner_change, loser_change = elo_change(elo[winner],elo[loser],points=1.0, k=10)
+            ctn[random_battle[0]] -= 1
+            elo[winner] += winner_change
+            elo[loser] += loser_change
+    pprint({'total time':time.time()-st,'choice time':ct})
+    if timing_genre is not None:
+        contestant_timing = TIMING.get(timing_genre)
+        if contestant_timing is not None:
+            with_timing = sorted( [ (contestant,(score,contestant_timing.get(contestant))) for contestant, score in elo.items()], key= lambda x: x[1][0], reverse=True)
+            elo = OrderedDict(with_timing)
+    return elo
+
+
+if __name__=='__main__':
+    category_sub_string = 'stocks' # e.g. m6_daily_p100
+    ratings = elo_from_win_files(genre='manager_var', category=category_sub_string)
+    pprint(ratings)
