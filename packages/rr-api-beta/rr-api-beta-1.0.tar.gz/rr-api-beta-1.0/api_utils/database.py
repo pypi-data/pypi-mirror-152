bdd5841f875c02244165d346bdac28076e78e21b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:08 +0000

    Add package rr-api-beta/1.0/rr-api-beta-1.0.tar.gz

---
+++ packages/rr-api-beta/rr-api-beta-1.0.tar.gz/rr-api-beta-1.0/api_utils/database.py
@@ -0,0 +1,14 @@
+import os
+from rr.my_sql import MySqlConnection
+from api_utils.constants import General
+
+db_user = os.environ.get('user')
+db_password = os.environ.get('password')
+
+if not db_user:
+    db_user = General.DEFAULT_USER
+
+if not db_password:
+    db_password = General.DEFAULT_PASSWORD
+
+conn = MySqlConnection(user=db_user, password=db_password, database=General.DATABASE)
