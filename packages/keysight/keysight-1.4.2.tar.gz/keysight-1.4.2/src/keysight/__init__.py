a642df89dc1ce798358be659e1113556663b25fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:41 +0000

    Add package keysight/1.4.2/keysight-1.4.2.tar.gz

---
+++ packages/keysight/keysight-1.4.2.tar.gz/keysight-1.4.2/src/keysight/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2013-2022 The keysight developers. All rights reserved.
+# Project site: https://github.com/questrail/keysight
+# Use of this source code is governed by a MIT-style license that
+# can be found in the LICENSE.txt file for the project.
+"""Package initialization.
+"""
+__version__ = '1.4.2'
