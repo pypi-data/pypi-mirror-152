d9855dc81e4a7fdaa6ea13b638fbf19e898e0f51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:52 +0000

    Add package hssh/0.0.6/hssh-0.0.6.tar.gz

---
+++ packages/hssh/hssh-0.0.6.tar.gz/hssh-0.0.6/hssh/__init__.py
@@ -0,0 +1,13 @@
+__all__=['DefClass','Kmean']
+
+from .DefClass import *
+import random
+import pandas as pd
+from konlpy.tag import *
+from collections import Counter
+import numpy as np
+from math import log
+import copy
+import operator
+from numpy import dot
+from numpy.linalg import norm
