e8dd5b92629f04f09c67155b3ce8e80a39d02e6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:41 +0000

    Add package Auto-ML-study/0.0.2/Auto_ML_study-0.0.2-py3-none-any.whl

---
+++ packages/Auto-ML-study/Auto_ML_study-0.0.2-py3-none-any.whl/machine_learning/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.0.1"
+
+__library_info__ = {
+        "author": "EunJae Yong (Jay Yong)",
+        "version": "0.0.1",
+        "description": "This library was created for students studying autoML.",
+        "python_requires": ">=3.6"
+    }
