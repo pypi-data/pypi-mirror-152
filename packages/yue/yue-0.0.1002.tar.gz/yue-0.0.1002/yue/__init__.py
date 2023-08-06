d7fe6b9702fd46a1ce938831ebeb38f294f492c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:48:05 +0000

    Add package yue/0.0.1002/yue-0.0.1002.tar.gz

---
+++ packages/yue/yue-0.0.1002.tar.gz/yue-0.0.1002/yue/__init__.py
@@ -0,0 +1,11 @@
+# from orbitkit import util
+# from orbitkit.file_extractor.dispatcher import FileDispatcher
+
+name = 'orbitkit'
+
+__version__ = '0.0.1002'
+VERSION = __version__
+
+__all__ = [
+    'wy',
+]
