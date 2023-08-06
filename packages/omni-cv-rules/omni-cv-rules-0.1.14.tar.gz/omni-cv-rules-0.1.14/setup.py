1f6b7fb8957c013be9575c415542c5510d3d5678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:02 +0000

    Add package omni-cv-rules/0.1.14/omni-cv-rules-0.1.14.tar.gz

---
+++ packages/omni-cv-rules/omni-cv-rules-0.1.14.tar.gz/omni-cv-rules-0.1.14/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['omni_cv_rules',
+ 'omni_cv_rules.coconut',
+ 'omni_cv_rules.place_holder',
+ 'omni_cv_rules.py_310',
+ 'omni_cv_rules.py_38']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Pillow',
+ 'attrs',
+ 'bqplot',
+ 'frozendict',
+ 'ipywidgets',
+ 'numpy',
+ 'pandas',
+ 'pprintpp',
+ 'py-omni-converter']
+
+setup_kwargs = {
+    'name': 'omni-cv-rules',
+    'version': '0.1.14',
+    'description': '',
+    'long_description': None,
+    'author': 'proboscis',
+    'author_email': 'nameissoap@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
