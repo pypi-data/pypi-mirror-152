f913f8c71aadc989dece6efb4b86bb066c018d51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:19 +0000

    Add package bilibili-api-python/10.0.1/bilibili_api_python-10.0.1-py3-none-any.whl

---
+++ packages/bilibili-api-python/bilibili_api_python-10.0.1-py3-none-any.whl/bilibili_api/__init__.py
@@ -0,0 +1,19 @@
+import asyncio
+from modulefinder import Module
+import platform
+
+import nest_asyncio
+
+from .utils.aid_bvid_transformer import aid2bvid, bvid2aid
+from .utils.Credential import Credential
+from .utils.Danmaku import Danmaku
+from .utils.network import get_session, request, set_session
+from .utils.short import get_real_url
+from .utils.sync import sync
+
+# 支持嵌套事件
+nest_asyncio.apply()
+
+# 如果系统为 Windows，则修改默认策略，以解决代理报错问题
+if "windows" in platform.system().lower():
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
---
+++ packages/bilibili-api-python/bilibili_api_python-10.0.1-py3-none-any.whl/bilibili_api/misc.py
@@ -0,0 +1,51 @@
+"""
+杂项
+"""
+from enum import Enum
+from .utils.utils import get_api
+from .utils.network import request
+
+API = get_api('misc')
+
+class SearchObjectType(Enum):
+    VIDEO = "video"
+    BANGUMI = "media_bangumi"
+    FT = "media_ft"
+    LIVE = "live"
+    ARTICLE = "article"
+    TOPIC = "topic"
+    USER = "bili_user"
+
+async def web_search(keyword: str):
+    """
+    只指定关键字在 web 进行搜索，返回未经处理的字典
+
+    Args:
+        keyword (str): 搜索关键词
+
+    Returns:
+        调用 api 返回的结果
+    """
+    api = API["search"]["web_search"]
+    params = {
+        "keyword": keyword
+    }
+    return await request('GET', url=api["url"], params=params)
+
+async def web_search_by_type(keyword: str, search_type: SearchObjectType):
+    """
+    指定关键字和类型进行搜索，返回未经处理的字典
+    类型：视频(video)、番剧(media_bangumi)、影视(media_ft)、直播(live)、专栏(article)、话题(topic)、用户(bili_user)
+
+    Args:
+        keyword     (str): 搜索关键词
+        search_type (str): 搜索类型
+    Returns:
+        调用 api 返回的结果
+    """
+    api = API["search"]["web_search_by_type"]
+    params = {
+        "keyword": keyword,
+        "search_type": search_type.value
+    }
+    return await request('GET', url=api["url"], params=params)
