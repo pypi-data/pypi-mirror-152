5912169215bdc4579afbb7e21e1e92abe2c1594e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:22 +0000

    Add package binary-refinery/0.4.37/binary-refinery-0.4.37.tar.gz

---
+++ packages/binary-refinery/binary-refinery-0.4.37.tar.gz/binary-refinery-0.4.37/refinery/units/formats/archive/xttar.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import tarfile
+import datetime
+
+from refinery.lib.structures import MemoryFile
+from refinery.units.formats.archive import ArchiveUnit
+
+
+class xttar(ArchiveUnit):
+    """
+    Extract files from a Tar archive.
+    """
+    def __init__(self, *paths, list=False, join_path=False, drop_path=False, path=b'path', date=b'date'):
+        super().__init__(*paths, list=list, join_path=join_path, drop_path=drop_path, path=path, date=date)
+
+    def unpack(self, data: bytearray):
+        archive = tarfile.open(fileobj=MemoryFile(data))
+        for info in archive.getmembers():
+            if not info.isfile():
+                continue
+            extractor = archive.extractfile(info)
+            if extractor is None:
+                continue
+            date = datetime.datetime.fromtimestamp(info.mtime)
+            yield self._pack(info.name, date, lambda e=extractor: e.read())
+
+    @classmethod
+    def handles(cls, data: bytearray) -> bool:
+        ustar = data.find(B'ustar')
+        if ustar >= 0:
+            return data[ustar:ustar + 3] in (B'\x00\x30\x30', B'\x20\x20\x00')
+        return False
