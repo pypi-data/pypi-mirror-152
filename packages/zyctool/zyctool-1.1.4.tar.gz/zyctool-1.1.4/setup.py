4e7cbbc6e494bea77ba850406c75632859d61333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:33 +0000

    Add package zyctool/1.1.4/zyctool-1.1.4.tar.gz

---
+++ packages/zyctool/zyctool-1.1.4.tar.gz/zyctool-1.1.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="zyctool",
+    version="1.1.4",
+    author="zyc",
+    author_email="1109203201@qq.com",
+    description="爬虫工具库",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
