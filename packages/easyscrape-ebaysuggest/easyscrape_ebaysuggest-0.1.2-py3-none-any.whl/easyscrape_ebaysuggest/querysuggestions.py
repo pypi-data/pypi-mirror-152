7f70f17558f12a0c078dda672881450aecf51728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:18 +0000

    Add package easyscrape-ebaysuggest/0.1.2/easyscrape_ebaysuggest-0.1.2-py3-none-any.whl

---
+++ packages/easyscrape-ebaysuggest/easyscrape_ebaysuggest-0.1.2-py3-none-any.whl/easyscrape_ebaysuggest/querysuggestions.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+def query(term):
+
+
+    # set headers
+    header = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36','Referer':'https://www.google.com/'}
+
+    # define url
+    url = f"https://www.ebay.com/autosug?kwd={term}&_jgr=1&sId=0&_ch=0&_store=1&_help=1&callback=0"
+
+    # store http request into html variable
+    html = requests.get(url,params=None,headers=header)
+    results = html.json()
+
+    return results["res"]["sug"]
