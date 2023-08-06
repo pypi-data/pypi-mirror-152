2d06b2911dbbccd19b6f17d7b1028f8287289e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:40 +0000

    Add package ja3/1.0/ja3-1.0.tar.gz

---
+++ packages/ja3/ja3-1.0.tar.gz/ja3-1.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Copyright 2022 rev1si0n (ihaven0emmail@gmail.com). All rights reserved.
+import setuptools
+
+setuptools.setup(
+    name            = "ja3",
+    version         = "1.0",
+    python_requires = ">=3.6,<4.0",
+    zip_safe        = False,
+    install_requires= [
+        "pyja3==1.0.0",
+    ],
+)
