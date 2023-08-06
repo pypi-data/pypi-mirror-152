e506f0397024109254f910db091c772327e17663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:28 +0000

    Add package allmlmodelscomparing/0.0.3/allmlmodelscomparing-0.0.3.tar.gz

---
+++ packages/allmlmodelscomparing/allmlmodelscomparing-0.0.3.tar.gz/allmlmodelscomparing-0.0.3/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(name= "allmlmodelscomparing",
+                 version="0.0.3",
+                 license="MIT",
+                 author="Tugce Dogan",
+                 author_mail="tugcedogann@gmail.com",
+                 description="Data Science Tools",
+                 url="https://github.com/TWOCHE/pythonProject",
+                 keywords=["data science","machine learning","models"],
+                 classifiers=[
+                     "Development Status :: 4 - Beta",
+                     "License :: OSI Approved :: MIT License",
+                     "Programming Language :: Python :: 3.5",
+                     "Topic :: Scientific/Engineering",
+                 ],
+
+                 packages=setuptools.find_packages()
+                )
