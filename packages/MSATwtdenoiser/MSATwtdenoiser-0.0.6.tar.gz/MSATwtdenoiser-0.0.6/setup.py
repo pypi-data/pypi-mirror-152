2deee97249750e9ba719899c98a6de54e86558bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:20 +0000

    Add package MSATwtdenoiser/0.0.6/MSATwtdenoiser-0.0.6.tar.gz

---
+++ packages/MSATwtdenoiser/MSATwtdenoiser-0.0.6.tar.gz/MSATwtdenoiser-0.0.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(name='MSATwtdenoiser',
+      version='0.0.6',
+      description='Denoising a 2D signal based on wavelet',
+      long_description=readme,
+      long_description_content_type='text/markdown',
+      url='https://github.com/ahsouri/MSATwtdenoiser',
+      author='Amir Souri',
+      author_email='ahsouri@gmail.com',
+      license='MIT',
+      packages=['MSATwtdenoiser'],
+      install_requires=[
+          'numpy','matplotlib','scipy','PyWavelets','opencv-python'
+      ],
+      zip_safe=False)
