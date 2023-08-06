ecb26076c1aa36dcdae243063f96acb209943e33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:49 +0000

    Add package rlane-libcli/1.0.1/rlane-libcli-1.0.1.tar.gz

---
+++ packages/rlane-libcli/rlane-libcli-1.0.1.tar.gz/rlane-libcli-1.0.1/libcli/actions/basehelp.py
@@ -0,0 +1,8 @@
+"""Base class for our `HelpAction`s."""
+
+from libcli.actions.base import BaseAction
+
+
+class BaseHelpAction(BaseAction):
+    # pylint: disable=too-few-public-methods
+    """Base class for our `HelpAction`s."""
