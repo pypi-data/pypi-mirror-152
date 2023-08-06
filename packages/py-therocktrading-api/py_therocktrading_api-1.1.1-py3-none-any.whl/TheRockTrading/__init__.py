0205424af9417023a153b06607e7f871514eeb8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:49 +0000

    Add package py-therocktrading-api/1.1.1/py_therocktrading_api-1.1.1-py3-none-any.whl

---
+++ packages/py-therocktrading-api/py_therocktrading_api-1.1.1-py3-none-any.whl/TheRockTrading/__init__.py
@@ -0,0 +1,9 @@
+"""
+The official Python library for The Rock Trading exchange API
+"""
+
+__version__ = '1.1.1'
+
+from .client_sync import ClientSync as Client
+from .client_sync import ClientSync
+from .client_async import ClientAsync
