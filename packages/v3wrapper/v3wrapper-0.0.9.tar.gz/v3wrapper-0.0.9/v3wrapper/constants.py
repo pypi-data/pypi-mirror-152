925ce6933207382d87d6e1931dc1d1227cfcce96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:17:53 +0000

    Add package v3wrapper/0.0.9/v3wrapper-0.0.9.tar.gz

---
+++ packages/v3wrapper/v3wrapper-0.0.9.tar.gz/v3wrapper-0.0.9/v3wrapper/constants.py
@@ -0,0 +1,13 @@
+# V1 swagger
+# https://app.metrikus.io/api/v1
+
+parameter_to_return_value = {
+    'TEMPERATURE': 'Temperature',
+    'CO2': 'CO2',
+    'HUMIDITY': 'Humidity',
+    'VOC': 'VOC',
+    'DUST_PM_2_5': 'Dust PM2.5',
+    'NOISE': 'Noise',
+    'VIRUS_RISK':'Virus Risk',
+    'CAPACITY_COUNT': 'Capacity Count',
+    'AREA_COUNT': 'Area Count'}
