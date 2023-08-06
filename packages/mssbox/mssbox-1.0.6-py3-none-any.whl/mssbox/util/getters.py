c5116aeaa6c87f6a2999b3e770e25b0925f02d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:45 +0000

    Add package mssbox/1.0.6/mssbox-1.0.6-py3-none-any.whl

---
+++ packages/mssbox/mssbox-1.0.6-py3-none-any.whl/mssbox/util/getters.py
@@ -0,0 +1,48 @@
+import requests
+import json
+import xml.etree.ElementTree as ET
+from pathlib import Path
+
+
+def get_connected_sim(mssb_ip, mssb_serial, slot_terminal):
+    """
+    This method returns the SIM Card allocated to the provided slot
+    :param slot_terminal: int MSSB terminal port assigned to the slot
+    :return: returns a dict with SIM attribs (id, name, custom_1 (IMSI), custom_2 (PLMN)
+    """
+    sim = get_all_connected_sims(mssb_ip, mssb_serial).get(int(slot_terminal))
+    return sim
+
+
+def get_all_connected_sims(mssb_ip, mssb_serial):
+    """
+    This method returns all SIMs connected to terminals
+    :return: A dict with all SIMs with their attribs (id, name, custom_1 (IMSI), custom_2 (PLMN)
+    """
+    response = requests.get(f"http://{mssb_ip}/rest.php?command=listcon&mssbSerial={mssb_serial}")                                                 
+    response_xml = ET.fromstring(response.content)
+    connections = response_xml.find('connections').findall("terminal")
+    sims = dict()
+    for i in range(0, len(connections)):
+        sims[i] = connections[i].find("sim")
+    return sims
+
+
+def get_sims_info(repos_dir, mssb_serial, mssb_file_dir):
+    """
+    This function gets the info for the SIMs connected to the MSSB via file exported from MSSB
+    :param repos_dir: Your repository directory
+    :param mssb_serial: Serial for the MSSB
+    :param mssb_file_dir: The directory with the mssb file
+    :return: dict with the SIMs Config
+    """
+    file = f"{repos_dir}/{mssb_serial}_sims.json"
+    #mssb_file = f"{mssb_file_dir}/{mssb_serial}_sims.json"
+    mssb_file = Path(f"{mssb_file_dir}/{mssb_serial}_sims.json")
+    with open(file, 'wb') as f:
+        f.write(mssb_file.content)
+    with open(file) as json_file:
+        data = json.load(json_file)
+    return data
+
+    
