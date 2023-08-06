fed1e71c9de130979ebec5951122376d9ee88c3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:33 +0000

    Add package Random-decimal/1.1.0/Random%20decimal-1.1.0.tar.gz

---
+++ packages/Random-decimal/Random%20decimal-1.1.0.tar.gz/Random decimal-1.1.0/Random_decimal/__init__.py
@@ -0,0 +1,11 @@
+from . import Random_decimal
+__author__ = 'Nuoran'
+__all__ = [
+       're',
+       'datetime',
+       'json',
+       'uuid',
+       'math',
+       'requests',
+       'number'
+       ]
