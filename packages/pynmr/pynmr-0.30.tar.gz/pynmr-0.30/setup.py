3746c9acb3270b81f5b88f8e926a68706a4a1571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:49 +0000

    Add package pynmr/0.30/pynmr-0.30.tar.gz

---
+++ packages/pynmr/pynmr-0.30.tar.gz/pynmr-0.30/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open('README.txt') as f:
+    long_description = f.read()
+
+setup(name='pynmr',
+      version='0.30',
+      description='Parse and Process NMR data.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      classifiers=['License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python',
+                   'Topic :: Scientific/Engineering :: Physics'],
+      keywords='pynmr NMR TopSpin RS2D NTNMR Magritek',
+      url='http://github.com/bennomeier/pyNMR',
+      author='Benno Meier',
+      author_email='meier.benno@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+            install_requires = [
+          'numpy',
+          'spindata',
+                'scipy',
+                'paramiko',
+                'pyqtgraph',
+                'PyQt5',
+                'BeautifulSoup4',
+                'spindata',
+                'pysftp',
+          ],
+      zip_safe=False)
+
