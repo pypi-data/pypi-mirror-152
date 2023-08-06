03640890ff328612fc061e41db7313c1476a7972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:39 +0000

    Add package econpizza/0.1.6/econpizza-0.1.6-py3-none-any.whl

---
+++ packages/econpizza/econpizza-0.1.6-py3-none-any.whl/econpizza/parser/checks.py
@@ -0,0 +1,74 @@
+"""Checking functions for parsing.py.
+"""
+
+import jax.numpy as jnp
+
+
+def check_if_defined(evars, eqns, skipped_vars):
+    """Check if all variables are defined in period t.
+    """
+
+    for v in evars:
+        v_in_eqns = [
+            v in e.replace(v + "SS", "").replace(v + "Lag",
+                                                 "").replace(v + "Prime", "")
+            for e in eqns
+        ]
+        if not any(v_in_eqns) and not v in skipped_vars:
+            raise Exception(f"Variable `{v}` is not defined for time t.")
+    return
+
+
+def check_dublicates_and_determinancy(evars, eqns):
+    """Check if there are dublicates and the numbers of eqns/vars match.
+    """
+
+    dubs = [x for i, x in enumerate(evars) if x in evars[:i]]
+    dubmess = (
+        ", variables list contains dublicate(s): %s" % ", ".join(
+            dubs) if dubs else ""
+    )
+
+    sorted_evars = evars[:] = sorted(list(set(evars)), key=str.lower)
+
+    if len(sorted_evars) != len(eqns):
+        raise Exception(
+            "Model has %s variables but %s equations%s."
+            % (len(sorted_evars), len(eqns), dubmess)
+        )
+    elif dubs:
+        print("(parse:) Warning%s" % dubmess)
+
+    return sorted_evars
+
+
+def check_func(model, shocks, par):
+
+    init = model['init'][..., jnp.newaxis]
+    # collect some information needed later
+    model['init_run'] = {}
+
+    if model.get('decisions'):
+        # make a test backward and forward run
+        init_vf = model['init_vf']
+        _, decisions_output_init, exog_grid_vars_init = model['context']['func_backw'](
+            init, init, init, init, init_vf, jnp.zeros(len(shocks)), jnp.array(list(par.values())))
+        dists_init, _ = model['context']['func_stst_dist'](
+            decisions_output_init, 1e-8, 10_000)
+    else:
+        decisions_output_init = dists_init = exog_grid_vars_init = []
+
+    model['init_run']['decisions_output'] = decisions_output_init
+    model['init_run']['dists'] = dists_init
+    model['init_run']['exog_grid_vars'] = exog_grid_vars_init
+
+    # final test of main function
+    test = model['context']['func_eqns'](init, init, init, init, jnp.zeros(len(shocks)), jnp.array(list(
+        par.values())), jnp.array(dists_init)[..., jnp.newaxis], jnp.array(decisions_output_init)[..., jnp.newaxis])
+
+    if jnp.isnan(test).any():
+        raise Exception("Initial values are NaN.")
+    if jnp.isinf(test).any():
+        raise Exception("Initial values are not finite.")
+
+    return
---
+++ packages/econpizza/econpizza-0.1.6-py3-none-any.whl/econpizza/test_all.py
@@ -0,0 +1,79 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+from econpizza.__init__ import *
+import econpizza as ep
+
+
+def test_bh():
+
+    mod = load(example_bh, raise_errors=False)
+    _ = mod.solve_stst()
+
+    state = np.zeros(len(mod["variables"]))
+    state[:-1] = [0.1, 0.2, 0.0]
+
+    x, _, flag = find_path(
+        mod, state, T=50, max_horizon=500, tol=1e-8, verbose=2)
+
+    assert flag == 0
+    assert np.allclose(x[9], np.array(
+        [0.22287535, 0.25053816, 0.24429734, 0.23821162]))
+
+
+def test_nk():
+
+    mod = load(example_nk)
+    _ = mod.solve_stst()
+
+    state = mod["stst"].copy()
+    state["beta"] *= 1.02
+
+    x, _, flag = find_path(mod, state.values(), T=10,
+                           max_horizon=10, verbose=2)
+
+    assert flag == 0
+    assert np.allclose(
+        x[9], np.array([1.00608913, 0.32912145, 6.50140449, 1.00499411,
+                       1.00266364, 1.00266364, 0.83199537, 0.32912146])
+    )
+
+
+def test_stacked():
+
+    mod = load(example_nk)
+    _ = mod.solve_stst()
+
+    shk = ("e_beta", 0.02)
+
+    x, _, flag = find_path_stacked(mod, shock=shk)
+
+    assert flag == 0
+    assert np.allclose(
+        x[9],
+        np.array([1.00703268, 0.32763172, 6.50140449, 1.00377031,
+                 1., 0.99306852, 0.82840695, 0.32765387])
+    )
+
+
+def test_hank():
+
+    mod_dict = ep.parse(example_hank)
+    mod = ep.load(mod_dict)
+    _ = mod.solve_stst(tol_newton=1e-4)
+
+    x0 = mod['stst'].copy()
+    x0['beta'] = 0.99  # setting a shock on the discount factor
+
+    x, _, flag = mod.find_stack(x0.values(), horizon=50, tol=1e-4)
+
+    assert not flag
+    assert np.allclose(
+        x[9], np.array(
+            [5.60257455,  0.99016942,  0.99946153,  0.16642222,
+             1.00079412,  0.99473307,  0.99683063,  1.,
+             0.99156129,  1.002, -0.01775662,  0.42670816,
+             0.28734971,  0.78473366,  0.83237827,  0.99946153,
+             1.00079412,  1.]
+        )
+    )
