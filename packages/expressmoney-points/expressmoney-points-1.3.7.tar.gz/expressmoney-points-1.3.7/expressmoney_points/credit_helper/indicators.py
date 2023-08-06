3d50a69864c064173ed089e587596efc1dde5892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:38 +0000

    Add package expressmoney-points/1.3.7/expressmoney-points-1.3.7.tar.gz

---
+++ packages/expressmoney-points/expressmoney-points-1.3.7.tar.gz/expressmoney-points-1.3.7/expressmoney_points/credit_helper/indicators.py
@@ -0,0 +1,46 @@
+__all__ = ('IndicatorPoint', 'IndicatorFillingTaskPoint')
+
+from expressmoney.api import *
+
+SERVICE = 'credit_helper'
+APP = 'indicators'
+
+
+class IndicatorFillingTaskCreateContract(Contract):
+    pass
+
+
+class IndicatorReadContract(Contract):
+    BAD = 'BAD'
+    GOOD = 'GOOD'
+    RATE_CHOICES = (
+        (BAD, BAD),
+        (GOOD, GOOD),
+    )
+    created = serializers.DateTimeField()
+    value = serializers.IntegerField(min_value=0)
+    rate = serializers.ChoiceField(choices=RATE_CHOICES)
+    recommendation = serializers.CharField(max_length=1024)
+
+
+class IndicatorFillingTaskID(ID):
+    _service = SERVICE
+    _app = APP
+    _view_set = 'indicator_filling_task'
+
+
+class IndicatorID(ID):
+    _service = SERVICE
+    _app = APP
+    _view_set = 'indicator'
+
+
+class IndicatorFillingTaskPoint(CreatePointMixin, ContractPoint):
+    _point_id = IndicatorFillingTaskID()
+    _create_contract = IndicatorFillingTaskCreateContract
+
+
+class IndicatorPoint(ListPointMixin, ContractPoint):
+    _point_id = IndicatorID()
+    _read_contract = IndicatorReadContract
+    _sort_by = 'created'
