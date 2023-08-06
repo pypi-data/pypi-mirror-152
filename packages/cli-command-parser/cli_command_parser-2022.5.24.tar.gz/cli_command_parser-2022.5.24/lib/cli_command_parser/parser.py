99ac45a2828913ed525f1f88d9526f4cfec8ecf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:05 +0000

    Add package cli-command-parser/2022.5.24/cli_command_parser-2022.5.24.tar.gz

---
+++ packages/cli-command-parser/cli_command_parser-2022.5.24.tar.gz/cli_command_parser-2022.5.24/lib/cli_command_parser/parser.py
@@ -0,0 +1,238 @@
+"""
+The class that handles parsing input.
+
+:author: Doug Skrypa
+"""
+
+import logging
+from collections import deque
+from typing import TYPE_CHECKING, Optional, Any, Deque
+
+from .context import ctx
+from .exceptions import (
+    UsageError,
+    NoSuchOption,
+    MissingArgument,
+    ParamUsageError,
+    CommandDefinitionError,
+    ParamsMissing,
+)
+from .parameters import BaseOption, Parameter, BasePositional
+
+if TYPE_CHECKING:
+    from .core import CommandType
+    from .command_parameters import CommandParameters
+
+__all__ = ['CommandParser']
+log = logging.getLogger(__name__)
+
+
+class CommandParser:
+    """Stateful parser used for a single pass of argument parsing"""
+
+    def __init__(self):
+        self.params = ctx.params
+        self.deferred = None
+        self.arg_deque = None
+        self.positionals = ctx.params.positionals.copy()
+
+    @classmethod
+    def parse_args(cls) -> Optional['CommandType']:
+        params = ctx.params
+        sub_cmd_param = params.sub_command
+        if sub_cmd_param is not None and not sub_cmd_param.choices:
+            ctx.failed = True
+            raise CommandDefinitionError(f'{ctx.command}.{sub_cmd_param.name} = {sub_cmd_param} has no sub Commands')
+
+        cls()._parse_args()
+        cls._validate_groups(params)
+
+        if sub_cmd_param is not None:
+            try:
+                next_cmd = sub_cmd_param.result()  # type: CommandType
+            except UsageError:
+                ctx.failed = True
+                if not ctx.parsed_always_available_action_flags:
+                    raise
+            else:
+                missing = params.missing()
+                if missing and next_cmd.__class__.parent(next_cmd) is not ctx.command:
+                    ctx.failed = True
+                    if ctx.parsed_always_available_action_flags:
+                        return None
+                    raise ParamsMissing(missing)
+                return next_cmd
+
+        missing = params.missing()
+        if missing and not ctx.allow_missing and (not params.action or params.action not in missing):
+            ctx.failed = True
+            # Action is excluded because it provides a better error message
+            if not ctx.parsed_always_available_action_flags:
+                raise ParamsMissing(missing)
+        elif ctx.remaining and not ctx.ignore_unknown:
+            ctx.failed = True
+            raise NoSuchOption('unrecognized arguments: {}'.format(' '.join(ctx.remaining)))
+
+        return None
+
+    @classmethod
+    def _validate_groups(cls, params: 'CommandParameters'):
+        exc = None
+        for group in params.groups:
+            try:
+                group.validate()
+            except ParamsMissing as e:  # Let ParamConflict propagate before ParamsMissing
+                if exc is None:
+                    exc = e
+
+        if exc is not None:
+            raise exc
+
+    def _parse_args(self):
+        self.arg_deque = arg_deque = self.handle_pass_thru()
+        self.deferred = ctx.remaining = []
+        while arg_deque:
+            arg = arg_deque.popleft()
+            if arg == '--':
+                if ctx.params.find_nested_pass_thru():
+                    self.deferred.append(arg)
+                    self.deferred.extend(arg_deque)
+                    break
+                else:
+                    raise NoSuchOption(f'invalid argument: {arg}')
+            elif arg.startswith('---'):
+                raise NoSuchOption(f'invalid argument: {arg}')
+            elif arg.startswith('--'):
+                self.handle_long(arg)
+            elif arg.startswith('-') and arg != '-':
+                try:
+                    self.handle_short(arg)
+                except KeyError:
+                    self._check_sub_command_options(arg)
+                    if self.positionals:
+                        try:
+                            self.handle_positional(arg)
+                        except UsageError:
+                            self.deferred.append(arg)
+                    else:
+                        self.deferred.append(arg)
+            else:
+                self.handle_positional(arg)
+
+    def handle_pass_thru(self) -> Deque[str]:
+        remaining = ctx.remaining
+        pass_thru = self.params.pass_thru
+        if pass_thru is not None:
+            try:
+                separator_pos = remaining.index('--')
+            except ValueError as e:
+                if pass_thru.required:
+                    raise MissingArgument(pass_thru, "missing pass thru args separated from others with '--'") from e
+            else:
+                remainder_start = separator_pos + 1
+                pass_thru.take_action(remaining[remainder_start:])
+                return deque(remaining[:separator_pos])
+        return deque(remaining)
+
+    def handle_positional(self, arg: str):
+        # log.debug(f'handle_positional({arg=})')
+        try:
+            param = self.positionals.pop(0)  # type: BasePositional
+        except IndexError:
+            self.deferred.append(arg)
+        else:
+            try:
+                found = param.take_action(arg)
+            except UsageError:
+                self.positionals.insert(0, param)
+                raise
+            self.consume_values(param, found=found)
+
+    def handle_long(self, arg: str):
+        # log.debug(f'handle_long({arg=})')
+        try:
+            param, value = self.params.long_option_to_param_value_pair(arg)
+        except KeyError:
+            self._check_sub_command_options(arg)
+            self.deferred.append(arg)
+        else:
+            if value is not None or (param.accepts_none and not param.accepts_values):
+                param.take_action(value)
+            elif not self.consume_values(param) and param.accepts_none:
+                param.take_action(None)
+
+    def handle_short(self, arg: str):
+        # log.debug(f'handle_short({arg=})')
+        param_val_combos = self.params.short_option_to_param_value_pairs(arg)
+        # log.debug(f'Split {arg=} into {param_val_combos=}')
+        if len(param_val_combos) == 1:
+            param, value = param_val_combos[0]
+            self._handle_short_value(param, value)
+        else:
+            last = param_val_combos[-1][0]
+            for param, _ in param_val_combos[:-1]:
+                param.take_action(None, short_combo=True)
+
+            self._handle_short_value(last, None)
+
+    def _handle_short_value(self, param: BaseOption, value: Any):
+        # log.debug(f'Handling short {value=} for {param=}')
+        if value is not None or (param.accepts_none and not param.accepts_values):
+            param.take_action(value, short_combo=True)
+        elif not self.consume_values(param) and param.accepts_none:
+            param.take_action(None, short_combo=True)
+        # No need to raise MissingArgument if values were not consumed - consume_values handles checking nargs
+
+    def _check_sub_command_options(self, arg: str):
+        # log.debug(f'_check_sub_command_options({arg=})')
+        # This check is only needed when subcommand option values may be misinterpreted as positional values
+        if not self.positionals:
+            return
+        param = self.params.find_nested_option_that_accepts_values(arg)
+        if param is not None:
+            raise ParamUsageError(param, 'subcommand arguments must be provided after the subcommand')
+
+    def consume_values(self, param: Parameter, found: int = 0) -> int:
+        while True:
+            try:
+                value = self.arg_deque.popleft()
+            except IndexError:
+                # log.debug(f'Ran out of values in deque while processing {param=}')
+                return self._finalize_consume(param, None, found)
+            else:
+                # log.debug(f'Found {value=} in deque - may use it for {param=}')
+                if value.startswith('--'):
+                    return self._finalize_consume(param, value, found)
+                elif value.startswith('-') and value != '-':
+                    if self.params.get_option_param_value_pairs(value):
+                        # log.debug(f'{value=} will not be used with {param=} - it is also a parameter')
+                        return self._finalize_consume(param, value, found)
+                    else:
+                        try:
+                            self._check_sub_command_options(value)
+                        except ParamUsageError as e:
+                            return self._finalize_consume(param, value, found, e)
+
+                    if not param.would_accept(value):
+                        # log.debug(f'{value=} will not be used with {param=} - it would not be accepted')
+                        return self._finalize_consume(param, value, found, NoSuchOption(f'invalid argument: {value}'))
+                    # log.debug(f'{value=} may be used with {param=} as a value')
+
+                try:
+                    found += param.take_action(value)
+                except UsageError as e:
+                    # log.debug(f'{value=} was rejected by {param=}', exc_info=True)
+                    return self._finalize_consume(param, value, found, e)
+
+    def _finalize_consume(
+        self, param: Parameter, value: Optional[str], found: int, exc: Optional[Exception] = None
+    ) -> int:
+        if param.nargs.satisfied(found):
+            if value is not None:
+                self.arg_deque.appendleft(value)
+            # log.debug(f'consume_values {found=} for {param=}')
+            return found
+        elif exc:
+            raise exc
+        else:
+            raise MissingArgument(param, f'expected {param.nargs.min} values, but only found {found}')
---
+++ packages/cli-command-parser/cli_command_parser-2022.5.24.tar.gz/cli_command_parser-2022.5.24/lib/cli_command_parser/utils.py
@@ -0,0 +1,225 @@
+"""
+Utilities for extracting types from annotations, finding / storing program metadata, and other misc utilities.
+
+:author: Doug Skrypa
+"""
+
+import re
+import sys
+from collections.abc import Collection, Iterable, Callable
+from inspect import stack, getsourcefile, isclass
+from pathlib import Path
+from typing import Any, Union, Optional, Type, get_type_hints
+from string import whitespace, printable
+from urllib.parse import urlparse
+
+try:
+    from typing import get_origin, get_args as _get_args
+except ImportError:  # Added in 3.8; the versions from 3.8 are copied here
+    from .compat import get_origin, _get_args
+
+try:
+    from types import NoneType
+except ImportError:  # Added in 3.10
+    NoneType = type(None)
+
+from .exceptions import ParameterDefinitionError
+
+Bool = Union[bool, Any]
+_NotSet = object()
+
+
+class cached_class_property(classmethod):
+    def __init__(self, func: Callable):
+        super().__init__(property(func))  # noqa  # makes Sphinx handle it better than if this was not done
+        self.__doc__ = func.__doc__
+        self.func = func
+        self.values = {}
+
+    def __get__(self, obj: None, cls):  # noqa
+        try:
+            return self.values[cls]
+        except KeyError:
+            self.values[cls] = value = self.func(cls)
+            return value
+
+
+def validate_positional(
+    param_cls: str, value: str, prefix: str = 'choice', exc: Type[Exception] = ParameterDefinitionError
+):
+    if not value or value.startswith('-'):
+        raise exc(f"Invalid {param_cls} {prefix}={value!r} - may not be empty or start with '-'")
+
+    bad = {c for c in value if (c in whitespace and c != ' ') or c not in printable}
+    if bad:
+        raise exc(f'Invalid {param_cls} {prefix}={value!r} - invalid characters: {bad}')
+
+
+class ProgramMetadata:
+    def __init__(
+        self,
+        prog: str = None,
+        *,
+        url: str = None,
+        docs_url: str = None,
+        email: str = None,
+        version: str = None,
+        usage: str = None,
+        description: str = None,
+        epilog: str = None,
+    ):
+        try:
+            found_email, found_version, found_url, path = self._find_info()
+        except Exception:  # noqa
+            path = Path(__file__)
+            found_email, found_version, found_url = None, None, None
+
+        self.path = path
+        self.prog = prog or path.name
+        docs_url_from_repo_url = self._docs_url_from_repo_url
+        self.docs_url = docs_url or docs_url_from_repo_url(url) or docs_url_from_repo_url(found_url)
+        self.url = url or found_url
+        self.email = email or found_email
+        self.version = version or found_version or ''
+        self.usage = usage
+        self.description = description
+        self.epilog = epilog
+
+    def _find_info(self):
+        _stack = stack()
+        top_level_frame_info = _stack[-1]
+        installed_via_setup, g = self._find_dunder_info(top_level_frame_info)
+        email, version, repo_url = g.get('__author_email__'), g.get('__version__'), g.get('__url__')
+
+        path = Path(getsourcefile(top_level_frame_info[0]))
+        if installed_via_setup and path.name.endswith('-script.py'):
+            try:
+                path = path.with_name(Path(sys.argv[0]).name)
+            except IndexError:
+                path = path.with_name(path.stem[:-7] + '.py')
+        return email, version, repo_url, path
+
+    def _find_dunder_info(self, top_level_frame_info):  # noqa
+        g = top_level_frame_info.frame.f_globals
+        installed_via_setup = 'load_entry_point' in g and 'main' not in g  # TODO: This may need to be tweaked
+        for level in reversed(stack()[:-1]):
+            g = level.frame.f_globals
+            if any(k in g for k in ('__author_email__', '__version__', '__url__')):
+                return installed_via_setup, g
+        return installed_via_setup, g
+
+    def _docs_url_from_repo_url(self, repo_url: Optional[str]):  # noqa
+        try:  # Note: This is only done this way to address a false positive on a GitHub security scan
+            parsed = urlparse(repo_url)
+            if parsed.scheme == 'https' and parsed.hostname == 'github.com':
+                user, repo = parsed.path[1:].split('/')
+                return f'https://{user}.github.io/{repo}/'
+        except Exception:  # noqa
+            pass
+        return None
+
+    def format_epilog(self, extended: Bool = True) -> str:
+        parts = [self.epilog] if self.epilog else []
+        if parts and not extended:
+            return parts[0]
+
+        version = self.version
+        if version:
+            version = f' [ver. {version}]'
+        if self.email:
+            parts.append(f'Report {self.prog}{version} bugs to {self.email}')
+        url = self.docs_url or self.url
+        if url:
+            parts.append(f'Online documentation: {url}')
+        return '\n\n'.join(parts)
+
+
+def camel_to_snake_case(text: str, delim: str = '_') -> str:
+    return ''.join(f'{delim}{c}' if i and c.isupper() else c for i, c in enumerate(text)).lower()
+
+
+# region Annotation Inspection
+
+
+def get_descriptor_value_type(command_cls: type, attr: str) -> Optional[type]:
+    try:
+        annotation = get_type_hints(command_cls)[attr]
+    except KeyError:
+        return None
+
+    return get_annotation_value_type(annotation)
+
+
+def get_annotation_value_type(annotation) -> Optional[type]:
+    origin = get_origin(annotation)
+    """
+    Note on get_origin return values:
+    get_origin(List[str]) -> list
+    get_origin(List) -> list
+    get_origin(list[str]) -> list
+    get_origin(list) -> None
+    """
+    if origin is None and isinstance(annotation, type):
+        return annotation
+    elif isclass(origin) and issubclass(origin, (Collection, Iterable)):
+        return _type_from_collection(origin, annotation)
+    elif origin is Union:
+        return _type_from_union(annotation)
+    return None
+
+
+def get_args(annotation) -> tuple:
+    """
+    Wrapper around :func:`python:typing.get_args` for 3.7~8 compatibility, to make it behave more like it does in 3.9+
+    """
+    if getattr(annotation, '_special', False):  # 3.7-3.8 generic collection alias with no content types
+        return ()
+    return _get_args(annotation)
+
+
+def _type_from_union(annotation) -> Optional[type]:
+    args = get_args(annotation)
+    # Note: Unions of a single argument return the argument; i.e., Union[T] returns T, so the len can never be 1
+    if len(args) == 2 and NoneType in args:
+        arg = args[0] if args[1] is NoneType else args[1]
+    else:
+        return None
+
+    origin = get_origin(arg)
+    if origin is None and isinstance(arg, type):
+        return arg
+    elif isclass(origin) and issubclass(origin, (Collection, Iterable)):
+        return _type_from_collection(origin, arg)
+    else:
+        return None
+
+
+def _type_from_collection(origin, annotation) -> Optional[type]:
+    args = get_args(annotation)
+    try:
+        annotation = args[0]
+    except IndexError:  # The annotation was a collection with no content types specified
+        return origin
+
+    n_args = len(args)
+    if n_args > 2 or (n_args > 1 and (origin is not tuple or args[1] is not Ellipsis)):
+        return None
+
+    origin = get_origin(annotation)
+    if origin is None and isinstance(annotation, type):
+        return annotation
+    elif origin is Union:
+        return _type_from_union(annotation)
+    else:
+        return None
+
+
+# endregion
+
+
+def is_numeric(text: str) -> Bool:
+    try:
+        num_match = is_numeric._num_match
+    except AttributeError:
+        is_numeric._num_match = num_match = re.compile(r'^-\d+$|^-\d*\.\d+?$').match
+    return num_match(text)
