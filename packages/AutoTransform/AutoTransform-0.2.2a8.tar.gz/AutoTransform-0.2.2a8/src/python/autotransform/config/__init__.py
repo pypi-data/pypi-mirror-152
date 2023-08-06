ec5a2efe227767d7acf7006d737675f831335b5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:17 +0000

    Add package AutoTransform/0.2.2a8/AutoTransform-0.2.2a8.tar.gz

---
+++ packages/AutoTransform/AutoTransform-0.2.2a8.tar.gz/AutoTransform-0.2.2a8/src/python/autotransform/config/__init__.py
@@ -0,0 +1,34 @@
+# AutoTransform
+# Large scale, component based code modification library
+#
+# Licensed under the MIT License <http://opensource.org/licenses/MIT>
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2022-present Nathan Rockenbach <http://github.com/nathro>
+
+# @black_format
+
+"""Provides configuration to AutoTransform. By default this configuration is pulled from
+autotransform/config/config.ini (see autotransform/config/sample_config.ini). The configuration
+fetcher used can be overridden using the AUTO_TRANSFORM_CONFIG environment variable. This can
+support different methods of configuration, such as console input or environment variables.
+"""
+
+
+import os
+from typing import Dict, Type
+
+from autotransform.config.default import DefaultConfigFetcher
+from autotransform.config.envvar import EnvironmentVariableConfigFetcher
+from autotransform.config.fetcher import ConfigFetcher
+
+fetcher_to_use = os.getenv("AUTO_TRANSFORM_CONFIG")
+
+fetchers: Dict[str, Type[ConfigFetcher]] = {
+    "default": DefaultConfigFetcher,
+    "environment": EnvironmentVariableConfigFetcher,
+}
+
+if fetcher_to_use is not None:
+    fetcher: ConfigFetcher = fetchers.get(fetcher_to_use, DefaultConfigFetcher)()
+else:
+    fetcher = DefaultConfigFetcher()
