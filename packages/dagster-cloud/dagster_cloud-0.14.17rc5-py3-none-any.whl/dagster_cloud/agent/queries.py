5c3e64e9b6f80be7b748471a0b0a02f07c371e4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:01 +0000

    Add package dagster-cloud/0.14.17rc5/dagster_cloud-0.14.17rc5-py3-none-any.whl

---
+++ packages/dagster-cloud/dagster_cloud-0.14.17rc5-py3-none-any.whl/dagster_cloud/agent/queries.py
@@ -0,0 +1,41 @@
+GET_USER_CLOUD_REQUESTS_QUERY = """
+    mutation GetUserCloudRequests {
+        userCloudAgent {
+            popUserCloudAgentRequests(limit:10) {
+                requestId
+                requestApi
+                requestBody
+            }
+        }
+    }
+"""
+
+WORKSPACE_ENTRIES_QUERY = """
+    query WorkspaceEntries {
+        workspace {
+            workspaceEntries {
+                locationName
+                serializedDeploymentMetadata
+                hasOutdatedData
+                metadataTimestamp
+                sandboxSavedTimestamp
+                sandboxProxyInfo {
+                    hostname
+                    port
+                    authToken
+                }
+            }
+        }
+    }
+"""
+
+
+ADD_AGENT_HEARTBEAT_MUTATION = """
+    mutation AddAgentHeartbeat($serializedAgentHeartbeat: String!) {
+        userCloudAgent {
+            addAgentHeartbeat (serializedAgentHeartbeat: $serializedAgentHeartbeat) {
+                ok
+            }
+        }
+    }
+"""
