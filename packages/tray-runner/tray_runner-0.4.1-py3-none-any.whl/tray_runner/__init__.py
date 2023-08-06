5ee68070e5a5d61b7075c4831880f8f2556ad6e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:31 +0000

    Add package tray-runner/0.4.1/tray_runner-0.4.1-py3-none-any.whl

---
+++ packages/tray-runner/tray_runner-0.4.1-py3-none-any.whl/tray_runner/__init__.py
@@ -0,0 +1,28 @@
+"""
+tray_runner module.
+"""
+import importlib.metadata
+import os
+import sys
+
+import click
+
+from tray_runner.constants import DEVELOPMENT_VERSION
+
+try:
+    __version__ = importlib.metadata.version(__package__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = DEVELOPMENT_VERSION
+
+PACKAGE_DIR = os.path.dirname(__file__)
+
+if os.getenv("CMD_RUNNER_PORTABLE") == "1":
+    if getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS"):
+        # Running using Pyinstaller, so we take the dir where the executable is located
+        # https://pyinstaller.org/en/stable/runtime-information.html#using-sys-executable-and-sys-argv-0
+        APP_DIR = os.path.dirname(sys.argv[0])
+    else:
+        APP_DIR = os.getcwd()
+else:
+    APP_DIR = click.get_app_dir(__name__)
+DEFAULT_CONFIG_FILE = os.path.join(APP_DIR, "config.json")
