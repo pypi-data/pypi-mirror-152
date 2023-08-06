a3499f70b79647043e2627cfebdaf81ababcbd7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:05 +0000

    Add package delta-task/0.5.3/delta-task-0.5.3.tar.gz

---
+++ packages/delta-task/delta-task-0.5.3.tar.gz/delta-task-0.5.3/delta/task/__init__.py
@@ -0,0 +1,5 @@
+from .analytics import HorizontalAnalytics
+from .learning import HorizontalLearning, FaultTolerantFedAvg, FedAvg
+
+
+__all__ = ["HorizontalAnalytics", "HorizontalLearning", "FaultTolerantFedAvg", "FedAvg"]
