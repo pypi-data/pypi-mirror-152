bc187be390e754786c79d6c54598fcc8f5cd1cc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:07 +0000

    Add package mypy-boto3-emr-serverless/1.23.9/mypy_boto3_emr_serverless-1.23.9-py3-none-any.whl

---
+++ packages/mypy-boto3-emr-serverless/mypy_boto3_emr_serverless-1.23.9-py3-none-any.whl/mypy_boto3_emr_serverless/__init__.pyi
@@ -0,0 +1,32 @@
+"""
+Main interface for emr-serverless service.
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_emr_serverless import (
+        Client,
+        EMRServerlessWebServiceClient,
+        ListApplicationsPaginator,
+        ListJobRunsPaginator,
+    )
+
+    session = Session()
+    client: EMRServerlessWebServiceClient = session.client("emr-serverless")
+
+    list_applications_paginator: ListApplicationsPaginator = client.get_paginator("list_applications")
+    list_job_runs_paginator: ListJobRunsPaginator = client.get_paginator("list_job_runs")
+    ```
+"""
+from .client import EMRServerlessWebServiceClient
+from .paginator import ListApplicationsPaginator, ListJobRunsPaginator
+
+Client = EMRServerlessWebServiceClient
+
+__all__ = (
+    "Client",
+    "EMRServerlessWebServiceClient",
+    "ListApplicationsPaginator",
+    "ListJobRunsPaginator",
+)
