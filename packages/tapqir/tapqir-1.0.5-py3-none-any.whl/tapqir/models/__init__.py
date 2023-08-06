4d816eaebfb5a314c6ce55267018651b680fa61e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:40 +0000

    Add package tapqir/1.0.5/tapqir-1.0.5-py3-none-any.whl

---
+++ packages/tapqir/tapqir-1.0.5-py3-none-any.whl/tapqir/models/__init__.py
@@ -0,0 +1,18 @@
+# Copyright Contributors to the Tapqir project.
+# SPDX-License-Identifier: Apache-2.0
+
+from tapqir.models.cosmos import cosmos
+from tapqir.models.hmm import HMM
+from tapqir.models.model import Model
+
+__all__ = [
+    "models",
+    "Model",
+    "cosmos",
+    "HMM",
+]
+
+models = {
+    cosmos.name: cosmos,
+    HMM.name: HMM,
+}
