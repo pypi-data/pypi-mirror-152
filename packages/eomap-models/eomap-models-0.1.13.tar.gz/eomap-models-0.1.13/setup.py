aa4581e27141f6e60f35725e95f77bbb9c7acd07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:00 +0000

    Add package eomap-models/0.1.13/eomap-models-0.1.13.tar.gz

---
+++ packages/eomap-models/eomap-models-0.1.13.tar.gz/eomap-models-0.1.13/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['eomap_models', 'eomap_models.altimetry']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['geojson-pydantic==0.3.1', 'pydantic==1.7.3']
+
+setup_kwargs = {
+    'name': 'eomap-models',
+    'version': '0.1.13',
+    'description': '',
+    'long_description': None,
+    'author': 'Luis Coelho',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
