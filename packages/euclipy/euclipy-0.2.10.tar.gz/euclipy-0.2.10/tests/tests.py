c927e69c61da7d5c095f19d1723904cc045ba6cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:18:20 +0000

    Add package euclipy/0.2.10/euclipy-0.2.10.tar.gz

---
+++ packages/euclipy/euclipy-0.2.10.tar.gz/euclipy-0.2.10/tests/tests.py
@@ -0,0 +1,29 @@
+from euclipy.core import *
+from euclipy.geometric_objects import *
+from euclipy.measure import *
+from euclipy.polygon import *
+from euclipy.registry import *
+from euclipy.theorems import *
+from euclipy.tools import *
+
+if __name__ == '__main__':
+    import pprint
+    pp = pprint.PrettyPrinter(indent=4)
+
+    A = Point('A')
+    B = Point('B')
+    C = Point('C')
+    D = Point('D')
+    T1 = Triangle([A, B, C])
+    # T2 = Triangle([B, C, A]) # Test for identity of two triangles expressed in different point
+    # try:
+    #     T3 = Triangle([B, A, C])
+    # except:
+    #     print('Inconsistent triangle')
+    T1.angles[0].measure.value = 60
+    T1.angles[1].measure.value = 60
+    T1.angles[2].measure.value = 60
+    T1.edges[2].measure.value = 1
+    theorem_applied = isosceles_triangle_theorem(T1)
+    print(f'isosceles_triangle_theorem ran: {theorem_applied}')
+    pp.pprint(Registry().entries)
