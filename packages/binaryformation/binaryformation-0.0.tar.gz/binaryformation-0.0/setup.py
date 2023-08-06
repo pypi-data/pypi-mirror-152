29819180f6c91013c2f5d1039c260dcb6a23d603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:51 +0000

    Add package binaryformation/0.0/binaryformation-0.0.tar.gz

---
+++ packages/binaryformation/binaryformation-0.0.tar.gz/binaryformation-0.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name="binaryformation",
+version="0.0",
+description="Can be used for conversion of binary",
+author="Saksham jain",
+packages=['binaryformation'],
+install_requires=[])
