92d76ffe60b71aa8fdafd02393ec59eecc83c484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:00 +0000

    Add package Blackeyed/0.0.1/Blackeyed-0.0.1-py3-none-any.whl

---
+++ packages/Blackeyed/Blackeyed-0.0.1-py3-none-any.whl/BLACKEYED/main.py
@@ -0,0 +1,16 @@
+series = {
+    1: "The Office (2005)",
+    2: "Scenes from a Marriage (2021)",
+    3: "The Sopranos (1999)",
+    4: "Twin Peaks (1990)",
+    5: "Olive Kitteridge (2014)"
+}
+
+
+def best_series():
+    for k, v in series.items():
+        print(f"{k}. {v}")
+
+
+
+
