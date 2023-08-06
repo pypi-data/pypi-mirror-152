6184bbc54f2c230f1e93c28fb2fa91e65e543c34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:44:57 +0000

    Add package pipelines-test/1.0.4/pipelines_test-1.0.4.tar.gz

---
+++ packages/pipelines-test/pipelines_test-1.0.4.tar.gz/pipelines_test-1.0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='pipelines_test',
+    version='1.0.4',
+    description='SkillCorner API client',
+    author='SkillCorner',
+    author_email='support@skillcorner.com',
+    packages=['pipelines'],
+    python_requires='>=3.6',
+    install_requires=[
+        'requests>=2.20.0',
+        'makefun>=1.10.0'
+    ],
+    extras_require={
+        'test': 'mock==4.0.3',
+        'release': ['Sphinx==4.2.0', 'sphinx-rtd-theme==1.0.0']
+    }
+)
