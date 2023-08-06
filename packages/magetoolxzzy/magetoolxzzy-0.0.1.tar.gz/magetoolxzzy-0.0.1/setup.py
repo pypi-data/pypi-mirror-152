921cf25055ea4b170be22a2c2d4fd9f8c159518d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:18 +0000

    Add package magetoolxzzy/0.0.1/magetoolxzzy-0.0.1.tar.gz

---
+++ packages/magetoolxzzy/magetoolxzzy-0.0.1.tar.gz/magetoolxzzy-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="magetoolxzzy",
+    version="0.0.1",
+    author="ken",
+    author_email="125462@gmail.com",
+    description="Image kernel.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/kenblikylee/imgkernel",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
