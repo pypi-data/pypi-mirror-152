74cc8a0823f98dc929d814851e58440f09d0b2f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:24:19 +0000

    Add package alpha0/0.0.0/alpha0-0.0.0.tar.gz

---
+++ packages/alpha0/alpha0-0.0.0.tar.gz/alpha0-0.0.0/yue/wn.py
@@ -0,0 +1,15 @@
+import time
+from urllib import parse, request
+
+
+def notify(token,msg):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.82 Safari/537.36'}
+    localtime = time.asctime( time.localtime(time.time()) )
+    # print(localtime+'\n'+responses)
+    data = parse.urlencode({'text': localtime+'\n'+msg}).encode()
+    req = request.Request(
+        "https://api.chanify.net/v1/sender/"+token.strip()+'?sound=1',
+        data=data)
+    request.urlopen(req)
+    print("已通知，"+"本地时间为 :"+ localtime)
