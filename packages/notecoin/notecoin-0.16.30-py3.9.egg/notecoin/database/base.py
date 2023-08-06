74f3644bf322c24b86b7dfd86777ecc4b70df5c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:17 +0000

    Add package notecoin/0.16.30/notecoin-0.16.30-py3.9.egg

---
+++ packages/notecoin/notecoin-0.16.30-py3.9.egg/notecoin/database/base.py
@@ -0,0 +1,46 @@
+import logging
+import os
+
+from notetool.secret import read_secret
+from sqlalchemy import (BIGINT, Column, Float, MetaData, String, Table,
+                        create_engine)
+from sqlalchemy.orm import declarative_base
+
+uri = read_secret(cate1='notecoin', cate2='dataset', cate3='db_path')
+uri = uri or f'sqlite:///{os.path.abspath(os.path.dirname(__file__))}/data/notecoin.db'
+# engine = create_engine(uri, echo=True)
+meta = MetaData()
+engine = create_engine(uri)
+Base = declarative_base()
+logging.info(f'uri:{uri}')
+
+
+class BTable:
+    def __init__(self, table_name, *args, **kwargs):
+        self.table_name = table_name
+        self.table: Table
+        meta.create_all(engine)
+
+    def insert(self, values, *args, **kwargs):
+        meta.create_all(engine)
+        cols = [col.name for col in self.table.columns]
+        if isinstance(values, dict):
+            values = dict([(k, v) for k, v in values.items() if k in cols])
+        elif isinstance(values, list):
+            values = [dict([(k, v) for k, v in item.items() if k in cols]) for item in values]
+        ins = self.table.insert(values=values).prefix_with("OR REPLACE")
+        engine.execute(ins)
+
+
+class KlineData(BTable):
+    def __init__(self, table_name="kline_data_1min", *args, **kwargs):
+        super(KlineData, self).__init__(table_name=table_name, *args, **kwargs)
+        self.table = Table(self.table_name, meta,
+                           Column('source', String(20), comment='source', primary_key=True),
+                           Column('symbol', String(30), comment='symbol', primary_key=True),
+                           Column('timestamp', BIGINT, comment='timestamp', primary_key=True),
+                           Column('open', Float, comment='open', default=0),
+                           Column('close', Float, comment='close', default=0),
+                           Column('low', Float, comment='low', default=0),
+                           Column('high', Float, comment='high', default=0),
+                           Column('vol', Float, comment='vol', default=0))
