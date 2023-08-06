838dc621586b1889633bcb108f37e2f7753fdf1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:40 +0000

    Add package HOPYBOX/1.5.0/HOPYBOX-1.5.0.tar.gz

---
+++ packages/HOPYBOX/HOPYBOX-1.5.0.tar.gz/HOPYBOX-1.5.0/hopybox/version.py
@@ -0,0 +1,12 @@
+import sys
+from platform import system,python_version
+
+version_number = 150
+version_type = 'default'
+# version_type = 'Beta'
+gcc_version = sys.version.split(' ')[8].split(']')[0]
+
+head_version = 'HOPYBOX 1.5.0 ({}, May 25 2022, 18:54:01)\n[Python {}] on {}\nType "help" , "copyright" , "version" , "update" or "license" for more information'.format(version_type,python_version(),system())
+
+def system_version():
+  print('\033[96mHOPYBOX:1.4.9\nPython:{}\nGCC:{}'.format(python_version(),gcc_version))
