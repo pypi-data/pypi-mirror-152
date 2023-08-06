2425a84da13f2291e995448c94cba197a02fed07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:34 +0000

    Add package freshworks/0.0.24/freshworks-0.0.24-py3-none-any.whl

---
+++ packages/freshworks/freshworks-0.0.24-py3-none-any.whl/freshdesk/__init__.py
@@ -0,0 +1,12 @@
+from freshdesk.client import Client
+from freshdesk.models.api import APIVersion
+from freshdesk.models.plan import Plan
+
+
+__all__ = [
+    # Client
+    "Client",
+    # Enumerators
+    "Plan",
+    "APIVersion",
+]
