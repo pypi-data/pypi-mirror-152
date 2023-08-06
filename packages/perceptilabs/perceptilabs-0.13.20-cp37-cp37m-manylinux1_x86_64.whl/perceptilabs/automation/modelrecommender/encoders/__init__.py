529cb189edcda2b5b3392541297f04f50ff1d30f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:41 +0000

    Add package perceptilabs/0.13.20/perceptilabs-0.13.20-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/perceptilabs/perceptilabs-0.13.20-cp37-cp37m-manylinux1_x86_64.whl/perceptilabs/automation/modelrecommender/encoders/__init__.py
@@ -0,0 +1,14 @@
+from perceptilabs.automation.modelrecommender.encoders.base import EncoderBlueprint
+from perceptilabs.automation.modelrecommender.encoders.numerical import (
+    NumericalEncoderBlueprint,
+)
+from perceptilabs.automation.modelrecommender.encoders.image import (
+    ImageEncoderBlueprint,
+)
+from perceptilabs.automation.modelrecommender.encoders.binary import (
+    BinaryEncoderBlueprint,
+)
+from perceptilabs.automation.modelrecommender.encoders.categorical import (
+    CategoricalEncoderBlueprint,
+)
+from perceptilabs.automation.modelrecommender.encoders.text import TextEncoderBlueprint
