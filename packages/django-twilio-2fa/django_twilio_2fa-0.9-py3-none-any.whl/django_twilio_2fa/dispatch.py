2bdfc6d7286023fd73b692aff9a271f4cd7b6373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:56 +0000

    Add package django-twilio-2fa/0.9/django_twilio_2fa-0.9-py3-none-any.whl

---
+++ packages/django-twilio-2fa/django_twilio_2fa-0.9-py3-none-any.whl/django_twilio_2fa/dispatch.py
@@ -0,0 +1,13 @@
+import django.dispatch
+
+
+__all__ = [
+    "twilio_2fa_verification_sent", "twilio_2fa_verification_success", "twilio_2fa_verification_status_changed",
+]
+
+
+twilio_2fa_verification_sent = django.dispatch.Signal()
+
+twilio_2fa_verification_success = django.dispatch.Signal()
+
+twilio_2fa_verification_status_changed = django.dispatch.Signal()
