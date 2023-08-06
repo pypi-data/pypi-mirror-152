9b7f2988822ce59b1177213f5debf3144753be58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:54 +0000

    Add package HOPYBOX/1.4.7/HOPYBOX-1.4.7.tar.gz

---
+++ packages/HOPYBOX/HOPYBOX-1.4.7.tar.gz/HOPYBOX-1.4.7/hopybox/__init__.py
@@ -0,0 +1,5 @@
+'''
+            Copyright (c) 2022 HOStudio123(ChenJinlin) ,
+                      All Rights Reserved.
+'''
+from .__main__ import *
---
+++ packages/HOPYBOX/HOPYBOX-1.4.7.tar.gz/HOPYBOX-1.4.7/hopybox/version.py
@@ -0,0 +1,5 @@
+from platform import system,python_version
+version_number = 147
+version_type = 'default'
+# version_type = 'Beta'
+version = 'HOPYBOX 1.4.7 ({}, May 25 2022, 18:54:01)\n[Python {}] on {}\nType "help" , "copyright" or "license" for more information'.format(version_type,python_version(),system())
