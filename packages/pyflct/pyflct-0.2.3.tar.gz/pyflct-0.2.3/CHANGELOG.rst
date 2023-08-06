71a10d7d18dbf3314738545fc3092f5791fd43e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:44 +0000

    Add package pyflct/0.2.3/pyflct-0.2.3.tar.gz

---
+++ packages/pyflct/pyflct-0.2.3.tar.gz/pyflct-0.2.3/CHANGELOG.rst
@@ -0,0 +1,46 @@
+0.2.3 (2022-05-24)
+==================
+
+Breaking Changes
+----------------
+
+- Dropped support for Python 3.6 and 3.7
+
+Features
+--------
+
+- Added wheels for python 3.10
+
+0.2.2 (2020-05-01)
+==================
+
+Features
+--------
+
+- Added wheels for python 3.9
+
+0.2.1 (2020-05-08)
+==================
+
+Features
+--------
+
+- Fix PyPi upload.
+
+
+0.2.0 (2020-05-08)
+==================
+
+Features
+--------
+
+- Updated docs and enabled windows via anaconda.
+
+
+0.1.0 (2020-05-01)
+==================
+
+Features
+--------
+
+- Seeded pyflct with the C, Cython, Python code and examples to create the pyflct package. (`#1 <https://github.com/sunpy/pyflct/pull/1>`__)
---
+++ packages/pyflct/pyflct-0.2.3.tar.gz/pyflct-0.2.3/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os  # isort:skip
+from itertools import chain  # isort:skip
+
+from setuptools import setup  # isort:skip
+from setuptools.config import read_configuration  # isort:skip
+from extension_helpers import get_extensions  # isort:skip
+
+################################################################################
+# Programmatically generate some extras combos.
+################################################################################
+extras = read_configuration("setup.cfg")["options"]["extras_require"]
+
+# Dev is everything
+extras["dev"] = list(chain(*extras.values()))
+
+# All is everything but tests and docs
+exclude_keys = ("tests", "docs", "dev")
+ex_extras = dict(filter(lambda i: i[0] not in exclude_keys, extras.items()))
+# Concatenate all the values together for 'all'
+extras["all"] = list(chain.from_iterable(ex_extras.values()))
+
+setup(
+    extras_require=extras,
+    use_scm_version={
+        "write_to": os.path.join("pyflct", "_version.py"),
+    },
+    ext_modules=get_extensions(),
+)
