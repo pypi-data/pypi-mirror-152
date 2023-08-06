2b0064c88fef111b3f62f48d3e6e31b7489fc2a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:46 +0000

    Add package ForTheMainWord/1.0.0/ForTheMainWord-1.0.0.tar.gz

---
+++ packages/ForTheMainWord/ForTheMainWord-1.0.0.tar.gz/ForTheMainWord-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ForTheMainWord',
+    version='1.0.0',
+    packages=['kits'],
+    url='https://github.com/ReddishXia/TheMainword',
+    license='MIT',
+    author='79988',
+    author_email='77957147@qq.com',
+    description='Get the main word in the paper'
+)
