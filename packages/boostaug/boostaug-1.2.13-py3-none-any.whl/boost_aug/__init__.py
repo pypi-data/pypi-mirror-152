6c7b2957605bef788b54ca1db4eb2c1811f8e3ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:49 +0000

    Add package boostaug/1.2.13/boostaug-1.2.13-py3-none-any.whl

---
+++ packages/boostaug/boostaug-1.2.13-py3-none-any.whl/boost_aug/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# file: __init__.py.py
+# time: 2022/1/1
+# author: yangheng <yangheng@m.scnu.edu.cn>
+# github: https://github.com/yangheng95
+# Copyright (C) 2021. All Rights Reserved.
+
+__version__ = '1.2.13'
+__name__ = 'boostaug'
+
+from boost_aug.boostaug_core import BoostingAug, AugmentBackend
