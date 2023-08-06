1d41b53502648310931308d1411104307c3b9ded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:18 +0000

    Add package hbutils/0.6.6/hbutils-0.6.6.tar.gz

---
+++ packages/hbutils/hbutils-0.6.6.tar.gz/hbutils-0.6.6/hbutils/design/__init__.py
@@ -0,0 +1,8 @@
+"""
+Overview:
+    Design pattern module. Even in python, design patterns are not emphasized like java, \
+    but some simple packages are often useful, such as singleton patterns.
+"""
+from .decorator import *
+from .final import *
+from .singleton import *
