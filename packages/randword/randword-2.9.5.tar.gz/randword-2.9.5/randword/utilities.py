304aa7c7a86fc463e57581e63c132ee376b24b08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:44 +0000

    Add package randword/2.9.5/randword-2.9.5.tar.gz

---
+++ packages/randword/randword-2.9.5.tar.gz/randword-2.9.5/randword/utilities.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from pkg_resources import resource_filename
+
+
+def get_data(folder: str, filename: str) -> list[str]:
+    words = []
+    data_path = Path(resource_filename('randword', 'data'))
+    filepath = data_path / folder / f'{filename}.txt'
+
+    with open(filepath, 'r') as pos_file:
+        pos_words = pos_file.readlines()
+        words.extend(pos_words)
+
+    return [word.rstrip() for word in words]
