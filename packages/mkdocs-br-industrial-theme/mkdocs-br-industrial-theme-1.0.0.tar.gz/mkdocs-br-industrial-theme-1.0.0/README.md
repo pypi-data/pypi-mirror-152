8f58f5c578a9bb4a80a4d7e935fe1a1482bbe606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:15 +0000

    Add package mkdocs-br-industrial-theme/1.0.0/mkdocs-br-industrial-theme-1.0.0.tar.gz

---
+++ packages/mkdocs-br-industrial-theme/mkdocs-br-industrial-theme-1.0.0.tar.gz/mkdocs-br-industrial-theme-1.0.0/README.md
@@ -0,0 +1,10 @@
+# B&R Industrial Automation Theme
+
+This is a base theme for use with MkDocs in the style of B&R Industrial Automation's documentation. You can see the style in action with this documentation. 
+
+Example of documentation can be found here: [Example](https://brcclark.github.io/)
+
+```
+pip install mkdocs-br-industrial-theme
+```
+
---
+++ packages/mkdocs-br-industrial-theme/mkdocs-br-industrial-theme-1.0.0.tar.gz/mkdocs-br-industrial-theme-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0'
+
+setup(
+    name="mkdocs-br-industrial-theme",
+    version=VERSION,
+    url='https://github.com/brcclark/mkdocs-br-theme',
+    license='GPL-3',
+    long_description='Theme to match with the standard B&R Industrial Automation documentation styling.',
+    author='Connor Trostel',
+    author_email='connor.trostel@br-automation.com',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'mkdocs.themes': [
+            'brtheme = br_theme',
+        ]
+    },
+    zip_safe=False
+)
