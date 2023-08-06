c80edb4284b236c16d36fa79d85828d8cd557e26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:52 +0000

    Add package KojiChannelValidator/0.1.1/KojiChannelValidator-0.1.1.tar.gz

---
+++ packages/KojiChannelValidator/KojiChannelValidator-0.1.1.tar.gz/KojiChannelValidator-0.1.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.rst").read_text()
+
+
+setup(
+  name = 'KojiChannelValidator',
+  packages = ['kojichannelvalidator'],
+  version = '0.1.1',
+  license='MIT',
+  description = 'Identify system resource drift across many Koji builders',
+  long_description=long_description,
+  long_description_content_type='text/x-rst',
+  author = 'Rory Wirch',
+  url = 'https://github.com/RoryWirch/KojiChannelValidator',
+  install_requires=[
+        "requests",
+        "koji"
+      ],
+  classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+  entry_points={
+    "console_scripts":[
+      "kcv=kojichannelvalidator.__main__:main",
+    ]
+  },
+)
