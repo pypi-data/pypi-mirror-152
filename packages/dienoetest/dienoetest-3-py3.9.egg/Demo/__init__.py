1f28e7509bdd7ac68320a25515df568c6042fed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:43 +0000

    Add package dienoetest/3/dienoetest-3-py3.9.egg

---
+++ packages/dienoetest/dienoetest-3-py3.9.egg/Demo/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+from demo_monitor import *
+def main():
+    print('test')
+def demo_monotor_api(target,content):
+    type_code='module_monitor'
+    demo_monitor_api(target,content)
+    print("succed")
