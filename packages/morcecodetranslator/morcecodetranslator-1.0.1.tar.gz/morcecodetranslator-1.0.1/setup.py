4a179621cf012cf9ede35225e6ef70ea38dd6f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:43 +0000

    Add package morcecodetranslator/1.0.1/morcecodetranslator-1.0.1.tar.gz

---
+++ packages/morcecodetranslator/morcecodetranslator-1.0.1.tar.gz/morcecodetranslator-1.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='morcecodetranslator',
+    version='1.0.1',
+    license='MIT',
+    author='Gughanathan M',
+    author_email='gugu.ap900@gmail.com',
+    packages=find_packages(),
+    url='https://github.com/gugupy/morcecodetranslator',
+    include_package_data=True,
+    keywords='Morce Code'
+)
