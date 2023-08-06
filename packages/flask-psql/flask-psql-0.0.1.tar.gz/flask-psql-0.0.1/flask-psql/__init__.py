a466552113bf6f774dc70fa84c7e5398a54b9f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:06 +0000

    Add package flask-psql/0.0.1/flask-psql-0.0.1.tar.gz

---
+++ packages/flask-psql/flask-psql-0.0.1.tar.gz/flask-psql-0.0.1/flask-psql/__init__.py
@@ -0,0 +1,3 @@
+from app import sayHello
+
+sayHello()
---
+++ packages/flask-psql/flask-psql-0.0.1.tar.gz/flask-psql-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'FlaskPostgresConnection'
+
+# Setting up
+setup(
+    name="flask-psql",
+    version=VERSION,
+    author="Rahil Patel (TechAddict)",
+    author_email="<rahil.geek21@outlook.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
+    keywords=['python', 'flask', 'postgres', 'database', 'sql', 'sequelize'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
