4e8539da45c4e5258092ef01fc0505cedf7dc94d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:54 +0000

    Add package likejs-pipe/0.0.1/likejs_pipe-0.0.1.tar.gz

---
+++ packages/likejs-pipe/likejs_pipe-0.0.1.tar.gz/likejs_pipe-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='likejs_pipe',
+    version='0.0.1',
+    url='https://github.com/CarlosDevlpr/functional_python',
+    license='MIT License',
+    author='Carlos Magno & Lucas Simoni',
+    author_email='carlosdiamantino13@gmail.com',
+    keywords='Functional Python',
+    description=u'This is a python library to simplify even more the life of python developers.')
+    
