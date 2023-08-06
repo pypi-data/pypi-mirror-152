727e4ad7a36731a3e7ccdcb226367390ca6b5424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:24:05 +0000

    Add package tvmbase/2.2.3/tvmbase-2.2.3.tar.gz

---
+++ packages/tvmbase/tvmbase-2.2.3.tar.gz/tvmbase-2.2.3/tvmbase/abi/converter.py
@@ -0,0 +1,26 @@
+from jsonizer import Jsonizer
+from tonclient.types import AbiContract, AbiFunction, AbiEvent, AbiData, AbiParam
+
+
+def json_to_abi(json: dict) -> AbiContract:
+    # fix abi via removing "outputs" field
+    for event in json['events']:
+        event.pop('outputs', None)
+    parser = Jsonizer(AbiContract, AbiFunction, AbiEvent, AbiData, AbiParam, lowercase_keys=True, replace_space='_')
+    return parser.parse(json)
+
+
+def abi_params_to_str(abi_params: list[AbiParam]) -> str:
+    result = list()
+    for abi_param in abi_params:
+        sub_result = _abi_param_to_tuple(abi_param)
+        result.append(sub_result)
+    return ','.join(result)
+
+
+def _abi_param_to_tuple(abi_param: AbiParam) -> str:
+    type_ = abi_param.type
+    if abi_param.components:
+        components_str = abi_params_to_str(abi_param.components)
+        type_ = type_.replace('tuple', '(' + components_str + ')')
+    return type_
