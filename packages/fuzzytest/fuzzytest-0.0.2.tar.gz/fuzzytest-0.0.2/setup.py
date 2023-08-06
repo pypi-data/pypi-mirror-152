2926413ea09c4ce33cd6872629ade5a3ffdbbb39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:54 +0000

    Add package fuzzytest/0.0.2/fuzzytest-0.0.2.tar.gz

---
+++ packages/fuzzytest/fuzzytest-0.0.2.tar.gz/fuzzytest-0.0.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md","r") as fh:
+	long_description = fh.read()
+	
+setuptools.setup(
+  name = "fuzzytest",
+  version = "0.0.2",
+  author = "Caio Souza",
+  author_email = "caios@take.net",
+  description = "Pacote para auxiliar no teste dos inputs na API de FuzzyMatch",
+  long_description = long_description,
+  long_description_content_type="text/markdown",
+  package_dir={'': 'src'},
+  keywords = [],
+  install_requires=[
+  'pandas',
+  'ujson',
+  'requests'
+  ],
+  classifiers=[  
+    "Development Status :: 4 - Beta",
+    "License :: OSI Approved :: MIT License",
+	"Operating System :: OS Independent",
+    "Programming Language :: Python :: 3"
+  ],
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
