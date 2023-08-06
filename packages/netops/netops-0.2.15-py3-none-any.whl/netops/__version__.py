2a4df62b4fbe8caefb65568cfb49790da4508b51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:46 +0000

    Add package netops/0.2.15/netops-0.2.15-py3-none-any.whl

---
+++ packages/netops/netops-0.2.15-py3-none-any.whl/netops/__version__.py
@@ -0,0 +1,5 @@
+# Version of the netdevops package
+
+VERSION = (0, 2, 15)
+
+__version__ = '.'.join(map(str, VERSION))
---
+++ packages/netops/netops-0.2.15-py3-none-any.whl/netops/digital_twin/models.py
@@ -0,0 +1,7 @@
+PORT_MAPPING_DB = 'port_mapping_'
+
+PORTS_TABLE =  'PORTS'
+
+PORTS_COLUMN_NAMES = ['id', 'po_interface', 'lo_interface']
+
+INVALID_JUNOS_CONFIG_STATEMENTS = ['data', 'configuration-', 'version', 'forwarding-options']
