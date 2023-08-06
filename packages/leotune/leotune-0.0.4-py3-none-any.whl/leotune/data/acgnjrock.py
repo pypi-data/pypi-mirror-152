54f349d0930eacacaa4f6463893667b07d0ff8d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:03 +0000

    Add package leotune/0.0.4/leotune-0.0.4-py3-none-any.whl

---
+++ packages/leotune/leotune-0.0.4-py3-none-any.whl/leotune/data/acgnjrock.py
@@ -0,0 +1,109 @@
+# (Lyrics from J-Rock songs of ACGN)
+# GNU General Public License v3.0,
+#             Copyright (C) 2022 凪坤 (GitHub ID: sandyzikun)
+# -*-
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# -*-
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# -*-
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# -*-
+lyrics = [
+    {   "title": "After Rain",
+        "author": "林 ゆうき (Yuki Hayashi), 深川 琴美 (Kotomi Fukagawa), ...",
+        "vocal": "水槽",
+        "content": [
+            "たとえば　今はまだ遠くても",
+            "ここまで　たどり着いた昨日が",
+            "何度も　背中を押してくれる",
+            "大きな大きな虹を描け！",
+        ],
+        "translation": [
+            "縱使現在距離終點還很遙遠",
+            "但我們努力到達的昨日",
+            "無數次在身後推動著我前進",
+            "繪下美麗而盛大的彩虹",
+        ]
+    },
+    {   "title": "CRY",
+        "author": "澤野 弘之 (Sawano Hiroyuki), ...",
+        "vocal": "Mizuki",
+        "content": [
+            "願い望んでも追いかけても　抱きたい声が遠く消される",
+            "だけど僕らは忘れていない　倒れても立つ子供の影",
+            "But I must know the end (I\'m calling in the rain)",
+            "And I won\'t ever stop (I won\'t see you again)",
+            "But I must not forget (We never can forget)",
+            "And I won\'t ever stop (That childlike silhouette)",
+        ],
+        "translation": [
+            "不管祈願也好，奮身追逐也罷，渴求擁抱的心聲早已於遠方消散",
+            "但是我們卻從未忘記，曾今跌倒再站起的孩童的身影",
+            "但我必須知曉結果 (我在雨中呼喚)",
+            "我將永不停下前進 (我已無法與你再次相會)",
+            "但我不能忘記 (我們永遠不會忘記)",
+            "我將永不停下前進 (曾經那充滿著孩子氣的身影)",
+        ]
+    },
+    {   "title": "Through the Dark",
+        "author": "Rei Yasuda, Kuraaki Hori, Jeff Miyahara",
+        "vocal": "安田レイ",
+        "content": [
+            "let me light your darkness 離れてても",
+            "won\'t you let me light your darkness",
+            "見つけるから",
+            "光よ走れ　あなたの元へ",
+            "through the dark",
+        ],
+        "translation": [
+            "讓我照亮你的黑暗　即使分隔兩地",
+            "難道你不想這樣麼",
+            "我會找到你的",
+            "跟隨那道光芒　奔向你的身旁",
+            "跨過黑暗",
+        ]
+    },
+    {   "title": "Through the Dark",
+        "author": "Rei Yasuda, Kuraaki Hori, Jeff Miyahara",
+        "vocal": "安田レイ",
+        "content": [
+            "let me light your darkness その温もり",
+            "won\'t you let me light your darkness",
+            "忘れられない",
+            "声を届けて　わたしの元へ",
+            "through the dark",
+        ],
+        "translation": [
+            "讓我照亮你的黑暗　還有那份溫存",
+            "難道你不想這樣麼",
+            "我此生都難忘",
+            "就讓你的聲音　傳達到我身旁",
+            "跨過黑暗",
+        ]
+    },
+    {   "title": "Through the Dark",
+        "author": "Rei Yasuda, Kuraaki Hori, Jeff Miyahara",
+        "vocal": "安田レイ",
+        "content": [
+            "let me light your darkness 一つの未来",
+            "won\'t you let me light your darkness",
+            "叶えたいだけ",
+            "光よ走れ　あなたの元へ",
+            "through the dark",
+        ],
+        "translation": [
+            "讓我照亮你的黑暗　那全新的未來",
+            "難道你不想這樣麼",
+            "我只想實現它",
+            "跟隨那道光芒　奔向你的身旁",
+            "跨過黑暗",
+        ]
+    },
+]
