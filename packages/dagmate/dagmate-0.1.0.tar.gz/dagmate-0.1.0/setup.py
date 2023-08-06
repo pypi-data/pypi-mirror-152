c373142af525ee2305db3e0582dbbdf3115290ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:48:02 +0000

    Add package dagmate/0.1.0/dagmate-0.1.0.tar.gz

---
+++ packages/dagmate/dagmate-0.1.0.tar.gz/dagmate-0.1.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dagmate']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['dagit>=0.14.13,<0.15.0',
+ 'dagster-graphql>=0.14.13,<0.15.0',
+ 'dagster>=0.14.13,<0.15.0']
+
+setup_kwargs = {
+    'name': 'dagmate',
+    'version': '0.1.0',
+    'description': 'Make your Dagster deployment faster and easier with Dagger',
+    'long_description': None,
+    'author': 'Anoop Sharma',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
