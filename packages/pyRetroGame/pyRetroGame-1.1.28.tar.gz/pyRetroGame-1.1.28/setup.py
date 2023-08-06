c62a5d9a2f4e18520ddbe5251924b6a56800c737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:16 +0000

    Add package pyRetroGame/1.1.28/pyRetroGame-1.1.28.tar.gz

---
+++ packages/pyRetroGame/pyRetroGame-1.1.28.tar.gz/pyRetroGame-1.1.28/setup.py
@@ -0,0 +1,26 @@
+from ctypes.wintypes import LONG
+from setuptools import setup, find_packages
+
+VERSION = '1.1.28'
+DESCRIPTION = 'A Python library for creating games in the terminal. test'
+LONG_DESCRIPTION = open('ReadME.md', 'r').read()
+
+# Setting up
+setup(
+    name="pyRetroGame",
+    version=VERSION,
+    author="Lorix & JProgrammer", 
+    author_email="<ciminata08@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'consoleGames', 'games', 'terminalGames', 'console', 'terminal', 'game', 'gameEngine', 'gameEngineLibrary', 'gameEngineLibraryPython'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
