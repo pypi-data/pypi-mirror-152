81efc20279b0c6864cb30f8a07da661f81ad6fb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:40 +0000

    Add package colorprt/2.0.0/colorprt-2.0.0.tar.gz

---
+++ packages/colorprt/colorprt-2.0.0.tar.gz/colorprt-2.0.0/test/test.py
@@ -0,0 +1,13 @@
+from colorprt import colorstr, ColorprtConfig, Fore, Mode, Back
+
+lang_output_config = ColorprtConfig(Fore.BLUE, Back.YELLOW)
+a = colorstr('asdfasdf', config=lang_output_config)
+print(a)
+
+pycolor_config = ColorprtConfig(Back.RED, foreground=Fore.DEFAULT)
+pycolor_config("I love You!!", end="\n\n")
+
+pycolor_config = ColorprtConfig(Back.DEFAULT, Fore.RED, Mode.BOLD)
+pycolor_config("I hate You!!", end="")
+pass
+
