bd15e442cae10801954c1913031ce9d840618fcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:05 +0000

    Add package je-web-runner/0.0.11/je_web_runner-0.0.11-py3-none-any.whl

---
+++ packages/je-web-runner/je_web_runner-0.0.11-py3-none-any.whl/je_web_runner/selenium_utils_wrapper/desired_capabilities/desired_capabilities.py
@@ -0,0 +1,26 @@
+from selenium.webdriver import DesiredCapabilities
+
+from je_web_runner.utils.exception.exceptions import WebDriverException
+from je_web_runner.utils.exception.exception_tag import selenium_wrapper_web_driver_not_found_error
+
+
+desired_capabilities_dict = {
+    "firefox": DesiredCapabilities.FIREFOX,
+    "chrome": DesiredCapabilities.CHROME,
+    "edge": DesiredCapabilities.EDGE,
+    "opera": DesiredCapabilities.OPERA,
+    "safari": DesiredCapabilities.SAFARI,
+}
+
+
+def get_desired_capabilities_keys():
+    return desired_capabilities_dict.keys()
+
+
+def get_desired_capabilities(webdriver_name: str):
+    desired_capabilities = desired_capabilities_dict.get(webdriver_name)
+    if desired_capabilities is None:
+        raise WebDriverException(selenium_wrapper_web_driver_not_found_error)
+    return desired_capabilities.copy()
+
+
