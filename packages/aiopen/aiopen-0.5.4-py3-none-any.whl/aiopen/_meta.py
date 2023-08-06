6e2450b7016fee3c28cc5a6dc0ca361b5adfb728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:59 +0000

    Add package aiopen/0.5.4/aiopen-0.5.4-py3-none-any.whl

---
+++ packages/aiopen/aiopen-0.5.4-py3-none-any.whl/aiopen/_meta.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Package metadata/info"""
+
+__title__ = "aiopen"
+__description__ = "Async file io"
+__pkgroot__ = __file__.replace("_meta.py", "").rstrip("/\\")
+__version__ = "0.5.4"
