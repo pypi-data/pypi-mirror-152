0f1e23edc05d1f85dc06f859307d5c1923832f85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:53 +0000

    Add package colorizing-tool-uncolor/1.0.1/colorizing_tool_uncolor-1.0.1.tar.gz

---
+++ packages/colorizing-tool-uncolor/colorizing_tool_uncolor-1.0.1.tar.gz/colorizing_tool_uncolor-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'colorizing_tool_uncolor',
+    packages = ['uncolor'],
+    version = '1.0.1',
+    description = '🎨 | Uncomplicated colorizing tool',
+    author = 'Gátomo',
+    author_email='dontSendMeAnEmail@thanks.com',
+    url = 'https://github.com/gatomo-oficial/uncolor',
+    download_url = 'https://github.com/gatomo-oficial/uncolor/archive/refs/tags/1.0.tar.gz',
+    keywords = ['Terminals','Color', 'Colorize', 'Uncolor', 'Styling'],
+    classifiers = [],
+)
