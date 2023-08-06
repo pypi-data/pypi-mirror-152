3c2c6b65de9193e8aee7cca38d2273a35710d6aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:36 +0000

    Add package kcapi/1.0.21/kcapi-1.0.21-py3-none-any.whl

---
+++ packages/kcapi/kcapi-1.0.21-py3-none-any.whl/kcapi/rest/roles.py
@@ -0,0 +1,8 @@
+from .targets import Targets
+
+
+# The Keycloak guys decided to use another resource DELETE /roles-by-id, instead of sticking to DELETE /roles.
+def RolesURLBuilder(url):
+    targets = Targets.makeWithURL(url)
+    targets.targets['delete'].replaceResource('roles', 'roles-by-id')
+    return targets
