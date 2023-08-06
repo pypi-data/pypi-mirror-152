be66bf07e82d1d7134cf92404efeac62cbb5db37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:55 +0000

    Add package holour/4.0.0/holour-4.0.0.tar.gz

---
+++ packages/holour/holour-4.0.0.tar.gz/holour-4.0.0/holour/logging.py
@@ -0,0 +1,22 @@
+import logging
+import sys
+from typing import List
+
+
+def setup_log(level=logging.DEBUG, handlers: List[logging.Handler] = None) -> None:
+    if not handlers:
+        handlers = []
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(get_format())
+    logging.basicConfig(level=level, handlers=[handler] + handlers)
+
+
+def get_format() -> logging.Formatter:
+    return logging.Formatter('[%(asctime)s] [%(levelname)s] [%(name)s:%(funcName)s] - %(message)s')
+
+
+def get_json_format() -> logging.Formatter:
+    return logging.Formatter(
+        "{'time':'%(asctime)s', 'name': '%(name)s', 'func': '%(funcName)s', 'level': '%(levelname)s', "
+        "'message': '%(message)s'}"
+    )
