c9d8e2d68bfdec0f44cd51172d49bc503a01ee94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:49:57 +0000

    Add package st-card-component-alex/0.0.1/st_card_component_alex-0.0.1-py3-none-any.whl

---
+++ packages/st-card-component-alex/st_card_component_alex-0.0.1-py3-none-any.whl/st_card_component/frontend/build/precache-manifest.9da28e869bd47029a54a1f36a791753f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd78eb369b5b7e42ba8ffdc80838a2f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0bc73a3fa6a884f8afa6",
+    "url": "./static/js/2.cefea37b.chunk.js"
+  },
+  {
+    "revision": "5d057a7058da946466b00c3910263a1b",
+    "url": "./static/js/2.cefea37b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b28bbb35e477119654df",
+    "url": "./static/js/main.7d4bcb1d.chunk.js"
+  },
+  {
+    "revision": "adbcd5fbb40f43b74987",
+    "url": "./static/js/runtime-main.c6b6296f.js"
+  }
+]);
