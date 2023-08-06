8937340302164825bf019d0bcabb291705122933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:24:05 +0000

    Add package redis-limiter/0.0.2/redis-limiter-0.0.2.tar.gz

---
+++ packages/redis-limiter/redis-limiter-0.0.2.tar.gz/redis-limiter-0.0.2/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#-*- coding: utf8 -*-
+
+from setuptools import find_packages, setup
+
+VERSION = '0.0.2'
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='redis-limiter',
+    version=VERSION,
+    author='bao xie',
+    description='A Redis based rate limiter implementation for Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/laobaox/redis-limiter.git',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    entry_points = {
+        'console_scripts': [
+        ],
+    },
+    python_requires='>=2.7.9',
+    install_requires=[
+    ],
+)
