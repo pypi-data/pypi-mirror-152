2589f9750e8ee4346b5d93f556b7e48b832a33eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:46 +0000

    Add package symforce/0.4.0/symforce-0.4.0-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/symforce/symforce-0.4.0-cp310-cp310-macosx_10_9_x86_64.whl/symforce/codegen/__init__.py
@@ -0,0 +1,11 @@
+# ----------------------------------------------------------------------------
+# SymForce - Copyright 2022, Skydio, Inc.
+# This source code is under the Apache 2.0 license found in the LICENSE file.
+# ----------------------------------------------------------------------------
+
+"""
+Package for executable code generation from symbolic expressions.
+"""
+
+from .codegen_config import CodegenConfig, CppConfig, PythonConfig
+from .codegen import Codegen, LinearizationMode, GeneratedPaths
