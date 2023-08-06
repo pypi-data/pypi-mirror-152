112e9f883a94ebbfdeaa12e703a0a579037ac289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:36 +0000

    Add package git-mr-linear/0.0.4a0/git-mr-linear-0.0.4a0.tar.gz

---
+++ packages/git-mr-linear/git-mr-linear-0.0.4a0.tar.gz/git-mr-linear-0.0.4a0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="git-mr-linear",
+    version="0.0.4a",
+    author="Donghyeon Kim",
+    author_email="dhygns@gmail.com",
+    description="A command line utility to list and merge gitlab merge requests while maintaining linear history",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/dhpersonal/git-mr-linear",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
