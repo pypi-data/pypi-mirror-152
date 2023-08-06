3f01f337b8f6821758ffb799c26ab6c79aecbc26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:07 +0000

    Add package botoolkit/1.0.0/botoolkit-1.0.0-py3-none-any.whl

---
+++ packages/botoolkit/botoolkit-1.0.0-py3-none-any.whl/botoolkit/bo_jira/settings.py
@@ -0,0 +1,20 @@
+from pathlib import (
+    Path,
+)
+
+
+TOOL_NAME = NAMESPACE = 'bojira'
+
+TEMPLATE_CONF_FILE_PATH = (
+    Path(__file__).parent.absolute() / 'templates' / f'{TOOL_NAME}.conf'
+)
+
+CONSOLE_SCRIPTS = [
+    f'{TOOL_NAME} = botoolkit.bo_jira.main:main',
+]
+
+NAMESPACES = {
+    f'{NAMESPACE}': [
+        'configure = botoolkit.bo_jira.commands:ConfigureBOJiraCommand',
+    ],
+}
