fb241f5fad146be56ca5ce955a2576addb8fd9e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:10 +0000

    Add package persuader-technology-automata-missing-repository/0.0.8/persuader_technology_automata_missing_repository-0.0.8-py3-none-any.whl

---
+++ packages/persuader-technology-automata-missing-repository/persuader_technology_automata_missing_repository-0.0.8-py3-none-any.whl/missingrepo/Missing.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+from core.market.Market import Market
+from core.missing.Context import Context
+
+
+@dataclass
+class Missing:
+    missing: str
+    context: Context
+    market: str
+    description: str
+
+    def __eq__(self, other):
+        return f'{self.missing}{self.context.value}{self.market}' == f'{other.missing}{other.context.value}{other.market}'
