bbe7bda189c18aad6dcf09185d0b1a32739fbe88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:49:59 +0000

    Add package rafaycalculator/0.0.1/rafaycalculator-0.0.1.tar.gz

---
+++ packages/rafaycalculator/rafaycalculator-0.0.1.tar.gz/rafaycalculator-0.0.1/README.txt
@@ -0,0 +1,3 @@
+This is a very simple calculator
+which does:
+add , subtract , divide , multiply , square , cube
---
+++ packages/rafaycalculator/rafaycalculator-0.0.1.tar.gz/rafaycalculator-0.0.1/rafaycalculator/__init__.py
@@ -0,0 +1,17 @@
+def square(num1):
+    return num1**2
+
+def plus(num1 , num2):
+    return num1+num2
+
+def minus(num1 , num2):
+    return num1-num2
+
+def cube(num1):
+    return num1**3
+
+def divide(num1 , num2):
+    return num1/num2
+
+def multiply(num1 , num2):
+    return num1*num2
