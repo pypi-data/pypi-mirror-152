148012c29747f7e2423534ffa3f53228b673a4a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:52 +0000

    Add package cenroctest/0.0.5/cenROCTest-0.0.5.tar.gz

---
+++ packages/cenroctest/cenROCTest-0.0.5.tar.gz/cenROCTest-0.0.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.5'
+DESCRIPTION = 'Survival Package creating cenROC object'
+LONG_DESCRIPTION = 'A package that allows to calculate survival cutpoints based on times and events'
+
+# Setting up
+setup(
+    name="cenROCTest",
+    version=VERSION,
+    author="Yury Moskaltsov",
+    author_email="yury-m@hotmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['numpy', 'pandas', 'matplotlib', 'scipy','statsmodels'],
+    keywords=['python', 'cenROC', 'survival'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
