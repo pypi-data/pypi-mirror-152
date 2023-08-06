ad97325d889ec96a0307a09e8df0367b5ef04220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:18:18 +0000

    Add package numpatt-sequence/0.0.8/numpatt_sequence-0.0.8-py3-none-any.whl

---
+++ packages/numpatt-sequence/numpatt_sequence-0.0.8-py3-none-any.whl/numpatterns/__init__.py
@@ -0,0 +1,5 @@
+# import module
+from numpatterns.numpatt import convert_str_float
+from numpatterns.numpatt import classify_pattern
+from numpatterns.numpatt import common_difference
+from numpatterns.numpatt import multi_difference
