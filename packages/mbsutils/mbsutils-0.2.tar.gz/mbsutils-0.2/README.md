793c52ef9bee244ef4601bbe4815a69ee4cae567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:20 +0000

    Add package mbsutils/0.2/mbsutils-0.2.tar.gz

---
+++ packages/mbsutils/mbsutils-0.2.tar.gz/mbsutils-0.2/README.md
@@ -0,0 +1,3 @@
+# MBS
+
+Simple Python library for loading and plotting MB Scientific ARPES detector data in both KRX and TXT format.
---
+++ packages/mbsutils/mbsutils-0.2.tar.gz/mbsutils-0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='mbsutils',
+      version='0.2',
+      description='Utilities for MB Scientific analyzer data',
+      url='http://github.com/mueslo/mbs',
+      author='mueslo',
+      author_email='mueslo@mueslo.de',
+      license='GPLv3',
+      packages=['mbs'],
+      install_requires=[
+          'numpy',
+          'scipy',
+          'matplotlib~=3.4',
+          'pandas',
+      ],
+      extras_require={
+          'xarray': ["xarray"],
+          'widgets': ["jupyterlab~=3.2", "ipympl~=0.8", "ipywidgets~=7.6"],
+          'docs': ["sphinx~=4.5", "sphinx-rtd-theme"],
+          'tests': ["tox"],
+      },
+      zip_safe=False)
