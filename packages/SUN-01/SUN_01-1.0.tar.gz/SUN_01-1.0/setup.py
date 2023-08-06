ced87ac85119cf926b1d564f360604b33ebce1a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:20:01 +0000

    Add package SUN-01/1.0/SUN_01-1.0.tar.gz

---
+++ packages/SUN-01/SUN_01-1.0.tar.gz/SUN_01-1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages,setup
+setup(
+    name = 'SUN_01',#模块名
+    version = '1.0', #版本
+    packages = find_packages(exclude=[]), #目录所有文件
+    url='https://xxx.com', #文件文档下载地址
+    author='PY3021', #作者名
+    author_email='111@qq.com', #邮箱
+    install_requires=['requests>=2.25.0',
+                      'lxml','pikepdf','tkinter','PIL','ttkbootstrap'
+                      ],
+    python_requires=">=3.6",
+
+)
+
+# python setup.py build sdist bdist_wheel  #这个是打包到dist文件夹
+# twine upload dist/*  #这个是上传dist到pypi.org
