2d5241b6b8c8eb45cef0221b8293ec0d20aa8915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:09 +0000

    Add package sinta-scraper/0.23.1/sinta_scraper-0.23.1-py3-none-any.whl

---
+++ packages/sinta-scraper/sinta_scraper-0.23.1-py3-none-any.whl/sinta_scraper/scholar_documents_scraper.py
@@ -0,0 +1,97 @@
+from concurrent.futures import ThreadPoolExecutor
+
+from bs4 import BeautifulSoup
+from requests import get
+from string_utils.validation import is_integer
+
+import utils
+from sinta_scraper.dept_scraper import dept_authors
+from utils.config import get_config
+
+
+def author_scholar_docs(author_id, output_format='dictionary', pretty_print=None, xml_library='dicttoxml',
+                        min_year=None, max_year=None, max_workers=None):
+    domain = get_config()['domain']
+    url = f'{domain}/authors/detail?id={author_id}&view=documentsgs'
+    html = get(url)
+    soup = BeautifulSoup(html.content, 'html.parser')
+    page_info = soup.select('.uk-width-large-1-2.table-footer')
+    n_page = utils.cast(page_info[0].text.strip().split()[3])
+    worker_result = parse(soup)
+
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        for page in range(2, n_page + 1):
+            executor.submit(worker, author_id, page, worker_result)
+
+    if min_year is not None:
+        worker_result = [doc for doc in worker_result if is_integer(str(doc['year'])) and doc['year'] >= min_year]
+
+    if max_year is not None:
+        worker_result = [doc for doc in worker_result if is_integer(str(doc['year'])) and doc['year'] <= max_year]
+
+    return utils.format_output(worker_result, output_format, pretty_print, xml_library)
+
+
+def worker(author_id, page, worker_result):
+    domain = get_config()['domain']
+    url = f'{domain}/authors/detail?page={page}&id={author_id}&view=documentsgs'
+    html = get(url)
+    soup = BeautifulSoup(html.content, 'html.parser')
+    data = parse(soup)
+
+    worker_result.extend(data)
+
+
+def parse(soup):
+    rows = soup.select('table.uk-table tr')
+    result = []
+
+    for row in rows:
+        link = row.select('a.paper-link')
+
+        if not link:
+            continue
+
+        link = link[0]
+        authors = row.select('dd')[0].text.split(', ')
+        info = row.select('dd.indexed-by')[0].text.strip().split('|')
+        citations = row.select('.index-val')[1].text.strip()
+
+        result.append({
+            'title': link.text,
+            'authors': authors,
+            'url': link['href'],
+            'publisher': info[0].strip(),
+            'year': utils.cast(info[3].strip()),
+            'citations': utils.cast(citations) if is_integer(citations) else 0
+        })
+
+    return result
+
+
+def dept_scholar_docs(dept_ids, affil_id, output_format='dictionary', pretty_print=None, xml_library='dicttoxml',
+                      min_year=None, max_year=None, max_workers=None):
+    if type(dept_ids) is not list and type(dept_ids) is not tuple:
+        dept_ids = [dept_ids]
+
+    authors = []
+    worker_result = []
+
+    for dept_id in dept_ids:
+        authors.extend(dept_authors(dept_id, affil_id))
+
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        for author in authors:
+            executor.submit(dept_scholar_docs_worker, author['id'], min_year, max_year, worker_result)
+
+    return utils.format_output(worker_result, output_format, pretty_print, xml_library)
+
+
+def dept_scholar_docs_worker(author_id, min_year, max_year, worker_result):
+    docs = author_scholar_docs(author_id, min_year=min_year, max_year=max_year)
+
+    worker_result.extend(docs)
+
+
+if __name__ == '__main__':
+    print(author_scholar_docs(6689329, output_format='json', pretty_print=True))
