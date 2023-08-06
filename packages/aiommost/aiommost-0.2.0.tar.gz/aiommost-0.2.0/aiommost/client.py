726555a8d6c17fd18179c93705139cba3af4e5de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:30 +0000

    Add package aiommost/0.2.0/aiommost-0.2.0.tar.gz

---
+++ packages/aiommost/aiommost-0.2.0.tar.gz/aiommost-0.2.0/aiommost/client.py
@@ -0,0 +1,26 @@
+import httpx
+
+from aiommost.channels.client import ChannelsClient
+from aiommost.posts.client import PostClient
+from aiommost.users.client import UsersClient
+
+
+class MattermostClient:
+
+    def __init__(self, host: str, token: str) -> None:
+        self.token = token
+
+        self.session = httpx.AsyncClient(
+            base_url=f'{host}/api/v4',
+            headers={
+                'Authorization': f'Bearer {token}',
+                'Content-Type': 'application/json',
+            },
+        )
+
+        self.channels = ChannelsClient(self.session)
+        self.posts = PostClient(self.session)
+        self.users = UsersClient(self.session)
+
+    async def close(self) -> None:
+        await self.session.aclose()
