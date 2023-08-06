853b7a2077fecae2eb0b12eabdb9f26185758018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:57 +0000

    Add package pyclavis/0.0.7/pyclavis-0.0.7-py3-none-any.whl

---
+++ packages/pyclavis/pyclavis-0.0.7-py3-none-any.whl/pyclavis/pyclavis.py
@@ -0,0 +1,25 @@
+from .credit import Credential
+from .store import CredentialStore
+from .backend import (
+    FileBackend,
+    XorCryptedFileBackend,
+    CryptedFileBackend,
+    DEFAULT_STORE,
+)
+from .conv import Conv, ConvTabCsv, ConvJson
+from .keyconv import KeyBase64Conv
+from .loginproc import LoginParam, Autofill, send_seq, DELAY_STD
+
+
+class Formatter(object):
+    def to2line(self, c):
+        return c.user + "\n" + c.passwd
+
+    def to2tuple(self, c):
+        return c.user, c.passwd
+
+    def to2tab(self, c):
+        return c.user + "\t" + c.passwd
+
+
+VERSION = "v0.0.7"
