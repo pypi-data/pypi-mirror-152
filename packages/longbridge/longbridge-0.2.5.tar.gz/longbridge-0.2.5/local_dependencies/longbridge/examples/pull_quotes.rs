2553102155ecb3d4892e4868c6c42f4304b1d331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:27 +0000

    Add package longbridge/0.2.5/longbridge-0.2.5.tar.gz

---
+++ packages/longbridge/longbridge-0.2.5.tar.gz/longbridge-0.2.5/local_dependencies/longbridge/examples/pull_quotes.rs
@@ -0,0 +1,21 @@
+use std::sync::Arc;
+
+use anyhow::Result;
+use longbridge::{Config, QuoteContext};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Load configuration from environment variables
+    let config = Arc::new(Config::from_env()?);
+
+    // Create a context for quote APIs
+    let (ctx, _) = QuoteContext::try_new(config).await?;
+
+    // Get basic information of securities
+    let resp = ctx
+        .quote(["700.HK", "AAPL.US", "TSLA.US", "NFLX.US"])
+        .await?;
+    println!("{:?}", resp);
+
+    Ok(())
+}
