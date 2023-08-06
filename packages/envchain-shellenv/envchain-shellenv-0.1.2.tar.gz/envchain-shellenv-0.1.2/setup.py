c92850133748f0e17fddf4c89e0c7b6b811c1f3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:05 +0000

    Add package envchain-shellenv/0.1.2/envchain-shellenv-0.1.2.tar.gz

---
+++ packages/envchain-shellenv/envchain-shellenv-0.1.2.tar.gz/envchain-shellenv-0.1.2/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['envchain_shellenv']
+
+package_data = \
+{'': ['*'], 'envchain_shellenv': ['examples/*']}
+
+install_requires = \
+['keyring[keyring]>=23.5.1,<24.0.0']
+
+entry_points = \
+{'console_scripts': ['eesh = envchain_shellenv.cli:main_sync',
+                     'envchain-shellenv = envchain_shellenv.cli:main_sync']}
+
+setup_kwargs = {
+    'name': 'envchain-shellenv',
+    'version': '0.1.2',
+    'description': 'envchain shellenv - prints export statements for your secrets in the keychain',
+    'long_description': '',
+    'author': 'Xinyuan Chen',
+    'author_email': '45612704+tddschn@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/tddschn/envchain-shellenv',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
