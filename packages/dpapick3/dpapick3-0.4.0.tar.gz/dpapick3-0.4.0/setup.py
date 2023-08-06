7a51604657cad6c19b80b572ac381d55252ca439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:45 +0000

    Add package dpapick3/0.4.0/dpapick3-0.4.0.tar.gz

---
+++ packages/dpapick3/dpapick3-0.4.0.tar.gz/dpapick3-0.4.0/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dpapick3",
+    version="0.4.0",
+    author="Tijl Deneut",
+    author_email="deneut_tijl@hotmail.com",
+    description="A native implementation of DPAPI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tijldeneut/dpapick3",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.2',
+    install_requires=[
+        'pycryptodome',
+        'python-registry',
+        'pyasn1'
+    ]
+)
