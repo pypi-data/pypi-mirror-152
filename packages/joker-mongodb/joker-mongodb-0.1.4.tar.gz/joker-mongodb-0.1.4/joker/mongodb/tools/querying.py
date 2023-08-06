dcc5260f3c1843796df17b90d935f9ff09d63127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:10 +0000

    Add package joker-mongodb/0.1.4/joker-mongodb-0.1.4.tar.gz

---
+++ packages/joker-mongodb/joker-mongodb-0.1.4.tar.gz/joker-mongodb-0.1.4/joker/mongodb/tools/querying.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from pymongo.collection import Collection
+
+
+def _namemap_to_project(namemap: dict):
+    project = {}
+    for new_name, old_name in namemap.items():
+        if not old_name.startswith('$'):
+            old_name = '$' + old_name
+        project[new_name] = {'$ifNull': [old_name, None]}
+    return project
+
+
+def _namemap_from_fieldlist(fieldlist: list):
+    return {k.split('.')[-1]: k for k in fieldlist}
+
+
+def find_with_renaming(
+        coll: Collection, filtr: dict, namemap: dict, sort: dict = None):
+    pipelines = [
+        {'$match': filtr},
+        {'$sort': sort or {'_id': -1}},
+        {'$project': _namemap_to_project(namemap)},
+    ]
+    return coll.aggregate(pipelines)
+
+
+def find_one_with_renaming(
+        coll: Collection, filtr: dict, namemap: dict, sort: dict = None):
+    pipelines = [
+        {'$match': filtr},
+        {'$sort': sort or {'_id': -1}},
+        {'$limit': 1},
+        {'$project': _namemap_to_project(namemap)},
+    ]
+    docs = list(coll.aggregate(pipelines))
+    if docs:
+        return docs[0]
