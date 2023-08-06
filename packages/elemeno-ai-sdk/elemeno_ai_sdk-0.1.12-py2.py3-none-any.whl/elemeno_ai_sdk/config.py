897eb38db8c82974a55b09f28f7eb4169ecd1004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:06 +0000

    Add package elemeno-ai-sdk/0.1.12/elemeno_ai_sdk-0.1.12-py2.py3-none-any.whl

---
+++ packages/elemeno-ai-sdk/elemeno_ai_sdk-0.1.12-py2.py3-none-any.whl/elemeno_ai_sdk/config.py
@@ -0,0 +1,26 @@
+import logging
+from omegaconf import OmegaConf
+import os
+
+class Configs:
+    _instance = None
+    _props = None
+
+    def __init__(self):
+        raise RuntimeError("Call instance() instead")
+
+    @classmethod
+    def instance(cls):
+        cfg_path = os.getenv('ELEMENO_CFG_PATH', 'elemeno.yaml')
+        try:
+            if cls._instance is None:
+                cls._instance = cls.__new__(cls)
+                cls._props = OmegaConf.load(cfg_path)
+            return cls._instance.props
+        except:
+            logging.warn("Couldn't find a config file at %s, will continue without loading it", cfg_path)
+            return None
+
+    @property
+    def props(self):
+        return self._props
