ecf5215505a78fc013ef1676d77af45a090ebbde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:50 +0000

    Add package get-response/0.1.1/get_response-0.1.1.tar.gz

---
+++ packages/get-response/get_response-0.1.1.tar.gz/get_response-0.1.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+
+VERSION = '0.1.1'
+NAME = 'get_response'
+AUTHOR = 'skv0zsneg'
+DESCRIPTION = ("Parsing Tool for json-like, xml-like and etc Types of Response"
+               "for Humans.")
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+
+setuptools.setup(
+    name=NAME,
+    version=VERSION,
+    author=AUTHOR,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/skv0zsneg/get-response",
+    project_urls={
+        "Bug Tracker": "https://github.com/skv0zsneg/get-response/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "get_response"},
+    packages=setuptools.find_packages(where="get_response"),
+    python_requires=">=3.8",
+)
