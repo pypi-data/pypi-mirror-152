4829061e98dcffa59da8cd8d92b5861ab835e5d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:07 +0000

    Add package niit4/1.1/niit4-1.01.tar.gz

---
+++ packages/niit4/niit4-1.01.tar.gz/niit4-1.01/setup.py
@@ -0,0 +1,9 @@
+from  distutils.core import setup
+setup(
+    name='niit4',
+    version='1.01',
+    description='这是一个测试包，仅仅测试使用',
+    author='niit4_all',
+    author_email='152254455@qq.com',
+    py_modules=['niit4.student']
+)
