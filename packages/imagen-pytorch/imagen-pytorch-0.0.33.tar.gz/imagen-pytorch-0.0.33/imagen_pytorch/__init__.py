f5d55f0133cfe92c3af4d31a5e22461d803a16f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:47 +0000

    Add package imagen-pytorch/0.0.33/imagen-pytorch-0.0.33.tar.gz

---
+++ packages/imagen-pytorch/imagen-pytorch-0.0.33.tar.gz/imagen-pytorch-0.0.33/imagen_pytorch/__init__.py
@@ -0,0 +1,2 @@
+from imagen_pytorch.imagen_pytorch import Imagen, Unet
+from imagen_pytorch.trainer import ImagenTrainer
---
+++ packages/imagen-pytorch/imagen-pytorch-0.0.33.tar.gz/imagen-pytorch-0.0.33/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'imagen-pytorch',
+  packages = find_packages(exclude=[]),
+  version = '0.0.33',
+  license='MIT',
+  description = 'Imagen - unprecedented photorealism × deep level of language understanding',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/lucidrains/imagen-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'transformers',
+    'text-to-image',
+    'denoising-diffusion'
+  ],
+  install_requires=[
+    'click',
+    'einops>=0.4',
+    'einops-exts',
+    'numpy',
+    'pillow',
+    'pydantic',
+    'resize-right',
+    'sentencepiece',
+    'torch>=1.6',
+    'torchvision',
+    'transformers',
+    'tqdm'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
