91532d6fc323d0c00dd19703bc9562e5398636ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:10 +0000

    Add package eth-heimdall/1.0.3b4/eth_heimdall-1.0.3b4.tar.gz

---
+++ packages/eth-heimdall/eth_heimdall-1.0.3b4.tar.gz/eth_heimdall-1.0.3b4/heimdall/lib/config.py
@@ -0,0 +1,39 @@
+import json
+import pickle
+import pathlib
+
+from .utils.io import write, loadFileAsJson
+
+def getConfig():
+  try:
+    return loadFileAsJson(f'{pathlib.Path(__file__).parent.parent.resolve()}/env/conf.json')
+  except:
+    # config file is missing, create it
+    conf = {
+      "build": {
+        "version": "v1.0.3-stable"
+      },
+      "autoupdate": False,
+      "defaults": {
+
+        "providers": {
+          "local": "http://127.0.0.1:7545",
+          "remote": "https://mainnet.infura.io/v3/"
+        }
+
+      },
+
+      "environment": {
+        "apis": {
+          "etherscan": ""
+        }
+      }
+    }
+    
+    with open(getConfigPath(), 'w') as outfile:
+      json.dump(conf, outfile)
+    return conf
+
+# get the config file path, because it can be hidden in pypi directories
+def getConfigPath():
+  return f'{pathlib.Path(__file__).parent.parent.resolve()}/env/conf.json'
