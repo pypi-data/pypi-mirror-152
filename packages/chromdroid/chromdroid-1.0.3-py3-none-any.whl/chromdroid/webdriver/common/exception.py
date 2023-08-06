c09044ff7e570dca67a06bf96f3e21ecaaa7e900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:15 +0000

    Add package chromdroid/1.0.3/chromdroid-1.0.3-py3-none-any.whl

---
+++ packages/chromdroid/chromdroid-1.0.3-py3-none-any.whl/chromdroid/webdriver/common/exception.py
@@ -0,0 +1,18 @@
+class WebDriverException(Exception):
+    pass
+
+
+class NoSuchElementException(WebDriverException):
+    pass
+
+
+class NoSuchPageSourceException(WebDriverException):
+    pass
+
+
+class InvalidElementStateException(WebDriverException):
+    pass
+
+
+class ApplicationClosed(WebDriverException):
+    pass
