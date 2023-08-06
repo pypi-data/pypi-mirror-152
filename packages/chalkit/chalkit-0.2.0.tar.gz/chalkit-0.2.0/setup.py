a016995391217d5480ad625cced5958abfeae9f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:10 +0000

    Add package chalkit/0.2.0/chalkit-0.2.0.tar.gz

---
+++ packages/chalkit/chalkit-0.2.0.tar.gz/chalkit-0.2.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['chalkit']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['typer[all]>=0.4.1,<0.5.0']
+
+entry_points = \
+{'console_scripts': ['chalkit = chalkit.main:app']}
+
+setup_kwargs = {
+    'name': 'chalkit',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': '# CLI to save ideas/todos by just opening your terminal.\n',
+    'author': 'Anurag-gg',
+    'author_email': 'dsanurag520@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
