54972cb8386ba5afe1b911ab185111a6e609e33a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:05 +0000

    Add package quark-engine/22.5.1/quark-engine-22.5.1.tar.gz

---
+++ packages/quark-engine/quark-engine-22.5.1.tar.gz/quark-engine-22.5.1/quark/utils/tools.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# This file is part of Quark-Engine - https://github.com/quark-engine/quark-engine
+# See the file 'LICENSE' for copying permission.
+
+import copy
+import re
+
+
+def remove_dup_list(element):
+    """
+    Remove the duplicate elements in  given list.
+    """
+    return list(set(element))
+
+
+def contains(subset_to_check, target_list):
+    """
+    Check the sequence pattern within two list.
+    -----------------------------------------------------------------
+    subset_to_check = ["getCellLocation", "sendTextMessage"]
+    target_list = ["put", "getCellLocation", "query", "sendTextMessage"]
+    then it will return true.
+    -----------------------------------------------------------------
+    subset_to_check = ["getCellLocation", "sendTextMessage"]
+    target_list = ["sendTextMessage", "put", "getCellLocation", "query"]
+    then it will return False.
+    """
+
+    target_copy = copy.copy(target_list)
+
+    # Delete elements that do not exist in the subset_to_check list
+    for item in target_copy:
+        if item not in subset_to_check:
+            target_copy.remove(item)
+
+    for i in range(len(target_copy) - len(subset_to_check) + 1):
+        for j in range(len(subset_to_check)):
+            if target_copy[i + j] != subset_to_check[j]:
+                break
+        else:
+            return True
+    return False
+
+
+def descriptor_to_androguard_format(descriptor):
+    if "(" not in descriptor or ")" not in descriptor:
+        raise ValueError(f"Invalid descriptor. {descriptor}")
+
+    delimiter = descriptor.index(")")
+
+    arg_str = descriptor[:delimiter]
+    args = re.findall(r"L.+?;|[ZBCSIJFD]|\[", arg_str)
+
+    new_descriptor = "(" + " ".join(args) + descriptor[delimiter:]
+    new_descriptor = re.sub(r"\[ ", "[", new_descriptor)
+
+    return new_descriptor
