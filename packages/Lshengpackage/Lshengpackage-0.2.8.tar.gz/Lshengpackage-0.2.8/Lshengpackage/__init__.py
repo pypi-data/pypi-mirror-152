182afed4062c12d83de220eaba4acc2cbca6e502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:50:04 +0000

    Add package Lshengpackage/0.2.8/Lshengpackage-0.2.8.tar.gz

---
+++ packages/Lshengpackage/Lshengpackage-0.2.8.tar.gz/Lshengpackage-0.2.8/Lshengpackage/__init__.py
@@ -0,0 +1,8 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+
+from Lshengpackage.Command_adb import command
+from Lshengpackage.Pac_adb import Pic, Mouse
+from Lshengpackage.Reg import RegDm
+from Lshengpackage.chaojiying import Verification
