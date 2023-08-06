ffc9156a34365074812d761e62aa560c833f67b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:49 +0000

    Add package replicate/0.0.1a9/replicate-0.0.1a9.tar.gz

---
+++ packages/replicate/replicate-0.0.1a9.tar.gz/replicate-0.0.1a9/setup.py
@@ -0,0 +1,24 @@
+# !/usr/bin/env python
+
+from setuptools import setup
+
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+setup(
+    name="replicate",
+    packages=["replicate"],
+    version="0.0.1a9",
+    description="Python client for Replicate",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Replicate, Inc.",
+    license="BSD",
+    url="https://github.com/replicate/replicate-python",
+    python_requires=">=3.6",
+    install_requires=["requests", "pydantic"],
+    classifiers=[],
+)
