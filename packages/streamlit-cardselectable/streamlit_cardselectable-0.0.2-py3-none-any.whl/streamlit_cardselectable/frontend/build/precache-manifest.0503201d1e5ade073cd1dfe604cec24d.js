75e8cf2f52d608985210db9bd0276c609c8be703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:20 +0000

    Add package streamlit-cardselectable/0.0.2/streamlit_cardselectable-0.0.2-py3-none-any.whl

---
+++ packages/streamlit-cardselectable/streamlit_cardselectable-0.0.2-py3-none-any.whl/streamlit_cardselectable/frontend/build/precache-manifest.0503201d1e5ade073cd1dfe604cec24d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7933c9337dd7a64cf34b3bdcf08ac17",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6d66b7c93d9e39adfab4",
+    "url": "./static/js/2.6d5d4386.chunk.js"
+  },
+  {
+    "revision": "a8638117c4368da0221d727ca4894c97",
+    "url": "./static/js/2.6d5d4386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f64e668a17056139b1e4",
+    "url": "./static/js/main.8c9573d2.chunk.js"
+  },
+  {
+    "revision": "e97faeef50a5ef0b3c35",
+    "url": "./static/js/runtime-main.ef4c9d3e.js"
+  }
+]);
