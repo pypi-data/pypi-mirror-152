760b66109412b3fedbc13444914133b6a989ec17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:01 +0000

    Add package ast-import-checker/0.1.1/ast_import_checker-0.1.1.tar.gz

---
+++ packages/ast-import-checker/ast_import_checker-0.1.1.tar.gz/ast_import_checker-0.1.1/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ast_import_checker',
+ 'ast_import_checker.import_test_5',
+ 'ast_import_checker.import_test_6']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['absolutely_nothing>=0.1,<0.2', 'pytest>=6.0,<7.0']
+
+entry_points = \
+{'console_scripts': ['ast_import_checker = '
+                     'ast_import_checker.import_checker:main',
+                     'import_checker = ast_import_checker.import_checker:main']}
+
+setup_kwargs = {
+    'name': 'ast-import-checker',
+    'version': '0.1.1',
+    'description': 'A small utility for checking python imports via the AST module',
+    'long_description': None,
+    'author': 'Zach Taira',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10.0,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
