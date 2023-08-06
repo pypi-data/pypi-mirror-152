5a71a6c1fa6981aeb9c577863103e7ded5e30e38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:14 +0000

    Add package pycg-producer/0.0.2/pycg-producer-0.0.2.tar.gz

---
+++ packages/pycg-producer/pycg-producer-0.0.2.tar.gz/pycg-producer-0.0.2/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+def get_long_desc():
+    with open("pypi-readme.md", "r") as readme:
+        desc = readme.read()
+
+    return desc
+
+setup(name='pycg-producer',
+      version='0.0.2',
+      license='Apache Software License',
+      long_description=get_long_desc(),
+      long_description_content_type='text/markdown',
+      description='Call Graph Producer for PyPI Packages with the use of PyCG ',
+      author='Georgios-Petros Drosos',
+      author_email='drosos007@gmail.com',
+      url='https://github.com/fasten-project/pypi-tools/tree/main/cg-producer',
+      packages=['pycg_producer'],
+      install_requires=['pycg==0.0.5'],
+     )
