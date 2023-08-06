651faef3c6817bb36e43358d29913968e4a88726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:47 +0000

    Add package listas-anidadas/1.0.0/listas_anidadas-1.0.0.tar.gz

---
+++ packages/listas-anidadas/listas_anidadas-1.0.0.tar.gz/listas_anidadas-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+        name            = 'listas_anidadas',
+        version         = '1.0.0',
+        py_modules      = ['listas_anidadas'],
+        author          = 'Fortinux',
+        author_email    = 'info@fortinux.com',
+        url             = 'https://fortinux.com',
+        description     = 'Función que imprime listas anidadas',
+    )
+
