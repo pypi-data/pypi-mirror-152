ffa63e04b84446c48ac436db09d557df6584e52c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:28 +0000

    Add package pandas-prototype/1.0.2/pandas_prototype-1.0.2.tar.gz

---
+++ packages/pandas-prototype/pandas_prototype-1.0.2.tar.gz/pandas_prototype-1.0.2/pandas_prototype/debug.py
@@ -0,0 +1,25 @@
+from pandas_prototype.data_frame import DataFrame
+data = DataFrame(
+    [
+        {
+            "id": 1,
+            "body": "Hello wolrdEnim sunt ex cupidatat occaecat eiusmod aute .",
+            "uuid": "ghjk3878j321134",
+            "color": "red",
+        },
+        {
+            "id": 2,
+            "body": "Ex aute adipisicing esse do excepteur dolore. ",
+            "uuid": "eihr8rhre8013nqfwe-9u",
+        },
+        {
+            "id": 3,
+            "body": "Elit consectetur nisi esse fugiat anim irure.",
+            "uuid": "b08349m320m=-081",
+        },
+    ] ,
+    ["id" , "data"] , indexed=False
+)
+
+print(data)
+
