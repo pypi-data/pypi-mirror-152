6a8fb46c0b3ddb4e3a58fd8b617b659bfdd37d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:22 +0000

    Add package pypyr/5.5.0/pypyr-5.5.0-py3-none-any.whl

---
+++ packages/pypyr/pypyr-5.5.0-py3-none-any.whl/pypyr/version.py
@@ -0,0 +1,11 @@
+"""Version information."""
+
+import platform
+
+__version__ = '5.5.0'
+
+
+def get_version():
+    """Return package-name __version__ python python_version."""
+    return (f'pypyr {__version__} '
+            f'python {platform.python_version()}')
