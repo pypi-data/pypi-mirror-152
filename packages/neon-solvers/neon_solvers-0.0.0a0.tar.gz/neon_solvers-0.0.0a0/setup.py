df19f1470a6a2462706a0a7e136ac9c5e5540f3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:08 +0000

    Add package neon-solvers/0.0.0a0/neon_solvers-0.0.0a0.tar.gz

---
+++ packages/neon-solvers/neon_solvers-0.0.0a0.tar.gz/neon_solvers-0.0.0a0/setup.py
@@ -0,0 +1,34 @@
+import os
+
+from setuptools import setup
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def required(requirements_file):
+    """ Read requirements file and remove comments and empty lines. """
+    with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
+        requirements = f.read().splitlines()
+        if 'MYCROFT_LOOSE_REQUIREMENTS' in os.environ:
+            print('USING LOOSE REQUIREMENTS!')
+            requirements = [r.replace('==', '>=').replace('~=', '>=') for r in requirements]
+        return [pkg for pkg in requirements
+                if pkg.strip() and not pkg.startswith("#")]
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='neon_solvers',
+    version='0.0.0a0',
+    packages=['neon_solvers'],
+    url='https://github.com/NeonGeckoCom/neon_solvers',
+    license='bsd3',
+    install_requires=required("requirements/requirements.txt"),
+    author='Neongecko',
+    author_email='developers@neon.ai',
+    description='neon question solver plugin framework',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
