442c0f0caa023b545e8ad06460a1301f8c886dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:41 +0000

    Add package Amino.ed/2.7.4.1/Amino.ed-2.7.4.1.tar.gz

---
+++ packages/Amino.ed/Amino.ed-2.7.4.1.tar.gz/Amino.ed-2.7.4.1/aminoed/__init__.py
@@ -0,0 +1,46 @@
+__title__ = 'Amino.ed'
+__author__ = 'Alert Aigul'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2020-2022 Alert'
+__version__ = '2.7.4.1'
+
+from asyncio import sleep, create_task, gather
+from asyncio.events import AbstractEventLoop
+from requests import get
+
+from .client import Client
+from .websocket import AminoWebSocket
+
+from .helpers.utils import *
+from .helpers.types import *
+from .helpers.models import *
+from .helpers.exceptions import *
+from .helpers.event import *
+
+
+def run_with_client(
+    ndc_id: str = None, 
+    device_id: str = None,
+    event_loop: AbstractEventLoop = None
+):
+    async def start(loop, callback):
+        async with Client(ndc_id, device_id, loop) as client:
+            await callback(client)
+
+    def _start(callback):
+        loop = event_loop or get_event_loop()
+        loop.run_until_complete(start(loop, callback))
+    return _start
+
+
+def run():
+    def start(callback):
+        loop = get_event_loop()
+        loop.run_until_complete(callback())
+    return start
+
+
+__newest__ = get("https://pypi.python.org/pypi/Amino.ed/json").json()["info"]["version"]
+
+if __version__ != __newest__:
+    print(f"New version available: {__newest__} (Using {__version__})")
