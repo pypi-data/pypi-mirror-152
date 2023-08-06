e891f5b9571dc44f5a2c990da7cadd8dcf219e06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:26 +0000

    Add package epyseg/0.1.28/epyseg-0.1.28-py3-none-any.whl

---
+++ packages/epyseg/epyseg-0.1.28-py3-none-any.whl/epyseg/ta/__main__.py
@@ -0,0 +1,20 @@
+#import argparse # later maybe allow arguments
+from epyseg.ta.GUI.pyta import TissueAnalyzer
+from PyQt5.QtWidgets import QApplication
+import sys
+import multiprocessing
+import platform
+
+if __name__ == '__main__':
+    # mac OSX fix to test
+    # if platform.system() == 'Darwin':
+    #     multiprocessing.set_start_method('spawn')
+    # multiprocessing.set_start_method('fork')
+
+    # run the deep learning GUI
+    # code can be run from the command line using 'python -m epyseg'
+    app = QApplication(sys.argv)
+    w = TissueAnalyzer()
+    w.show()
+    sys.exit(app.exec_())
+
