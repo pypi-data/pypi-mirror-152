bef7391a9d58323cd0bc7513b8894f201204e6bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:16 +0000

    Add package Speedometer/2.9/Speedometer-2.9.tar.gz

---
+++ packages/Speedometer/Speedometer-2.9.tar.gz/Speedometer-2.9/README.rst
@@ -0,0 +1,42 @@
+Speedometer
+===========
+
+*Speedometer is looking for a maintainer*. Please open a ticket on github if you believe you could help maintain this project.
+
+Measure and display the rate of data across a network connection or data being stored in a file.
+
+* `Speedometer Home Page <http://excess.org/speedometer/>`_
+
+
+New Changes
+------------
+
+1. Python 3 is required now, mac os x platform is included.
+
+2. Use python `psutil` module, instead of /dev/net for maximal compatibility.
+
+3. New -d options, support to measure data from running external shell standard output.
+
+   * python speedometer.py -d "ls -lR /"
+
+   * python speedometer.py -d "curl https://download.site/file.iso --output -"
+
+   * python speedometer.py -d "curl https://download.site/file.iso --output - | tee newfile.iso"
+
+4. Support data source from `standard input` or `pipe line`, working only for plain text mode.
+
+   * cat /dev/urandom | python speedometer.py
+
+   * python speedometer.py < /dev/urandom
+
+   * curl --output - https://download.site/file.iso 2>/dev/null | python speedometer.py
+
+
+Screen Shots
+------------
+
+.. image:: http://excess.org/media/speedometer-transp1.png
+   :alt: speedometer screen shot w/ transparent bg
+
+.. image:: http://excess.org/media/speedometer-light16.png
+   :alt: speedometer screen shot w/ multiple graphs
---
+++ packages/Speedometer/Speedometer-2.9.tar.gz/Speedometer-2.9/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+#
+# Urwid setup.py exports the useful bits
+#    Copyright (C) 2004-2010  Ian Ward
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+# Urwid web site: http://excess.org/urwid/
+
+from setuptools import setup
+
+import os
+
+setup_d = {
+    'name': "Speedometer",
+    'version': '2.9',
+    'author': "Ian Ward",
+    'author_email': "ian@excess.org",
+    'url': "http://excess.org/speedometer/",
+    'scripts': ['speedometer.py'],
+    'entry_points': {
+        'console_scripts': ['speedometer = speedometer:console'],},
+    'install_requires': ['urwid >= 0.9.9.1', 'psutil'],
+    'license':"LGPL",
+    'keywords':"network bandwidth monitor system speed download file progress console",
+    'platforms':"Linux",
+    'description':"Console monitor of the rate of data across a network connection or data being stored in a file.",
+    'classifiers':[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Environment :: Console :: Curses",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: System :: Monitoring",
+        "Topic :: System :: Networking :: Monitoring",
+        ],
+     }
+
+
+setup(** setup_d)
