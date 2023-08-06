7ae1b31fce98dbfe333f8af1c246d8870e9d634f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:08 +0000

    Add package rincewindwizzard-example-cli/0.0.4/rincewindwizzard-example-cli-0.0.4.tar.gz

---
+++ packages/rincewindwizzard-example-cli/rincewindwizzard-example-cli-0.0.4.tar.gz/rincewindwizzard-example-cli-0.0.4/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['rincewindwizzard_example_cli']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'rincewindwizzard-example-cli',
+    'version': '0.0.4',
+    'description': '',
+    'long_description': None,
+    'author': 'RincewindWizzard',
+    'author_email': 'git@magierdinge.de',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
