11e0d170341392c7a72a7f9f52c7daad337d9522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:53 +0000

    Add package DataRecorder/2.0.1/DataRecorder-2.0.1.tar.gz

---
+++ packages/DataRecorder/DataRecorder-2.0.1.tar.gz/DataRecorder-2.0.1/DataRecorder/base.py
@@ -0,0 +1,338 @@
+# -*- coding:utf-8 -*-
+from abc import abstractmethod
+from pathlib import Path
+from re import sub
+from threading import Lock
+from typing import Union, Tuple, Any
+
+from openpyxl import load_workbook, Workbook
+from openpyxl.cell import Cell, ReadOnlyCell
+from openpyxl.utils import column_index_from_string
+from openpyxl.utils.cell import coordinate_from_string
+
+from .tools import get_usable_path
+
+
+class BaseRecorder(object):
+    """记录器的父类"""
+    SUPPORTS = ('xlsx', 'csv')
+
+    def __init__(self, path: Union[str, Path] = None, cache_size: int = None) -> None:
+        """初始化                                            \n
+        :param path: 保存的文件路径
+        :param cache_size: 每接收多少条记录写入文件，0为不自动写入
+        """
+        self._data = []
+        self._before = []
+        self._after = []
+        self._type = None
+        self._path = None
+        self._lock = Lock()
+        self._pause_add = False  # 文件写入时暂停接收输入
+
+        if path:
+            self.set_path(path)
+        self.cache_size = cache_size if cache_size is not None else 1000
+        self.encoding: str = 'utf-8'
+        self.delimiter: str = ','  # csv文件分隔符
+        self.quote_char: str = '"'  # csv文件引用符
+
+    def __del__(self) -> None:
+        """对象关闭时把剩下的数据写入文件"""
+        self.record()
+
+    @property
+    def cache_size(self) -> int:
+        """返回缓存大小"""
+        return self._cache
+
+    @cache_size.setter
+    def cache_size(self, cache_size: int) -> None:
+        """设置缓存大小                   \n
+        :param cache_size: 缓存大小
+        :return: None
+        """
+        if not isinstance(cache_size, int) or cache_size < 0:
+            raise TypeError('cache_size值只能是int，且必须>=0')
+        self._cache = cache_size
+
+    @property
+    def path(self) -> str:
+        """返回文件路径"""
+        return self._path
+
+    @property
+    def type(self) -> str:
+        """返回文件类型"""
+        return self._type
+
+    @property
+    def data(self) -> list:
+        """返回当前保存在缓存的数据"""
+        return self._data
+
+    @property
+    def before(self) -> Any:
+        """返回当前before内容"""
+        return self._before
+
+    @property
+    def after(self) -> Any:
+        """返回当前after内容"""
+        return self._after
+
+    def set_path(self, path: Union[str, Path]) -> None:
+        """设置文件路径                \n
+        :param path: 文件路径
+        :return: None
+        """
+        if isinstance(path, str):
+            self._type = path.split('.')[-1].lower()
+        elif isinstance(path, Path):
+            self._type = path.suffix[1:].lower()
+        else:
+            raise TypeError(f'参数file_path只能是str或Path，非{type(path)}。')
+
+        if self._type not in self.SUPPORTS and 'any' not in self.SUPPORTS:
+            raise TypeError(f'只支持{"、".join(self.SUPPORTS)}格式文件。')
+
+        if self._path:
+            self.record()  # 更换文件前自动记录剩余数据
+
+        self._path = str(path) if isinstance(path, Path) else path
+
+    def set_before(self, before: Any) -> None:
+        """设置在数据前面补充的列                              \n
+        :param before: 列表、元组或字符串，为字符串时则补充一列
+        :return: None
+        """
+        self.record()
+
+        if before is None:
+            self._before = []
+        elif isinstance(before, (list, dict)):
+            self._before = before
+        elif isinstance(before, tuple):
+            self._before = list(before)
+        else:
+            self._before = [before]
+
+    def set_after(self, after: Any) -> None:
+        """设置在数据后面补充的列                                \n
+        :param after: 列表、元组或字符串，为字符串时则补充一列
+        :return: None
+        """
+        self.record()
+
+        if after is None:
+            self._after = []
+        elif isinstance(after, (list, dict)):
+            self._after = after
+        elif isinstance(after, tuple):
+            self._after = list(after)
+        else:
+            self._after = [after]
+
+    def clear(self) -> None:
+        """清空缓存中的数据"""
+        self._data = []
+
+    def record(self, new_path: Union[str, Path] = None) -> str:
+        """记录数据，可保存到新文件                                \n
+        :param new_path: 文件另存为的路径，会保存新文件
+        :return: 返回记录文件的路径
+        """
+        # 具体功能由_record()实现，本方法实现自动重试及另存文件功能
+        original_path = return_path = self._path
+        if new_path:
+            new_path = str(get_usable_path(new_path))
+            return_path = self._path = new_path
+
+            if Path(original_path).exists():
+                from shutil import copy
+                copy(original_path, self._path)
+
+        if not self._data:
+            return return_path
+
+        if not self._path:
+            raise ValueError('保存路径为空。')
+
+        with self._lock:
+            self._pause_add = True  # 写入文件前暂缓接收数据
+            while True:
+                try:
+                    self._record()
+                    break
+
+                except PermissionError:
+                    print('\r文件被打开，保存失败，请关闭，程序会自动重试...', end='')
+
+                except Exception as e:
+                    if self._data:
+                        print(f'\n{self._data}\n\n注意！！以上数据未保存')
+                    if 'Python is likely shutting down' not in str(e):
+                        raise
+                    break
+
+            if new_path:
+                self._path = original_path
+
+            self._data = []
+            self._pause_add = False
+
+        return return_path
+
+    def set_head(self, head: Union[list, tuple]) -> None:
+        """设置表头。只有 csv 和 xlsx 格式支持设置表头       \n
+        :param head: 表头，列表或元组
+        :return: None
+        """
+        if self.type == 'xlsx':
+            _set_xlsx_head(self.path, head)
+
+        elif self.type == 'csv':
+            _set_csv_head(self.path, head, self.encoding, self.delimiter, self.quote_char)
+
+        else:
+            raise TypeError('只能对xlsx和csv文件设置表头。')
+
+    @abstractmethod
+    def add_data(self, data):
+        pass
+
+    @abstractmethod
+    def _record(self):
+        pass
+
+    def _data_to_list(self, data: Union[list, tuple, dict]) -> list:
+        """将传入的数据转换为列表形式                                  \n
+        :param data: 要处理的数据
+        :return: 转变成列表方式的数据
+        """
+        return_list = []
+        if data is not None and not isinstance(data, (list, tuple, dict)):
+            data = [data]
+
+        for i in (self.before, data, self.after):
+            if isinstance(i, dict):
+                return_list.extend(list(i.values()))
+            elif i is None:
+                pass
+            elif isinstance(i, list):
+                return_list.extend(i)
+            elif isinstance(i, tuple):
+                return_list.extend(list(i))
+            else:
+                return_list.extend([str(i)])
+
+        return return_list
+
+
+def _set_csv_head(file_path: str,
+                  head: Union[list, tuple],
+                  encoding: str = 'utf-8',
+                  delimiter: str = ',',
+                  quotechar: str = '"') -> None:
+    """设置csv文件的表头              \n
+    :param file_path: 文件路径
+    :param head: 表头列表或元组
+    :param encoding: 编码
+    :param delimiter: 分隔符
+    :param quotechar: 引用符
+    :return: None
+    """
+    from csv import writer
+    if Path(file_path).exists():
+        with open(file_path, 'r', newline='', encoding=encoding) as f:
+            content = "".join(f.readlines()[1:])
+
+        with open(file_path, 'w', newline='', encoding=encoding) as f:
+            csv_write = writer(f, delimiter=delimiter, quotechar=quotechar)
+            csv_write.writerow(_ok_list(head))
+
+        with open(file_path, 'a+', newline='', encoding=encoding) as f:
+            f.write(f'{content}')
+
+    else:
+        with open(file_path, 'w', newline='', encoding=encoding) as f:
+            csv_write = writer(f, delimiter=delimiter, quotechar=quotechar)
+            csv_write.writerow(_ok_list(head))
+
+
+def _set_xlsx_head(file_path: str, head: Union[list, tuple]) -> None:
+    """设置xlsx文件的表头            \n
+    :param file_path: 文件路径
+    :param head: 表头列表或元组
+    :return: None
+    """
+    wb = load_workbook(file_path) if Path(file_path).exists() else Workbook()
+    ws = wb.active
+
+    for key, i in enumerate(head, 1):
+        ws.cell(1, key).value = _process_content(i, True)
+
+    wb.save(file_path)
+    wb.close()
+
+
+def _parse_coord(coord: Union[int, str, list, tuple],
+                 col: int = None,
+                 disable_type: Union[type, list, tuple] = None) -> Tuple[int, int]:
+    """解析坐标，返回坐标tuple                                              \n
+    :param coord: 坐标
+    :param col: 列号，用于只传入行号的情况
+    :param disable_type: 禁用的格式，可以传入单个格式或格式组成的list或tuple
+    :return: 坐标tuple（行, 列）
+    """
+    if disable_type and isinstance(coord, disable_type):
+        raise TypeError(f'当前坐标类型不允许为{disable_type}')
+
+    if isinstance(coord, (int, float)):
+        if col:
+            return int(coord), col
+        else:
+            raise ValueError('只输入行号时必须同时输入列号')
+
+    if isinstance(coord, str):
+        if ',' in coord:  # '3,1'形式
+            coord = coord.replace(' ', '').split(',')
+        else:  # 'A3'形式
+            xy = coordinate_from_string(coord)
+            return xy[1], column_index_from_string(xy[0])
+
+    if isinstance(coord, (tuple, list)) and len(coord) == 2:
+        return int(coord[0]), int(coord[1])
+    else:
+        raise ValueError('list或tuple时长度必须为2')
+
+
+def _process_content(content: Any, excel: bool = False) -> Union[int, str, float, None]:
+    """处理要写入的数据                  \n
+    :param content: 未处理的数据内容
+    :return: 处理后的数据
+    """
+    if isinstance(content, (int, str, float, type(None))):
+        data = content
+    elif isinstance(content, (Cell, ReadOnlyCell)):
+        data = content.value
+    else:
+        data = str(content)
+
+    if excel and isinstance(data, str):
+        data = sub(r'[\000-\010]|[\013-\014]|[\016-\037]', '', data)
+
+    return data
+
+
+def _ok_list(data_list: Union[list, dict], excel: bool = False, as_str: bool = False) -> list:
+    """处理列表中数据使其符合保存规范             \n
+    :param data_list: 数据列表
+    :param excel: 是否保存在excel
+    :return: 处理后的列表
+    """
+    if isinstance(data_list, dict):
+        data_list = data_list.values()
+    if as_str:
+        data_list = [str(i) for i in data_list]
+    return [_process_content(i, excel) for i in data_list]
---
+++ packages/DataRecorder/DataRecorder-2.0.1.tar.gz/DataRecorder-2.0.1/DataRecorder/filler.py
@@ -0,0 +1,335 @@
+# -*- coding:utf-8 -*-
+from csv import reader as csv_reader, writer as csv_writer
+from pathlib import Path
+from time import sleep
+from typing import Union, List
+
+from openpyxl import load_workbook, Workbook
+from openpyxl.utils import column_index_from_string
+
+from .base import BaseRecorder, _parse_coord, _process_content
+
+
+class Filler(BaseRecorder):
+    """Filler类用于根据现有文件中的关键字向文件填充数据"""
+
+    def __init__(self,
+                 path: Union[str, Path],
+                 cache_size: int = None,
+                 key_cols: Union[str, int, list, tuple] = 1,
+                 begin_row: int = 2,
+                 sign_col: Union[str, int] = 2,
+                 sign: str = None,
+                 data_col: Union[int, str] = None) -> None:
+        """初始化                                                    \n
+        :param path: 保存的文件路径
+        :param cache_size: 每接收多少条记录写入文件，传入0表示不自动保存
+        :param key_cols: 作为关键字的列，可以是多列，从1开始
+        :param begin_row: 数据开始的行，默认表头一行
+        :param sign_col: 用于判断是否已填数据的列，从1开始
+        :param sign: 按这个值判断是否已填数据
+        :param data_col: 要填入数据的第一列，从1开始
+        """
+        super().__init__(None, cache_size)
+        super().set_path(path)
+        self.key_cols = key_cols
+        self.begin_row = begin_row
+        self.sign_col = sign_col
+        self.sign = sign
+        self.data_col = data_col or sign_col
+
+    @property
+    def key_cols(self) -> Union[list, tuple]:
+        """返回作为关键字的列或列的集合"""
+        return self._key_cols
+
+    @key_cols.setter
+    def key_cols(self, cols: Union[str, int, list, tuple]) -> None:
+        """设置作为关键字的列，可以是多列                    \n
+        :param cols: 列号或列名，或它们组成的list或tuple
+        :return: None
+        """
+        self._key_cols = (cols,) if isinstance(cols, (int, str)) else cols
+
+    @property
+    def begin_row(self) -> Union[str, int]:
+        """返回数据开始的行号，从1开始"""
+        return self._begin_row
+
+    @begin_row.setter
+    def begin_row(self, row: int) -> None:
+        """设置数据开始的行
+        :param row: 行号
+        :return: None
+        """
+        if not isinstance(row, int) or row < 1:
+            raise TypeError('row值只能是int，且必须大于0')
+        self._begin_row = row
+
+    @property
+    def sign_col(self) -> Union[str, int]:
+        """返回用于判断是否已填数据的列"""
+        return self._sign_col
+
+    @sign_col.setter
+    def sign_col(self, col: Union[str, int]) -> None:
+        """设置用于判断是否已填数据的列
+        :param col: 列号或列名
+        :return: None
+        """
+        if not isinstance(col, (int, str)):
+            raise TypeError('col值只能是int或str')
+        self._sign_col = col
+
+    @property
+    def keys(self) -> list:
+        """返回key列内容，第一位为行号，其余为key列的值  \n
+        eg.[3, '名称', 'id']
+        """
+        if self.type == 'csv':
+            return _get_csv_keys(self.path, self.begin_row, self.sign_col, self.sign, self.key_cols, self.encoding,
+                                 self.delimiter, self.quote_char)
+        elif self.type == 'xlsx':
+            return _get_xlsx_keys(self.path, self.begin_row, self.sign_col, self.sign, self.key_cols)
+
+    def set_path(self,
+                 path: Union[str, Path],
+                 key_cols: Union[str, int, list, tuple] = None,
+                 begin_row: int = None,
+                 sign_col: Union[str, int] = None,
+                 sign: Union[int, float, str] = None,
+                 data_col: int = None) -> None:
+        """设置文件路径                             \n
+        :param path: 保存的文件路径
+        :param key_cols: 作为关键字的列，可以是多列
+        :param begin_row: 数据开始的行，默认表头一行
+        :param sign_col: 用于判断是否已填数据的列
+        :param sign: 按这个值判断是否已填数据
+        :param data_col: 要填入数据的第一列
+        """
+        if not path or not Path(path).exists():
+            raise FileNotFoundError('文件不存在')
+        super().set_path(path)
+        self.key_cols = key_cols or self.key_cols
+        self.begin_row = begin_row or self.begin_row
+        self.sign_col = sign_col or self.sign_col
+        self.sign = sign or self.sign
+        self.data_col = data_col or self.data_col
+
+    def add_data(self,
+                 data: Union[list, tuple, dict],
+                 coord: Union[list, tuple, str, int] = None) -> None:
+        """添加数据                                                                   \n
+        数据格式：第一位为行号或坐标（int或str），第二位开始为数据，数据可以是list, tuple, dict
+        :param data: 要添加的内容，第一位为行号，其余为要添加的内容
+        :param coord: 要添加数据的坐标，仅用于添加一行数据
+        :return: None
+        """
+        while self._pause_add:
+            sleep(.1)
+
+        if not isinstance(data, (list, tuple, dict)):
+            raise TypeError(f'只能接受list、tuple和dict格式数据，不能是{type(data)}')
+
+        if coord is not None:
+            coord = _parse_coord(coord, self.data_col)
+            coord = f'{coord[0]},{coord[1]}'
+            data = (coord, data)
+
+        if isinstance(data, dict) or isinstance(data[0], (int, str)):  # 只有一个数据的情况
+            data = (data,)
+
+        new_data = []
+        for item in data:
+            if isinstance(item, dict):
+                item = list(item.values())
+
+            length = len(item)
+            if not ((isinstance(item, (list, tuple, dict)) and length >= 2) or not isinstance(item[0], (int, str))):
+                raise ValueError('数据项必须为长度不少于2的list、tuple或dict，且第一位为int(行号)、str(eg."B3"或"3,2")。')
+
+            if length == 2 and isinstance(item[1], (list, tuple, dict)):  # 只有两位且第二位是数据集
+                if isinstance(item[1], dict):
+                    val = list(item[1].values())
+                elif isinstance(item[1], tuple):
+                    val = list(item[1])
+                else:
+                    val = item[1]
+                item = [item[0]]
+                item.extend(val)
+
+            new_data.append(item)
+
+        self._data.extend(new_data)
+
+        if 0 < self.cache_size <= len(self._data):
+            self.record()
+
+    def set_link(self,
+                 coord: Union[int, str, tuple, list],
+                 link: str,
+                 content: Union[int, str, float] = None) -> None:
+        """为单元格设置超链接                          \n
+        :param coord: 单元格坐标
+        :param link: 超链接
+        :param content: 单元格内容
+        :return: None
+        """
+        self.add_data(('set_link', coord, link, content))
+
+    def _record(self) -> None:
+        """记录数据"""
+        if self.type == 'xlsx':
+            self._to_xlsx()
+        elif self.type == 'csv':
+            self._to_csv()
+
+    def fill(self, func, *args) -> None:
+        """接收一个方法，根据keys自动填充数据。每条key调用一次该方法，并根据方法返回的内容进行填充  \n
+        方法第一个参数必须是keys，用于接收关键字列，返回的第一位必须是行号或坐标                 \n
+        :param func: 用于获取数据的方法，返回要填充的数据
+        :param args: 该方法的参数
+        :return: None
+        """
+        for i in self.keys:
+            print(i)
+            res = [i[0], func(i, *args)]
+            self.add_data(res)
+        self.record()
+
+    def _to_xlsx(self) -> None:
+        """填写数据到xlsx文件"""
+        wb = load_workbook(self.path) if Path(self.path).exists() else Workbook()
+        ws = wb.active
+        col = self.data_col if isinstance(self.data_col, int) else column_index_from_string(self.data_col)
+
+        for i in self._data:
+            if i[0] == 'set_link':
+                row, col1 = _parse_coord(i[1], col)
+                cell = ws.cell(row, col1)
+                cell.hyperlink = _process_content(i[2], True)
+                if i[3] is not None:
+                    cell.value = _process_content(i[3], True)
+            else:
+                row, col1 = _parse_coord(i[0], col, (list, tuple))
+                for key, j in enumerate(self._data_to_list(i[1:])):
+                    ws.cell(row, col1 + key).value = _process_content(j, True)
+
+        wb.save(self.path)
+        wb.close()
+
+    def _to_csv(self) -> None:
+        """填写数据到xlsx文件"""
+        col = self.data_col if isinstance(self.data_col, int) else column_index_from_string(self.data_col)
+
+        if not Path(self.path).exists():
+            with open(self.path, 'w', encoding=self.encoding):
+                pass
+
+        with open(self.path, 'r', encoding=self.encoding) as f:
+            reader = csv_reader(f, delimiter=self.delimiter, quotechar=self.quote_char)
+            lines = list(reader)
+            lines_len = len(lines)
+
+            for i in self._data:
+                if i[0] == 'set_link':
+                    txt = i[3] or i[2]
+                    now_data = f'=HYPERLINK("{i[2]}","{txt}")'
+                    row, col1 = _parse_coord(i[1], col)
+                else:
+                    now_data = self._data_to_list(i[1:])
+                    row, col1 = _parse_coord(i[0], col, (list, tuple))
+
+                # 若行数不够，填充行数
+                for _ in range(row - lines_len):
+                    lines.append([])
+                    lines_len += 1
+
+                row_num = row - 1
+                # 若列数不够，填充空列
+                lines[row_num].extend([None] * (col1 - len(lines[row_num]) + len(now_data) - 1))
+
+                # 填充数据
+                for k, j in enumerate(now_data):
+                    lines[row_num][col1 + k - 1] = _process_content(j)
+
+            writer = csv_writer(open(self.path, 'w', encoding=self.encoding, newline=''), delimiter=self.delimiter,
+                                quotechar=self.quote_char)
+            writer.writerows(lines)
+
+
+def _get_xlsx_keys(path: str,
+                   begin_row: int,
+                   sign_col: Union[int, str],
+                   sign: Union[int, float, str],
+                   key_cols: Union[list, tuple]) -> List[list]:
+    """返回key列内容，第一位为行号，其余为key列的值       \n
+    eg.[3, '名称', 'id']
+    :param path: 文件路径
+    :param begin_row: 数据起始行
+    :param sign_col: 用于判断是否已填数据的列，从1开始
+    :param sign: 按这个值判断是否已填数据
+    :param key_cols: 关键字所在列，可以是多列
+    :return: 关键字组成的列表
+    """
+    key_cols = [i if isinstance(i, int) else column_index_from_string(i) for i in key_cols]
+    sign_col = column_index_from_string(sign_col) if isinstance(sign_col, str) else sign_col
+
+    wb = load_workbook(path, data_only=True, read_only=True)
+    ws = wb.active
+
+    if ws.max_column is None:  # 遇到过read_only时无法获取列数的文件
+        wb.close()
+        wb = load_workbook(path, data_only=True)
+        ws = wb.active
+
+    if sign_col > ws.max_column:
+        res_keys = [[ind] + [i.value for k, i in enumerate(row, 1) if k in key_cols]
+                    for ind, row in enumerate(ws.rows, 1) if ind >= begin_row]
+    else:
+        res_keys = [[ind] + [i.value for k, i in enumerate(row, 1) if k in key_cols]
+                    for ind, row in enumerate(ws.rows, 1)
+                    if ind >= begin_row and row[sign_col - 1].value == sign]
+
+    wb.close()
+    return res_keys
+
+
+def _get_csv_keys(path: str,
+                  begin_row: int,
+                  sign_col: Union[int, str],
+                  sign: Union[int, float, str],
+                  key_cols: Union[list, tuple],
+                  encoding: str,
+                  delimiter: str,
+                  quotechar: str) -> List[list]:
+    """返回key列内容，第一位为行号，其余为key列的值       \n
+    eg.[3, '名称', 'id']
+    :param path: 文件路径
+    :param begin_row: 数据起始行
+    :param sign_col: 用于判断是否已填数据的列，从1开始
+    :param sign: 按这个值判断是否已填数据
+    :param key_cols: 关键字所在列，可以是多列
+    :param encoding: 字符编码
+    :param delimiter: 分隔符
+    :param quotechar: 引用符
+    :return: 关键字组成的列表
+    """
+    key_cols = [i - 1 if isinstance(i, int) else column_index_from_string(i) - 1 for i in key_cols]
+    sign_col = column_index_from_string(sign_col) if isinstance(sign_col, str) else sign_col
+    sign = '' if sign is None else str(sign)
+    sign_col -= 1
+    begin_row -= 1
+    res_keys = []
+
+    with open(path, 'r', encoding=encoding) as f:
+        reader = csv_reader(f, delimiter=delimiter, quotechar=quotechar)
+        lines = list(reader)[begin_row:]
+
+        for k, line in enumerate(lines):
+            row_sign = '' if sign_col > len(line) - 1 else line[sign_col]
+            if row_sign == sign:
+                key = [k + 1]
+                res_keys.append(key + [i for num, i in enumerate(line) if num in key_cols])
+
+    return res_keys
