bb37e3e0fdb291923703958ce3be5aa4cb18f546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:42 +0000

    Add package scipion-em-prody/3.1.0/scipion_em_prody-3.1.0-py3-none-any.whl

---
+++ packages/scipion-em-prody/scipion_em_prody-3.1.0-py3-none-any.whl/prody2/constants.py
@@ -0,0 +1,39 @@
+# **************************************************************************
+# *
+# * Authors:     James Krieger (jmkrieger@cnb.csic.es)
+# *
+# * Centro Nacional de Biotecnologia, CSIC
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 3 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+
+
+def getProDyEnvName(version):
+    return "prody-%s" % version
+
+DEVEL = 'github'
+V211 = '2.1.2'
+VERSIONS = [DEVEL, V211]
+PRODY_DEFAULT_VER_NUM = V211
+
+DEFAULT_ENV_NAME = getProDyEnvName(PRODY_DEFAULT_VER_NUM)
+DEFAULT_ACTIVATION_CMD = 'conda activate ' + DEFAULT_ENV_NAME
+PRODY_ENV_ACTIVATION = DEFAULT_ACTIVATION_CMD
+
