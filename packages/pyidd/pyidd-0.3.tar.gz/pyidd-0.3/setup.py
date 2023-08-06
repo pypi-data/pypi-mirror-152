69b8717b6645a1c6319f0779c4830c991cfbd536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:44 +0000

    Add package pyidd/0.3/pyidd-0.3.tar.gz

---
+++ packages/pyidd/pyidd-0.3.tar.gz/pyidd-0.3/setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setup(
+    name='pyidd',
+    version='0.3',
+    packages=find_packages(exclude=['tests*', 'examples*']),
+    license='MIT',
+    description='Identify the Distribution of Data',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=['numpy', 'pandas', 'scipy', 'matplotlib'],
+    url='https://github.com/MasoudKaviani/pyidd',
+    author='Masoud Kaviani',
+    author_email='kaviani.masoud@gmail.com'
+)
