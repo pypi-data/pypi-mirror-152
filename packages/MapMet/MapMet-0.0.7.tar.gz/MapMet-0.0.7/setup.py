efa2a963d6c1ff4f4371b71a0cdd080928f839e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:18 +0000

    Add package MapMet/0.0.7/MapMet-0.0.7.tar.gz

---
+++ packages/MapMet/MapMet-0.0.7.tar.gz/MapMet-0.0.7/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.7'
+DESCRIPTION = 'MapMet Package'
+LONG_DESCRIPTION = 'Dummy'
+
+setup(
+    name="MapMet",
+    version=VERSION,
+    author="Taschner-Mandl Group",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+    ]
+)
