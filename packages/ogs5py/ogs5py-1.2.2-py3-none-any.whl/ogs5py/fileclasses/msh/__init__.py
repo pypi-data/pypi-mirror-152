2baf9fe45959d0a237ce030c7a5f4c50eb30ec39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:21 +0000

    Add package ogs5py/1.2.2/ogs5py-1.2.2-py3-none-any.whl

---
+++ packages/ogs5py/ogs5py-1.2.2-py3-none-any.whl/ogs5py/fileclasses/msh/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Class for the ogs MESH file.
+
+.. currentmodule:: ogs5py.fileclasses.msh
+
+Subpackages
+^^^^^^^^^^^
+The generators can be called with :any:`MSH.generate`
+
+.. autosummary::
+   generator
+
+File Class
+^^^^^^^^^^
+
+.. autosummary::
+   :toctree: generated
+
+   MSH
+
+----
+"""
+from ogs5py.fileclasses.msh.core import MSH
+
+__all__ = ["MSH"]
