8510ced7b4ee32e41d91585246ab90e5e64be464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:56 +0000

    Add package djcalcilib/0.0.1/djcalcilib-0.0.1.tar.gz

---
+++ packages/djcalcilib/djcalcilib-0.0.1.tar.gz/djcalcilib-0.0.1/djcalcilib/djcalcilib.py
@@ -0,0 +1,17 @@
+def sum(x,y):
+    print("sum"," =",(x+y))
+    
+def subtract(x,y):
+    print("difference"," =",(x-y))
+    
+def divide(x,y):
+    print("division"," =",(x/y))
+    
+def multiply(x,y):
+    print("multiplication"," =",(x/y))    
+    
+    
+    
+    
+  
+    
---
+++ packages/djcalcilib/djcalcilib-0.0.1.tar.gz/djcalcilib-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+  name='djcalcilib',
+  version='0.0.1',
+  description='A basic calculator',
+  author='dasham',
+  zip_safe=False,
+  packages=['djcalcilib']
+  
+)
+                                                                   
