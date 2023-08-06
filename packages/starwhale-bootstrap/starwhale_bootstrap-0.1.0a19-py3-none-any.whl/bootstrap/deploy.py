ebdfc7c02b34f6916cd36ed6e8a204375e3cfebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:47 +0000

    Add package starwhale-bootstrap/0.1.0a19/starwhale_bootstrap-0.1.0a19-py3-none-any.whl

---
+++ packages/starwhale-bootstrap/starwhale_bootstrap-0.1.0a19-py3-none-any.whl/bootstrap/deploy.py
@@ -0,0 +1,35 @@
+import os
+
+import ansible_runner
+
+
+def deploy(logdir: str = "",
+           extravars: dict = {},
+           cmdline: str = "",
+           inventory: dict = {}):
+    cur_path = os.path.abspath(os.path.dirname(__file__))
+    if not os.path.exists(logdir):
+        os.makedirs(logdir)
+    # for issue https://github.com/ansible/ansible-runner/issues/853
+    os.remove(logdir+'/inventory/hosts.json')
+
+    # ABSPATH = '/mnt/c/Users/gaoxinxing/IdeaProjects/containerdJavaGrpc/bootstrap/'
+    # 可参考：https://gist.github.com/privateip/879683a0172415c408fb2afb82a97511
+
+    r = ansible_runner.run(
+        private_data_dir=logdir,
+        playbook=cur_path + '/playbook/bootstrap.yaml',
+        roles_path=cur_path + '/playbook/roles',
+        extravars=extravars,
+        cmdline=cmdline,
+        # 来源：https://ansible.leops.cn/advanced/dynamic-hosts/
+        inventory=inventory
+        # optional
+        # ssh_key='',
+    )
+    print("{}: {}".format(r.status, r.rc))
+    # successful: 0
+    for each_host_event in r.events:
+        print(each_host_event['event'])
+    print("Final status:")
+    print(r.stats)
