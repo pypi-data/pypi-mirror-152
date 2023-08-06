36f302592ae57eb254ef9a62251793a320f4b2c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:55 +0000

    Add package bernina-analysis/0.0.1/bernina_analysis-0.0.1-py2.py3-none-any.whl

---
+++ packages/bernina-analysis/bernina_analysis-0.0.1-py2.py3-none-any.whl/bernina_analysis/__init__.py
@@ -0,0 +1,7 @@
+from .analysis_smalldata.data_reduction import Data_Reduction
+from .analysis_smalldata import analysis_smalldata
+from .utilities import utilities
+from .analysis_online import analysis_online
+
+__version__ = ""0.0.1""
+
