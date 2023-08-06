0276d2c56662ecc6f31730f25b146b81d3b50535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:36 +0000

    Add package Omodem/1.0.0/Omodem-1.0.0-py3-none-any.whl

---
+++ packages/Omodem/Omodem-1.0.0-py3-none-any.whl/orange/__init__.py
@@ -0,0 +1,32 @@
+from requests import post, get
+from getpass import getpass
+from bs4 import BeautifulSoup
+login = get("http://192.168.0.1")
+
+def main():
+    if login.status_code == 200:
+        stats = [
+            str(i).split(">")[1].split("<")[0]
+            for i in BeautifulSoup(login.text, "html.parser").find_all("span", {"class": "severFontSize fontStyle01"})
+        ]
+        print(f"\n🌐 Wifi : {stats[0]}\n🌍 Accès internet : {stats[1]}\n🔌 Réseau câblé : {stats[2]}\n")
+
+        if "Incorrect password !" in post("http://192.168.0.1/goform/OrgLogin", data={"OrgPassword" : getpass("Mot de passe > ")}).text:
+            print("❌ Le mot de passe est incorrect")
+        else:
+            print("Adresse ip publique : " + BeautifulSoup(get("http://192.168.0.1/overview.asp")
+            .text, "html.parser").find('span', attrs={'class': 'severFontSize fontStyle01'}).get_text())
+
+            choice = int(input("Que faire ? : \n1 - Changer d'ip (redémarre le réseau local\n2 - quitter\n> "))
+
+            if choice == 1:
+                import dist.restart_local
+            elif choice == 2:
+                exit()
+    else:
+        print(
+            "La box me répond un code d'erreur ou je ne parviens tout simpletement pas à l'atteindre"
+        )
+
+if __name__ == '__main__':
+    main()
