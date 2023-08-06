d346b034a3cf516fb9dcb51550612d9217ca7517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:38 +0000

    Add package leapcli/0.2.27/leapcli-0.2.27.tar.gz

---
+++ packages/leapcli/leapcli-0.2.27.tar.gz/leapcli-0.2.27/templates/.tensorleap/model.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+
+def leap_save_model(target_file_path: Path):
+    # Load your model
+    # Save it to the path supplied as an argument.
+
+    print(f'Saving the model as {target_file_path}. Safe to delete this print.')
