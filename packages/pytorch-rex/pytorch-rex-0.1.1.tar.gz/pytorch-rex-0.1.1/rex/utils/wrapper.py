0c72fa73ddee9231f8fb432dcf9a96bf6d0173b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:39 +0000

    Add package pytorch-rex/0.1.1/pytorch-rex-0.1.1.tar.gz

---
+++ packages/pytorch-rex/pytorch-rex-0.1.1.tar.gz/pytorch-rex-0.1.1/rex/utils/wrapper.py
@@ -0,0 +1,44 @@
+import datetime
+import functools
+from datetime import timedelta
+from typing import Callable, Optional
+
+from rex.utils.logging import logger
+
+
+def safe_try(
+    func_placeholder: Optional[Callable] = None,
+    start_msg: Optional[str] = None,
+    end_msg: Optional[str] = None,
+):
+    def func_wrapper(func):
+        @functools.wraps(func)
+        def try_func(*args, **kwargs):
+            if start_msg is not None:
+                logger.info(str(start_msg))
+
+            err_happened = False
+            start_dt = datetime.datetime.now()
+
+            try:
+                return func(*args, **kwargs)
+            except KeyboardInterrupt:
+                err_happened = True
+                raise KeyboardInterrupt
+            except Exception as err:
+                err_happened = True
+                logger.exception(err)
+            finally:
+                time_delta = datetime.datetime.now() - start_dt
+                if not err_happened:
+                    logger.debug(f"Func `{func.__name__}` finished without err.")
+                if end_msg is not None:
+                    logger.info(str(end_msg))
+                logger.info(f"Func: `{func.__name__}` call time: {str(time_delta)}")
+
+        return try_func
+
+    if func_placeholder is None:
+        return func_wrapper
+    else:
+        return func_wrapper(func_placeholder)
