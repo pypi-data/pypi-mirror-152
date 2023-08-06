0735ade558d318f73734055def5e93dce4f2e284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:50:09 +0000

    Add package aws-prototyping-sdk/0.0.69/aws_prototyping_sdk-0.0.69.tar.gz

---
+++ packages/aws-prototyping-sdk/aws_prototyping_sdk-0.0.69.tar.gz/aws_prototyping_sdk-0.0.69/src/aws_prototyping_sdk/__init__.py
@@ -0,0 +1,25 @@
+'''
+TODO
+'''
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from ._jsii import *
+
+__all__ = [
+    "nx_monorepo",
+    "pipeline",
+]
+
+publication.publish()
+
+# Loading modules to ensure their types are registered with the jsii runtime library
+from . import nx_monorepo
+from . import pipeline
