6e085f2d8c5da68fd781df5a811a1920ecae3b8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:47 +0000

    Add package clitify/0.1.3/clitify-0.1.3.tar.gz

---
+++ packages/clitify/clitify-0.1.3.tar.gz/clitify-0.1.3/README.md
@@ -0,0 +1,7 @@
+# Install
+```sh
+pip install clitify
+```
+
+# Source
+![https://cdn-icons-png.flaticon.com/512/25/25231.png]([github.com/marcpartensky/clitify](https://github.com/marcpartensky/clitify))
---
+++ packages/clitify/clitify-0.1.3.tar.gz/clitify-0.1.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['clitify']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['rich>=12.4.4,<13.0.0', 'spotipy>=2.19.0,<3.0.0', 'termcolors>=0.1.0,<0.2.0']
+
+setup_kwargs = {
+    'name': 'clitify',
+    'version': '0.1.3',
+    'description': 'CLI for the Spotify API using Spotipy',
+    'long_description': '# Install\n```sh\npip install clitify\n```\n\n# Source\n![https://cdn-icons-png.flaticon.com/512/25/25231.png]([github.com/marcpartensky/clitify](https://github.com/marcpartensky/clitify))\n',
+    'author': 'Marc Partensky',
+    'author_email': 'marc.partensky@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/marcpartensky/clitify',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
