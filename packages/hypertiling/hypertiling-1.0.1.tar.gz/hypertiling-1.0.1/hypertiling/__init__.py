897dcd0694f21736ac5cde8f2fad35851f7e71c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:22 +0000

    Add package hypertiling/1.0.1/hypertiling-1.0.1.tar.gz

---
+++ packages/hypertiling/hypertiling-1.0.1.tar.gz/hypertiling-1.0.1/hypertiling/__init__.py
@@ -0,0 +1,7 @@
+from .core import HyperbolicTiling
+
+__all__ = ['HyperbolicTiling']
+
+__version__ = "0.9.5"
+__author__ = 'Manuel Schrauth, Felix Dusel, Florian Goth, Dietmar Herdt, Jefferson S. E. Portela'
+__credits__ = 'Institute for Theoretical Physics and Astrophysics, University of Wuerzburg'
