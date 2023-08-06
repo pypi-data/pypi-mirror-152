a8f25ac006f6fd2b9af140bec74eb744ed599df5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:47 +0000

    Add package dinteractions-Paginator/2.0.1/dinteractions_Paginator-2.0.1-py3-none-any.whl

---
+++ packages/dinteractions-Paginator/dinteractions_Paginator-2.0.1-py3-none-any.whl/interactions/ext/paginator/extension.py
@@ -0,0 +1,18 @@
+from interactions.ext import Base, Version, VersionAuthor
+
+version: Version = Version(
+    version="2.0.1",
+    author=VersionAuthor(
+        name="Toricane",
+        email="prjwl028@gmail.com",
+    ),
+)
+
+base = Base(
+    name="dinteractions-Paginator",
+    version=version,
+    description="Official interactions.py paginator",
+    link="https://github.com/Toricane/dinteractions-Paginator",
+    packages=["interactions.ext.paginator"],
+    requirements=["discord-py-interactions>=4.2.0", "interactions-wait-for>=1.0.4"],
+)
