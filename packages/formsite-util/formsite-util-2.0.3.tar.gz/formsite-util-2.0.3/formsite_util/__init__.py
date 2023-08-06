2c35a8675493ef6b9a682587b792f5b4bc6577b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:54 +0000

    Add package formsite-util/2.0.3/formsite-util-2.0.3.tar.gz

---
+++ packages/formsite-util/formsite-util-2.0.3.tar.gz/formsite-util-2.0.3/formsite_util/__init__.py
@@ -0,0 +1,34 @@
+"""
+
+## High level interfaces
+
+FormsiteParameters: Represents parameters for results/items requests
+
+FormsiteForm: Represents the form data and session
+
+FormsiteFormsList: Represents the list of all forms for the specified account
+
+FormCache: Store formsite forms in a folder
+
+## Low level interfaces
+
+FormFetcher: Result/Item fetching operations
+
+FormParser: Result/Item parsing operations
+
+FormData: Represents the form data without session
+
+FormsiteLogger: Custom logger you may connect to your own logging
+
+"""
+__version__ = "2.0.3"
+
+from .form import FormsiteForm
+from .list import FormsiteFormsList
+from .parameters import FormsiteParameters
+from .form_fetcher import FormFetcher
+from .form_parser import FormParser
+from .form_data import FormData
+
+# Legacy functionality
+from .legacy import FormsiteCredentials, FormsiteInterface, FormsiteParams
