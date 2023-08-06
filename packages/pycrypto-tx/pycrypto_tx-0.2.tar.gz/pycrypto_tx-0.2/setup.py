a81a42c2df9e249e5c0a48aabde80b97acddbdf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:21 +0000

    Add package pycrypto-tx/0.2/pycrypto_tx-0.2.tar.gz

---
+++ packages/pycrypto-tx/pycrypto_tx-0.2.tar.gz/pycrypto_tx-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+name = "pycrypto_tx",
+version = "0.2",
+license = "MIT",
+author = "IndentErr",
+description = "Free and easy python cryptocurrency transaction library",
+url = "https://github.com/IndentErr/pycrypto_tx",
+packages = find_packages(),
+install_requires = ["cryptocompare","datetime","web3","cryptodome","ecdsa","bitcoinlib","base58","hashlib"],
+python_version = ">=3.6",
+)
