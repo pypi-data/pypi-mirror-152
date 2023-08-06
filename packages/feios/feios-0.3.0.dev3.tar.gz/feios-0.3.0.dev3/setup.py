ca2b2712ef64afc98a30ce01f22c70a40ef2748c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:51 +0000

    Add package feios/0.3.0.dev3/feios-0.3.0.dev3.tar.gz

---
+++ packages/feios/feios-0.3.0.dev3.tar.gz/feios-0.3.0.dev3/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="feios",
+    version="0.3.0dev3",
+    author="fyc",
+    author_email="fycsfls_winter@126.com",
+    description="The feios main module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/devoter-fyc/fei-ros",
+    project_urls={
+        "Bug Tracker": "https://github.com/devoter-fyc/fei-ros/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha"
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.5",
+    install_requires=['feios-utils>=0.2.0b3']
+)
