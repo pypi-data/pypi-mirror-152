874d28b77840ac115e9fd9ee8a16ac0b89a15541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:00 +0000

    Add package nexoclom/3.2.8/nexoclom-3.2.8.tar.gz

---
+++ packages/nexoclom/nexoclom-3.2.8.tar.gz/nexoclom-3.2.8/nexoclom/__init__.py
@@ -0,0 +1,15 @@
+from nexoclom.modelcode.Input import Input
+from nexoclom.modelcode.Output import Output
+from nexoclom.modelcode.LOSResult import LOSResult
+from nexoclom.modelcode.ModelImage import ModelImage
+from nexoclom.utilities.configure import configure
+from nexoclom.solarsystem import SSObject
+
+
+__name__ = 'nexoclom'
+__author__ = 'Matthew Burger'
+__email__ = 'mburger@stsci.edu'
+__version__ = '3.2.8'
+__date__ = '2022-05-27'
+
+configure(verbose=True)
