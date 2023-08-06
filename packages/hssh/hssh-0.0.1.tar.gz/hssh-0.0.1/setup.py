60fc7349a695d66964d22495cea3ad4ace32adf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:22 +0000

    Add package hssh/0.0.1/hssh-0.0.1.tar.gz

---
+++ packages/hssh/hssh-0.0.1.tar.gz/hssh-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+install_requires=['pandas','konlpy','numpy']
+
+setup(
+    name='hssh',
+    version='0.0.1',
+    description='2702 김수진',
+    url='https://github.com/Sujin-Github/Study2022',
+    install_requires=install_requires,
+    packages=['hssh']
+)
