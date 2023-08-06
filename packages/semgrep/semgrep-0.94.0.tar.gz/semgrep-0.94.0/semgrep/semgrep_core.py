3c63156c1fea458002b36d06f6aa3f3aaed23b14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:27 +0000

    Add package semgrep/0.94.0/semgrep-0.94.0.tar.gz

---
+++ packages/semgrep/semgrep-0.94.0.tar.gz/semgrep-0.94.0/semgrep/semgrep_core.py
@@ -0,0 +1,55 @@
+import os
+import shutil
+import sys
+from typing import Optional
+
+import pkg_resources
+
+
+def compute_executable_path(exec_name: str) -> Optional[str]:
+    """
+    Determine full executable path if full path is needed to run it.
+
+    Return None if no executable found
+    """
+    # First, try packaged binaries
+    pkg_exec = pkg_resources.resource_filename("semgrep.bin", exec_name)
+    if os.path.isfile(pkg_exec):
+        return pkg_exec
+
+    # Second, try system binaries in PATH.
+    #
+    # Environment variables, including PATH, are not inherited by the pytest
+    # jobs (at least by default), so this won't work when running pytest
+    # tests.
+    #
+    which_exec = shutil.which(exec_name)
+    if which_exec is not None:
+        return which_exec
+
+    # Third, look for something in the same dir as the Python interpreter
+    relative_path = os.path.join(os.path.dirname(sys.executable), exec_name)
+    if os.path.isfile(relative_path):
+        return relative_path
+
+    return None
+
+
+class SemgrepCore:
+    _SEMGREP_PATH_: Optional[str] = None
+    _DEEP_PATH_: Optional[str] = None
+
+    @classmethod
+    def path(cls) -> str:
+        if cls._SEMGREP_PATH_ is None:
+            cls._SEMGREP_PATH_ = compute_executable_path("semgrep-core")
+            if cls._SEMGREP_PATH_ is None:
+                raise Exception("Could not locate semgrep-core binary")
+
+        return cls._SEMGREP_PATH_
+
+    @classmethod
+    def deep_path(cls) -> Optional[str]:
+        if cls._DEEP_PATH_ is None:
+            cls._DEEP_PATH_ = compute_executable_path("deep-semgrep")
+        return cls._DEEP_PATH_
