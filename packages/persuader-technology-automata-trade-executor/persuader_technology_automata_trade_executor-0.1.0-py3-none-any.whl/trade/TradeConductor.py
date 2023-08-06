696b93dc5b7fe2c168ee0418121875a59b9c9553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:59 +0000

    Add package persuader-technology-automata-trade-executor/0.1.0/persuader_technology_automata_trade_executor-0.1.0-py3-none-any.whl

---
+++ packages/persuader-technology-automata-trade-executor/persuader_technology_automata_trade_executor-0.1.0-py3-none-any.whl/trade/TradeConductor.py
@@ -0,0 +1,30 @@
+import logging
+
+from core.trade.InstrumentTrade import InstrumentTrade, Status
+from traderepo.repository.TradeRepository import TradeRepository
+
+from trade.executor.TradeExecutor import TradeExecutor
+
+
+class TradeConductor:
+
+    def __init__(self, options, trade_repository: TradeRepository, trade_executor: TradeExecutor):
+        self.log = logging.getLogger(__name__)
+        self.options = options
+        self.trade_repository = trade_repository
+        self.trade_executor = trade_executor
+
+    def store_submitted_trade(self, trade: InstrumentTrade):
+        self.trade_repository.store_trade(trade)
+
+    def fetch_trade_to_submit(self) -> InstrumentTrade:
+        return self.trade_repository.retrieve_trade()
+
+    def perform_trade(self):
+        trade = self.fetch_trade_to_submit()
+        if trade is not None and trade.status == Status.NEW:
+            self.log.info(f'About to execute trade:[{trade}]')
+            updated_trade = self.trade_executor.trade(trade)
+            self.store_submitted_trade(updated_trade)
+        else:
+            self.log.warning(f'Trade is not new, so will not trade -> {trade}')
