554316b8221c29a05041d867f6bd818661a9303b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:45:29 +0000

    Add package RelevanceAI-dev/2.2.0.2022.5.24.9.5.35.626621/RelevanceAI-dev-2.2.0.2022.5.24.9.5.35.626621.tar.gz

---
+++ packages/RelevanceAI-dev/RelevanceAI-dev-2.2.0.2022.5.24.9.5.35.626621.tar.gz/RelevanceAI-dev-2.2.0.2022.5.24.9.5.35.626621/relevanceai/operations_new/vectorize/ops.py
@@ -0,0 +1,7 @@
+from tkinter import N
+from relevanceai.operations_new.apibase import OperationAPIBase
+from relevanceai.operations_new.vectorize.base import VectorizeBase
+
+
+class VectorizeOps(OperationAPIBase, VectorizeBase):
+    pass
