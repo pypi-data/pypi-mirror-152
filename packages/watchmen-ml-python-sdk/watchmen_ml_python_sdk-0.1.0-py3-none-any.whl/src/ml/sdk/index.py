9872c05f7ac77d87c03d619b58ecb4ae4b3662f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:18:55 +0000

    Add package watchmen-ml-python-sdk/0.1.0/watchmen_ml_python_sdk-0.1.0-py3-none-any.whl

---
+++ packages/watchmen-ml-python-sdk/watchmen_ml_python_sdk-0.1.0-py3-none-any.whl/src/ml/sdk/index.py
@@ -0,0 +1,42 @@
+import os
+
+from src.ml.model.notebook import WatchmenNotebook
+from src.ml.sdk.watchmen.sdk import load_dataset_by_name
+
+
+class WatchmenClient(object):
+    def __init__(self,token):
+        if token:
+            self.token = token
+        else:
+            self.token = os.environ.get('TOKEN')
+
+    def load_dataset(self, name):
+        return load_dataset_by_name(self.token,name)
+
+
+    def register_notebook(self,notebook:WatchmenNotebook):
+        print(notebook)
+        pass
+
+    def save_topic_dataset(self,topic_name:str, dataset):
+        pass
+
+
+    def register_model(self):
+        pass
+
+
+
+client  = WatchmenClient(token="0Z6ag50cdIPamBIgf8KfoQ")
+df = client.load_dataset("DEMO")
+print(df)
+
+
+
+
+
+
+
+
+
