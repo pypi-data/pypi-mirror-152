899c7fd72e74ddf7b446b35927282c82babed137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:26 +0000

    Add package pyDaRUS/1.0.5/pyDaRUS-1.0.5.tar.gz

---
+++ packages/pyDaRUS/pyDaRUS-1.0.5.tar.gz/pyDaRUS-1.0.5/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name="pyDaRUS",
+    version="1.0.5",
+    author="easyDataverse",
+    license="MIT License",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "easyDataverse",
+        "fastapi",
+        "uvicorn",
+        "pydantic",
+        "jinja2",
+        "pyDataverse",
+        "pandas",
+        "pyaml",
+    ],
+)
