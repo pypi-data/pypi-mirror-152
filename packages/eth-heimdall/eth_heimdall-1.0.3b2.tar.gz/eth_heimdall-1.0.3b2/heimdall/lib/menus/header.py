16aaba18a614309594eec07d79f8cf018812e3c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:59 +0000

    Add package eth-heimdall/1.0.3b2/eth_heimdall-1.0.3b2.tar.gz

---
+++ packages/eth-heimdall/eth_heimdall-1.0.3b2.tar.gz/eth_heimdall-1.0.3b2/heimdall/lib/menus/header.py
@@ -0,0 +1,20 @@
+from ..utils.colors import colorLib
+from ..utils.version import getLocalVersion
+
+# returns the heimdall global header
+def getHeader():
+  return (
+    f'''{colorLib.BLUE}
+ _   _  _____ ________  ________  ___   _      _     
+| | | ||  ___|_   _|  \/  |  _  \/ _ \ | |    | |    
+| |_| || |__   | | | .  . | | | / /_\ \| |    | |    
+|  _  ||  __|  | | | |\/| | | | |  _  || |    | |    
+| | | || |___ _| |_| |  | | |/ /| | | || |____| |____
+\_| |_/\____/ \___/\_|  |_/___/ \_| |_/\_____/\_____/
+
+{colorLib.RESET}Version:  {getLocalVersion()}
+Author:   Jonathan Becker (jonathan@jbecker.dev)
+GitHub:   https://github.com/Jon-Becker/heimdall
+
+'''
+  )
