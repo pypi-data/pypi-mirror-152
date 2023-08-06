b05a89bd9f8ee6174cffa9ee0dde9d935f2c2ab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:48 +0000

    Add package create-icon/0.0.6/create_icon-0.0.6.tar.gz

---
+++ packages/create-icon/create_icon-0.0.6.tar.gz/create_icon-0.0.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='create_icon',
+      version='0.0.6',
+      description='Creates an Icon in the Users Folder',
+      url='http://Caveman-Software.github.com',
+      author='Michael Mulvey',
+      author_email='michael.e.mulvey@gmail.com',
+      license='MIT',
+      packages=[],
+      zip_safe=False)
