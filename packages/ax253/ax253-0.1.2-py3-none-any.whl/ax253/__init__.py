968ef2b6b43193b9b672b5668a5e7f6799aacf8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:18:48 +0000

    Add package ax253/0.1.2/ax253-0.1.2-py3-none-any.whl

---
+++ packages/ax253/ax253-0.1.2-py3-none-any.whl/ax253/__init__.py
@@ -0,0 +1,23 @@
+from importlib_metadata import version
+
+from .address import Address
+from .decode import GenericDecoder, FrameDecodeProtocol, SyncFrameDecode
+from .frame import AX25BytestreamDecoder, Control, Frame, FrameType
+
+__author__ = "Masen Furer KF7HVM <kf7hvm@0x26.net>"
+__copyright__ = "Copyright 2022 Masen Furer and Contributors"
+__license__ = "Apache License, Version 2.0"
+__distribution__ = "ax253"
+__version__ = version(__distribution__)
+__all__ = [
+    "Address",
+    "AX25BytestreamDecoder",
+    "Control",
+    "Frame",
+    "FrameDecodeProtocol",
+    "FrameType",
+    "GenericDecoder",
+    "SyncFrameDecode",
+    "__distribution__",
+    "__version__",
+]
