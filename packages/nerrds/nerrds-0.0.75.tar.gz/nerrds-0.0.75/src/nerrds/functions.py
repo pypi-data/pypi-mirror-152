009eae33827b82a62dfde6caa45c713204dde0ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:48:13 +0000

    Add package nerrds/0.0.75/nerrds-0.0.75.tar.gz

---
+++ packages/nerrds/nerrds-0.0.75.tar.gz/nerrds-0.0.75/src/nerrds/functions.py
@@ -0,0 +1,21 @@
+
+
+
+def check_quiet_print(quiet,msg,end='\n'):
+    out = open('NERRDS.log','a+')
+    out.write(msg+end)
+    out.close()
+    if not quiet:
+        print(msg,end=end)
+
+def suppress_stdout():
+
+    from contextlib import contextmanager
+    
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:  
+            yield
+        finally:
+            sys.stdout = old_stdout
