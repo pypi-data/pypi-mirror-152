ad209d2485954ac75e7229c0a4c58cd270754596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:02 +0000

    Add package tweety-ns/0.3.6/tweety-ns-0.3.6.tar.gz

---
+++ packages/tweety-ns/tweety-ns-0.3.6.tar.gz/tweety-ns-0.3.6/src/tweety/exceptions_.py
@@ -0,0 +1,63 @@
+class UserNotFound(Exception):
+    """Exception raised when user isn't found.
+
+    Attributes:
+        message -- explanation of the error
+    """
+
+    def __init__(self, message="Either User not Found or is Restricted"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class GuestTokenNotFound(Exception):
+    """
+    Exception Raised when the guest token wasn't found after specific number of retires
+
+    Attributes:
+        message -- explanation of the error
+    """
+
+    def __init__(self,message):
+        self.message = message
+        super().__init__(self.message)
+
+
+class InvalidTweetIdentifier(Exception):
+    """
+        Exception Raised when the tweet identifier is invalid
+
+        Attributes:
+            message -- explanation of the error
+    """
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+class ProxyParseError(Exception):
+    """
+    Exception Raised when an error occurs while parsing the provided proxy
+
+    Attributes:
+        message -- explanation of the error
+    """
+
+    def __init__(self, message="Error while parsing the Proxy, please make sure you are passing the right formatted proxy"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class UnknownError(Exception):
+    """
+        Exception Raised when an unknown error occurs
+
+        Attributes:
+            message -- explanation of the error
+        """
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
