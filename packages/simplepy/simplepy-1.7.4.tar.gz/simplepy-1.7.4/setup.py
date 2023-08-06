374ea10b5789b3a22caca56d0b244c65a5dc0188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:21:37 +0000

    Add package simplepy/1.7.4/simplepy-1.7.4.tar.gz

---
+++ packages/simplepy/simplepy-1.7.4.tar.gz/simplepy-1.7.4/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='simplepy',
+    version='1.7.4',
+    author='fovegage',
+    author_email='fovegage@gmail.com',
+    url='https://github.com/fovegage/simplepy',
+    description='Python General Toolkit Collection',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'pysnowflake',
+        'opencv-python',
+        'PyCryptodome',
+        'pandas',
+        'pymongo',
+        'pymysql',
+        'redis',
+        'environs',
+        'lxml',
+        'selenium',
+        'requests',
+        'aiohttp',
+        'flask',
+        'loguru',
+        'frida',
+        'imap_tools',
+        'tenacity',
+        'matplotlib',
+        'jieba',
+        'sqlalchemy',
+        'wordcloud',
+        'pypac',
+        'simplejson',
+        'pyDes',
+        'snowflake',
+        'paramiko',
+        'numpy',
+        'faker',
+        'BeautifulSoup4',
+        'pyvirtualdisplay'
+    ]
+)
