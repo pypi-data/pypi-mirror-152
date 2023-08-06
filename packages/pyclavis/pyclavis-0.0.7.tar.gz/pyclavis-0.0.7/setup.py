6b945c018e7649a0ba19aacdfbe3b0655d633146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:57 +0000

    Add package pyclavis/0.0.7/pyclavis-0.0.7.tar.gz

---
+++ packages/pyclavis/pyclavis-0.0.7.tar.gz/pyclavis-0.0.7/setup.py
@@ -0,0 +1,78 @@
+import setuptools
+import os
+import re
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+def find_version(fnam, version="VERSION"):
+    with open(fnam) as f:
+        cont = f.read()
+    regex = f'{version}\s*=\s*["]([^"]+)["]'
+    match = re.search(regex, cont)
+    if match is None:
+        raise Exception(
+            f"version with spec={version} not found, use double quotes for version string"
+        )
+    return match.group(1)
+
+
+def find_projectname():
+    cwd = os.getcwd()
+    name = os.path.basename(cwd)
+    return name
+
+
+projectname = find_projectname()
+
+file = os.path.join(projectname, "pyclavis.py")
+version = find_version(file)
+
+setuptools.setup(
+    name=projectname,
+    version=version,
+    author="k.r. goger",
+    author_email=f"k.r.goger+{projectname}@gmail.com",
+    description="pyclavis key-store password manager with autofill templates as predefined download",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/kr-g/{projectname}",
+    packages=setuptools.find_packages(),
+    keywords="python utility shell git password-manager key-store",
+    install_requires=[
+        "pygitgrab",
+        "pyjsoncfg",
+        "pyautogui",
+        "pyperclip",
+        "cryptography",
+        "semver",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Operating System :: POSIX :: Linux",
+        "Environment :: Console",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: End Users/Desktop",
+        "Topic :: Utilities",
+        "Topic :: Desktop Environment",
+        "Topic :: Internet",
+        "Topic :: Home Automation",
+        "Topic :: Office/Business",
+        "Topic :: Security",
+        "Topic :: Security :: Cryptography",
+        "Programming Language :: Python :: 3",
+        "License :: Free For Home Use",
+        "License :: Free for non-commercial use",
+        "License :: Free To Use But Restricted",
+        "License :: Other/Proprietary License",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+    ],
+    python_requires=">=3.8",
+)
+
+# python3 -m setup sdist build bdist_wheel
+# twine upload --repository testpypi dist/*
+# twine upload dist/*
