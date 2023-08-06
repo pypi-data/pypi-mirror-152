a32b295c30080fde3f4cea82a1956f90c0ca9c7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:20:20 +0000

    Add package aiopayok/0.1.6/aiopayok-0.1.6.tar.gz

---
+++ packages/aiopayok/aiopayok-0.1.6.tar.gz/aiopayok-0.1.6/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['aiopayok', 'aiopayok.models']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiohttp>=3.8.1,<4.0.0',
+ 'certifi>=2022.5.18,<2023.0.0',
+ 'pydantic>=1.9.1,<2.0.0']
+
+setup_kwargs = {
+    'name': 'aiopayok',
+    'version': '0.1.6',
+    'description': 'payok.io asynchronous python wrapper',
+    'long_description': '[![GitHub issues](https://img.shields.io/github/issues/layerqa/aiopayok?style=for-the-badge)](https://github.com/layerqa/aiopayok/issues)\n[![GitHub license](https://img.shields.io/github/license/layerqa/aiopayok?style=for-the-badge)](https://github.com/layerqa/aiopayok/blob/main/LICENSE)\n[![PyPI](https://img.shields.io/pypi/v/aiopayok?style=for-the-badge)](https://pypi.org/project/aiopayok/)\n# aiopayok\npayok.io asynchronous python wrapper\n',
+    'author': 'layerqa',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/layerqa/aiopayok',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
