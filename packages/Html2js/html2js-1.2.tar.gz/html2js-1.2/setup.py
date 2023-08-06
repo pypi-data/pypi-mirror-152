ce13befd56c29c836773501a3f8f32c792d109af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:14 +0000

    Add package Html2js/1.2/html2js-1.2.tar.gz

---
+++ packages/Html2js/html2js-1.2.tar.gz/html2js-1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+setup(
+    name='html2js',
+    version='1.2',
+    license='MIT',
+    author="Mhadhbi Issam",
+    author_email='mhadhbixissam@gmail.com ',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/MhadhbiXissam/Html2Js',
+    keywords='convert html to javascript code ( JSX like )',
+    install_requires=[
+          'beautifulsoup4','lxml'
+      ],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+
+)
