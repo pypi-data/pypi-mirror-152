424d7c215e859b4c385f55ca0f2e48dc66e39f24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:08 +0000

    Add package lightly/1.2.17/lightly-1.2.17-py3-none-any.whl

---
+++ packages/lightly/lightly-1.2.17-py3-none-any.whl/lightly/models/modules/__init__.py
@@ -0,0 +1,23 @@
+"""The lightly.models.modules package provides reusable modules.
+
+This package contains reusable modules such as the NNmemoryBankModule which
+can be combined with any lightly model.
+
+"""
+
+# Copyright (c) 2021. Lightly AG and its affiliates.
+# All Rights Reserved
+
+from lightly.models.modules.heads import BarlowTwinsProjectionHead
+from lightly.models.modules.heads import BYOLProjectionHead
+from lightly.models.modules.heads import BYOLPredictionHead
+from lightly.models.modules.heads import DINOProjectionHead
+from lightly.models.modules.heads import MoCoProjectionHead
+from lightly.models.modules.heads import NNCLRProjectionHead
+from lightly.models.modules.heads import NNCLRPredictionHead
+from lightly.models.modules.heads import SimCLRProjectionHead
+from lightly.models.modules.heads import SimSiamProjectionHead
+from lightly.models.modules.heads import SimSiamPredictionHead
+from lightly.models.modules.heads import SwaVProjectionHead
+from lightly.models.modules.heads import SwaVPrototypes
+from lightly.models.modules.nn_memory_bank import NNMemoryBankModule
