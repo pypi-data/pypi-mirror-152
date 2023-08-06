848f00f46dd882fe591ff00fb506453af434bb95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:37 +0000

    Add package Omodem/1.0.3/Omodem-1.0.3-py3-none-any.whl

---
+++ packages/Omodem/Omodem-1.0.3-py3-none-any.whl/orange/utils.py
@@ -0,0 +1,19 @@
+from os import system, name
+from time import sleep
+from requests import post, get
+from bs4 import BeautifulSoup
+
+
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+  
+    else:
+        _ = system('clear')
+  
+
+def restart_local():
+    post("http://192.168.0.1/goform/OrgNetworkRestart",data={"AskRgRestart":1})
+    sleep(10)
+    print("Nouvelle adresse ip publique : " + BeautifulSoup(get("http://192.168.0.1/overview.asp")
+    .text, "html.parser").find('span', attrs={'class': 'severFontSize fontStyle01'}).get_text())
