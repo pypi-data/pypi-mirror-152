a2356edc067790025fd81a7f4f795f271a59e039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:44 +0000

    Add package ms-mint/0.1.6/ms-mint-0.1.6.tar.gz

---
+++ packages/ms-mint/ms-mint-0.1.6.tar.gz/ms-mint-0.1.6/ms_mint/__init__.py
@@ -0,0 +1,12 @@
+"""
+A Python library for large-cohort metabolomics (MS1) processing.
+"""
+
+import os
+from ._version import get_versions
+from .Mint import Mint
+
+__version__ = get_versions()["version"]
+del get_versions
+
+MINT_DATA_PATH = os.path.abspath(os.path.join(__path__[0], "..", "static"))
