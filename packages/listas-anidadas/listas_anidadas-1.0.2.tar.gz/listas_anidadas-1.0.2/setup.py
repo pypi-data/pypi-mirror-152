87d94eec4a39433f3023f5c04d90fd23d28a0408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:55 +0000

    Add package listas-anidadas/1.0.2/listas_anidadas-1.0.2.tar.gz

---
+++ packages/listas-anidadas/listas_anidadas-1.0.2.tar.gz/listas_anidadas-1.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+# Lee el contenido del fichero README
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README").read_text()
+
+setup(
+        name            = 'listas_anidadas',
+        version         = '1.0.2',
+        py_modules      = ['listas_anidadas'],
+        author          = 'Fortinux',
+        author_email    = 'info@fortinux.com',
+        url             = 'https://fortinux.com',
+        description     = 'Función que imprime listas anidadas',
+        long_description=long_description,
+        long_description_content_type='text/markdown'
+    )
