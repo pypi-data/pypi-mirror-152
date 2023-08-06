34c2bd09e0d7ba87d423a40a748c2e7915d8afbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:09 +0000

    Add package prompt-engineering/0.0.5/prompt_engineering-0.0.5-py3-none-any.whl

---
+++ packages/prompt-engineering/prompt_engineering-0.0.5-py3-none-any.whl/prompt_engineering/__init__.py
@@ -0,0 +1,5 @@
+from prompt_engineering.decorators import example, description, task, api
+from prompt_engineering.prompts import list_tasks
+from prompt_engineering.utils import to_csv, from_csv
+
+__version__ = "0.0.5"
