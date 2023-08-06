da9e13e6a5306fe845a032599e86fa0899bb1c3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:12 +0000

    Add package lywt/0.0.6/lywt-0.0.6.tar.gz

---
+++ packages/lywt/lywt-0.0.6.tar.gz/lywt-0.0.6/lywt/file.py
@@ -0,0 +1,4 @@
+def funFile():
+    print('this is a fun in file')
+
+print('this is file.py')
---
+++ packages/lywt/lywt-0.0.6.tar.gz/lywt-0.0.6/lywt/musicEnjoy.py
@@ -0,0 +1,6 @@
+def music():
+    print('this is a fun in musicEnjoy')
+
+print('this is musicEnjoy.py')
+
+# 有点甜
