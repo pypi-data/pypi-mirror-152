d052c9e2e1d2075a724fbfeb6382f08b4a7bb7b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:44 +0000

    Add package baize/0.17.1/baize-0.17.1-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/baize/baize-0.17.1-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/baize/typing.py
@@ -0,0 +1,71 @@
+import sys
+from types import TracebackType
+from typing import (
+    Any,
+    Awaitable,
+    Callable,
+    Iterable,
+    List,
+    MutableMapping,
+    Optional,
+    Tuple,
+    Type,
+)
+
+if sys.version_info[:2] < (3, 8):  # pragma: py-gte-38
+    from typing_extensions import Final, Literal, Protocol, TypedDict, final
+else:  # pragma: py-lt-38
+    from typing import Final, Literal, Protocol, TypedDict, final
+
+__all__ = [
+    "Scope",
+    "Message",
+    "Receive",
+    "Send",
+    "ASGIApp",
+    "ExcInfo",
+    "Environ",
+    "StartResponse",
+    "WSGIApp",
+    # built-in types
+    "TypedDict",
+    "Literal",
+    "Final",
+    "final",
+    "Protocol",
+]
+
+# ASGI
+Scope = MutableMapping[str, Any]
+
+Message = MutableMapping[str, Any]
+
+Receive = Callable[[], Awaitable[Message]]
+
+Send = Callable[[Message], Awaitable[None]]
+
+ASGIApp = Callable[[Scope, Receive, Send], Awaitable[None]]
+
+# WSGI: view PEP3333
+Environ = MutableMapping[str, Any]
+
+ExcInfo = Tuple[Type[BaseException], BaseException, Optional[TracebackType]]
+
+
+class StartResponse(Protocol):
+    def __call__(
+        self,
+        status: str,
+        response_headers: List[Tuple[str, str]],
+        exc_info: Optional[ExcInfo] = None,
+    ) -> None:
+        ...  # pragma: no cover
+
+
+WSGIApp = Callable[[Environ, StartResponse], Iterable[bytes]]
+
+# Server-sent Event
+# https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events
+ServerSentEvent = TypedDict(
+    "ServerSentEvent", {"event": str, "data": str, "id": str, "retry": int}, total=False
+)
