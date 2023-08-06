d428a38529f3e67ad41795a15f2e5a6ee25965b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:19 +0000

    Add package azc1/0.0.1/azc1-0.0.1-py3-none-any.whl

---
+++ packages/azc1/azc1-0.0.1-py3-none-any.whl/azc/mod/func.py
@@ -0,0 +1,11 @@
+import time
+
+class FuncTime(object):
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        t1 = time.time()
+        res = self.func(*args, **kwargs)
+        t2 = time.time()
+        print("函数执行时长:"+ str(t2 - t1))
