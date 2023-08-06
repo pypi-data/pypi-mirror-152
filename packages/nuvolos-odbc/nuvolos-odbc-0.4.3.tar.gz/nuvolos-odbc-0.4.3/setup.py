453be3617d6af27d0bfb2bc0ef11bc1127c8636e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:31 +0000

    Add package nuvolos-odbc/0.4.3/nuvolos-odbc-0.4.3.tar.gz

---
+++ packages/nuvolos-odbc/nuvolos-odbc-0.4.3.tar.gz/nuvolos-odbc-0.4.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+
+exec(open("nuvolos/version.py").read())
+setup(
+    name="nuvolos-odbc",
+    version=__version__,
+    description="The Nuvolos python library for database connectivity, internal PyODBC-based version",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/nuvolos-cloud/python-connector-odbc",
+    author="Alphacruncher",
+    author_email="support@nuvolos.cloud",
+    license="MIT",
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=True,
+    install_requires=["keyring", "pyodbc", "pandas", "pyarrow"],
+    zip_safe=False,
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+)
