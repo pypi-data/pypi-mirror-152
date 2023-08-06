47ab85b4c339f57b543d7604b0b18f58e45672b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:35 +0000

    Add package pyeda-pranav/0.0.1/pyeda_pranav-0.0.1.tar.gz

---
+++ packages/pyeda-pranav/pyeda_pranav-0.0.1.tar.gz/pyeda_pranav-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'pyeda for private use'
+
+# Setting up
+setup(
+    name="pyeda_pranav",
+    version=VERSION,
+    author="Pranav Sinha",
+    author_email="<pranavs1997@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
