e88334456983cfeedfb10ff9c344cd5fa1852557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:21 +0000

    Add package coherency/0.0.0/coherency-0.0.0.tar.gz

---
+++ packages/coherency/coherency-0.0.0.tar.gz/coherency-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+if __name__ == '__main__':
+    setup(
+        name="coherency",
+        author="jon-edward",
+        version="0.0.0",
+        packages=find_packages()
+    )
