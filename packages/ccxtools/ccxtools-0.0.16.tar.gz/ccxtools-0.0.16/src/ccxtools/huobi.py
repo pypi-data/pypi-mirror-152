42d78447b31e44798ff080e4d8cb71bffb299095
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:10 +0000

    Add package ccxtools/0.0.16/ccxtools-0.0.16.tar.gz

---
+++ packages/ccxtools/ccxtools-0.0.16.tar.gz/ccxtools-0.0.16/src/ccxtools/huobi.py
@@ -0,0 +1,22 @@
+import ccxt
+from ccxtools.exchange import CcxtExchange
+
+
+class Huobi(CcxtExchange):
+
+    def __init__(self, who, market, config):
+        super().__init__(market)
+
+        if market == 'USDT':
+            self.ccxt_inst = ccxt.huobi({
+                'apiKey': config(f'HUOBI_API_KEY{who}'),
+                'secret': config(f'HUOBI_SECRET_KEY{who}'),
+                'options': {
+                    'defaultType': 'swap',
+                    'defaultSubType': 'linear'
+                }
+            })
+
+    def get_balance(self, ticker):
+        response = self.ccxt_inst.contractPrivatePostLinearSwapApiV1SwapCrossAccountPositionInfo(params={'margin_account': 'USDT'})
+        return float(response['data']['margin_balance'])
