e595a7753d63dcb23e1cc075829f9ed1c7a55eb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:18 +0000

    Add package adbnx-adapter/4.0.0/adbnx_adapter-4.0.0-py3-none-any.whl

---
+++ packages/adbnx-adapter/adbnx_adapter-4.0.0-py3-none-any.whl/adbnx_adapter/abc.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from abc import ABC
+from typing import Any, List, Set
+
+from arango.graph import Graph as ADBGraph
+from networkx.classes.graph import Graph as NXGraph
+from networkx.classes.multidigraph import MultiGraph as NXMultiDiGraph
+
+from .typings import ArangoMetagraph, Json, NxData, NxId
+
+
+class Abstract_ADBNX_Adapter(ABC):
+    def __init__(self) -> None:
+        raise NotImplementedError  # pragma: no cover
+
+    def arangodb_to_networkx(
+        self,
+        name: str,
+        metagraph: ArangoMetagraph,
+        is_keep: bool = True,
+        **query_options: Any,
+    ) -> NXMultiDiGraph:
+        raise NotImplementedError  # pragma: no cover
+
+    def arangodb_collections_to_networkx(
+        self,
+        name: str,
+        v_cols: Set[str],
+        e_cols: Set[str],
+        **query_options: Any,
+    ) -> NXMultiDiGraph:
+        raise NotImplementedError  # pragma: no cover
+
+    def arangodb_graph_to_networkx(
+        self, name: str, **query_options: Any
+    ) -> NXMultiDiGraph:
+        raise NotImplementedError  # pragma: no cover
+
+    def networkx_to_arangodb(
+        self,
+        name: str,
+        nx_graph: NXGraph,
+        edge_definitions: List[Json],
+        batch_size: int = 1000,
+        keyify_nodes: bool = False,
+        keyify_edges: bool = False,
+    ) -> ADBGraph:
+        raise NotImplementedError  # pragma: no cover
+
+    def __validate_attributes(self) -> None:
+        raise NotImplementedError  # pragma: no cover
+
+    def __fetch_adb_docs(self) -> None:
+        raise NotImplementedError  # pragma: no cover
+
+    def __insert_adb_docs(self) -> None:
+        raise NotImplementedError  # pragma: no cover
+
+    @property
+    def METAGRAPH_ATRIBS(self) -> Set[str]:
+        return {"vertexCollections", "edgeCollections"}
+
+    @property
+    def EDGE_DEFINITION_ATRIBS(self) -> Set[str]:
+        return {"edge_collection", "from_vertex_collections", "to_vertex_collections"}
+
+
+class Abstract_ADBNX_Controller(ABC):
+    def _prepare_arangodb_vertex(self, adb_vertex: Json, col: str) -> None:
+        raise NotImplementedError  # pragma: no cover
+
+    def _prepare_arangodb_edge(self, adb_edge: Json, col: str) -> None:
+        raise NotImplementedError  # pragma: no cover
+
+    def _identify_networkx_node(
+        self, nx_node_id: NxId, nx_node: NxData, adb_v_cols: List[str]
+    ) -> str:
+        raise NotImplementedError  # pragma: no cover
+
+    def _identify_networkx_edge(
+        self,
+        nx_edge: NxData,
+        from_nx_node: NxData,
+        to_nx_node: NxData,
+        adb_e_cols: List[str],
+    ) -> str:
+        raise NotImplementedError  # pragma: no cover
+
+    def _keyify_networkx_node(self, nx_node_id: NxId, nx_node: NxData, col: str) -> str:
+        raise NotImplementedError  # pragma: no cover
+
+    def _keyify_networkx_edge(
+        self,
+        nx_edge: NxData,
+        from_nx_node: NxData,
+        to_nx_node: NxData,
+        col: str,
+    ) -> str:
+        raise NotImplementedError  # pragma: no cover
+
+    @property
+    def VALID_KEY_CHARS(self) -> Set[str]:
+        return {
+            "_",
+            "-",
+            ":",
+            ".",
+            "@",
+            "(",
+            ")",
+            "+",
+            ",",
+            "=",
+            ";",
+            "$",
+            "!",
+            "*",
+            "'",
+            "%",
+        }
