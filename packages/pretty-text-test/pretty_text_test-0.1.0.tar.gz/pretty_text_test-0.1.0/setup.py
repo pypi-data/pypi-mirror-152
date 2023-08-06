464dd013300ab3c1dacb4cf16fa1004b98e59309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:30 +0000

    Add package pretty-text-test/0.1.0/pretty_text_test-0.1.0.tar.gz

---
+++ packages/pretty-text-test/pretty_text_test-0.1.0.tar.gz/pretty_text_test-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pretty_text_test']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pyfiglet>=0.8.post1,<0.9', 'termcolor>=1.1.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'pretty-text-test',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
