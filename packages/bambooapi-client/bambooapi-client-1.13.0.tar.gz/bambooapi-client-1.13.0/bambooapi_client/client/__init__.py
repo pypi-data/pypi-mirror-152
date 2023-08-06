c45e3e8f53ee2856fad0343081ec2f8d1b5a086f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:53 +0000

    Add package bambooapi-client/1.13.0/bambooapi-client-1.13.0.tar.gz

---
+++ packages/bambooapi-client/bambooapi-client-1.13.0.tar.gz/bambooapi-client-1.13.0/bambooapi_client/client/__init__.py
@@ -0,0 +1,6 @@
+from .bambooapi_client import BambooAPIClient
+from .contracts_api import ContractsApi
+from .markets_api import MarketsApi
+from .sites_api import SitesApi
+from .users_api import UsersApi
+from .weather_api import WeatherApi
