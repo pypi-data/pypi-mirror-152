655e483e5f1acdb1bba68af6bb4725132a9f3ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:26 +0000

    Add package flybirds/0.2.4/flybirds-0.2.4-py3-none-any.whl

---
+++ packages/flybirds/flybirds-0.2.4-py3-none-any.whl/flybirds/core/driver/ui_driver.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+ui driver proxy
+"""
+from flybirds.core.global_context import GlobalContext
+
+
+def air_bdd_screen_size(dr_instance):
+    return GlobalContext.ui_driver.air_bdd_screen_size(dr_instance)
+
+
+def init_driver():
+    return GlobalContext.ui_driver.init_driver()
+
+
+def close_driver():
+    return GlobalContext.ui_driver.close_driver()
