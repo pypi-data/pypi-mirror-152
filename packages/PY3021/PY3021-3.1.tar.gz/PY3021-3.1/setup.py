6c8160d8556b88b558766a880f9d8051a92ecd54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:15:41 +0000

    Add package PY3021/3.1/PY3021-3.1.tar.gz

---
+++ packages/PY3021/PY3021-3.1.tar.gz/PY3021-3.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+setup(
+    name = 'PY3021',#模块名
+    version = '3.1', #版本
+    packages = find_packages(), #目录所有文件
+    url='https://xxx.com', #文件文档下载地址
+    author='PY3021', #作者名
+    author_email='111@qq.com', #邮箱
+)
+
+# python setup.py build sdist bdist_wheel
+# twine upload dist/*
