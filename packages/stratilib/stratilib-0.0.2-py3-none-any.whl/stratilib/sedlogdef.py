471bf4fd4fa529256469263fc741f93825171588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:26 +0000

    Add package stratilib/0.0.2/stratilib-0.0.2-py3-none-any.whl

---
+++ packages/stratilib/stratilib-0.0.2-py3-none-any.whl/stratilib/sedlogdef.py
@@ -0,0 +1,63 @@
+#Variables para 'BASE BOUNDARY'
+DICT_SL_CONTACTS = {'S':'Sharp',
+                    'E':'Erosion',
+                    'G':'Gradational'}
+
+#Variables para SYMBOLS IN BED' y 'SYMBOLS/STRUCTURES'
+DICT_SL_STRUCTURES = {'r':'Current ripple cross-lamination',
+                   't':'Trough cross bedding',
+                   'p':'Planar cross bedding',
+                   'x1':'Fine ash',
+                   'x1':'Coarse ash'}
+
+#Variables para 'GRAIN SIZE BASE' y 'GRAIN SIZE TOP'
+DICT_SL_GRAINS = {'cl':'clay',
+                  'cls':'clay//silt',
+                  's':'silt',
+                  'svf':'silt//vf',
+                  'vf':'vf',
+                  'vff':'vf//f',
+                  'f':'f',
+                  'fm':'f//m',
+                  'm':'m',
+                  'mv':'m//c',
+                  'c':'c',
+                  'cvc':'c//vc',
+                  'vc':'vc',
+                  'vcg':'vc//granule',
+                  'g':'granule',
+                  'gp':'granule//pebble',
+                  'p':'pebble',
+                  'pco':'pebble//cobble',
+                  'co':'cobble',
+                  'cob':'cobble//boulder',
+                  'b':'boulder'}
+
+
+#Según tipos presentes en SEDLOG
+
+DICT_SL_LITHO = {'M':'Mudstone',
+              'x1':'Claystone',
+              'x1':'Shale',
+              'x1':'Siltstone',
+              'S':'Sandstone',
+              'G':'Conglomerate',
+              'x1':'Coal',
+              'x1':'Limestone',
+              'x1':'Chert',             
+              'x1':'Volcaniclastic',
+              
+              'x1':'Lime mudstone',
+              'x1':'Wackestone',
+              'x1':'Packestone',
+              'x1':'Grainstone',
+              'x1':'Halite',
+              'Gy':'Gypsum/Anhydrite',
+              'D':'Dolomite',
+              
+              'B':'Breccia',
+              'x1':'Matrix-supported conglomerate',
+              'x1':'Clast-supported conglomerate',
+              'x1':'Lava',
+              'x1':'Fine ash',
+              'x1':'Coarse ash'}
