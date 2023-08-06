fdc2ff312c7b73258299ec223f4256b36ce8e1d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:19:09 +0000

    Add package aiopayok/0.1.1/aiopayok-0.1.1.tar.gz

---
+++ packages/aiopayok/aiopayok-0.1.1.tar.gz/aiopayok-0.1.1/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['aiopayok', 'aiopayok.models']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiohttp>=3.8.1,<4.0.0',
+ 'certifi>=2022.5.18,<2023.0.0',
+ 'pydantic>=1.9.1,<2.0.0']
+
+setup_kwargs = {
+    'name': 'aiopayok',
+    'version': '0.1.1',
+    'description': 'payok.io asynchronous python wrapper',
+    'long_description': None,
+    'author': 'layerqa',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/layerqa/aiopayok',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
