0683d605151fba574582175bcf15c1d5f40fd8fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:52 +0000

    Add package niit3/1.0/niit3-1.0.tar.gz

---
+++ packages/niit3/niit3-1.0.tar.gz/niit3-1.0/setup.py
@@ -0,0 +1,9 @@
+from  distutils.core import setup
+setup(
+    name='niit3',
+    version='1.0',
+    description='本模块用于测试',
+    author='niit3_all',
+    author_email='5257444@qq.com',
+    py_module=['niit3.student']
+)
