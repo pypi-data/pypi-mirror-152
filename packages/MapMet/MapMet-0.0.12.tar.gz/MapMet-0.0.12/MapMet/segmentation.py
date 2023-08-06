6db61edf1c72f4eae23079ffc43a5c2a9b8aa27e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:50 +0000

    Add package MapMet/0.0.12/MapMet-0.0.12.tar.gz

---
+++ packages/MapMet/MapMet-0.0.12.tar.gz/MapMet-0.0.12/MapMet/segmentation.py
@@ -0,0 +1,8 @@
+class Segmentation():
+
+    def __init__(self):
+        print("Hey, sorry I cannot segment anything rightnow :(")
+        self.is_sorry = "actually not"
+
+    def are_you_sorry(self):
+        print(self.is_sorry)
