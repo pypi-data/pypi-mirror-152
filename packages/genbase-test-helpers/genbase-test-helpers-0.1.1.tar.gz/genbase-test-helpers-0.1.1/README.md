73a5065d13185b9e8b195adb54386f3a3670835f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:45:17 +0000

    Add package genbase-test-helpers/0.1.1/genbase-test-helpers-0.1.1.tar.gz

---
+++ packages/genbase-test-helpers/genbase-test-helpers-0.1.1.tar.gz/genbase-test-helpers-0.1.1/README.md
@@ -0,0 +1,4 @@
+# genbase-test-helpers
+Test helpers for [`genbase`](https://git.science.uu.nl/m.j.robeer/genbase/) and its dependent packages.
+
+&copy; Marcel Robeer, 2022.
---
+++ packages/genbase-test-helpers/genbase-test-helpers-0.1.1.tar.gz/genbase-test-helpers-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+  name='genbase-test-helpers',
+  version='0.1.1',
+  author='Marcel Robeer',
+  packages=['genbase_test_helpers'],
+  description='Genbase generic test helpers',
+  install_requires=[
+      "instancelib>=0.4.4.1",
+  ],
+)
