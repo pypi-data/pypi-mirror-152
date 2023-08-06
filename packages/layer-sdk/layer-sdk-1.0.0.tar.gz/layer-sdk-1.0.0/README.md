5b70f39dec031942ef63e9590e103283b67e7ac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:09 +0000

    Add package layer-sdk/1.0.0/layer-sdk-1.0.0.tar.gz

---
+++ packages/layer-sdk/layer-sdk-1.0.0.tar.gz/layer-sdk-1.0.0/README.md
@@ -0,0 +1,2 @@
+# obsolete-layer-sdk
+The skeleton project for the obsolete layer-sdk PyPI package for backwards compatibility
---
+++ packages/layer-sdk/layer-sdk-1.0.0.tar.gz/layer-sdk-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="layer-sdk",
+    description="layer-sdk is now layer",
+    long_description='''Use `layer <https://pypi.python.org/pypi/layer/>` instead.''',
+    long_description_content_type="text/markdown",
+    version="1.0.0",
+    install_requires=["layer"],
+    classifiers=["Development Status :: 7 - Inactive"],
+    url='https://pypi.python.org/pypi/layer/',
+)
+
