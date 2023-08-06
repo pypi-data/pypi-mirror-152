ceba41999a6e85a95bc34db56e70251f2a0dd229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:58 +0000

    Add package datascience-gear/0.1.6.1/datascience_gear-0.1.6.1.tar.gz

---
+++ packages/datascience-gear/datascience_gear-0.1.6.1.tar.gz/datascience_gear-0.1.6.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['datascience_gear']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'datascience-gear',
+    'version': '0.1.6.1',
+    'description': '',
+    'long_description': None,
+    'author': 'sinclairfr',
+    'author_email': 'sixfoursuited@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
