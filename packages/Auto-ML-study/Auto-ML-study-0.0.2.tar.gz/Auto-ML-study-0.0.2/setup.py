c8d3dc614e88f144fb72eba0913d0f77e5dcf6d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:41 +0000

    Add package Auto-ML-study/0.0.2/Auto-ML-study-0.0.2.tar.gz

---
+++ packages/Auto-ML-study/Auto-ML-study-0.0.2.tar.gz/Auto-ML-study-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Auto-ML-study", # Replace with your own username
+    version="0.0.2",
+    author="EunJae Yong (Jay Yong)",
+    author_email="jayyong.dev@gmail.com",
+    description="This library was created for students studying autoML.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dev-jay-yong/Auto-DL-Study",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
