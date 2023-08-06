544cf9f0070ce7e0975d822d61ec667886d740ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:50 +0000

    Add package pretalx-hitalx/0.0.11/pretalx-hitalx-0.0.11.tar.gz

---
+++ packages/pretalx-hitalx/pretalx-hitalx-0.0.11.tar.gz/pretalx-hitalx-0.0.11/pretalx_hitalx/signals.py
@@ -0,0 +1,54 @@
+# Register your receivers here
+from django.dispatch import receiver
+from django.urls import resolve, reverse
+from django.utils.translation import ugettext as _
+from pretalx.common.signals import activitylog_display
+from pretalx.orga.signals import nav_event
+
+
+@receiver(nav_event, dispatch_uid="hitalx_nav")
+def navbar_info(sender, request, **kwargs):
+    url = resolve(request.path_info)
+    return [
+        {
+            "label": _("Speaker expenses"),
+            "icon": "dollar",
+            "url": reverse(
+                "plugins:pretalx_hitalx:speakers_by_expense.view",
+                kwargs={
+                    "event": request.event.slug,
+                },
+            ),
+            "active": url.namespace == "plugins:pretalx_hitalx" and url.url_name == "speakers_by_expense.view",
+        },
+        {
+            'label': _('Tours'),
+            'icon': 'bus',
+            'url': reverse('plugins:pretalx_hitalx:tours.view', kwargs={
+                'event': request.event.slug,
+            }),
+            'active': url.namespace == 'plugins:pretalx_hitalx' and url.url_name == 'tours.view',
+        },
+        {
+            'label': _('Tours export'),
+            'icon': 'bus',
+            'url': reverse('plugins:pretalx_hitalx:tours.export', kwargs={
+                'event': request.event.slug,
+            }),
+            'active': url.namespace == 'plugins:pretalx_hitalx' and url.url_name == 'tours.export',
+        }
+    ]
+
+
+PLUGIN_ACTION_TYPES = {
+    "pretalx_hitalx.expense_item.edit": _("Speaker expense was edited"),
+    "pretalx_hitalx.expense_item.create": _("Speaker expense was created"),
+    "pretalx_hitalx.expense_item.mark": _("Expense paid status was changed"),
+}
+
+
+@receiver(activitylog_display)
+def default_activitylog_display(sender, activitylog, **kwargs):
+    if activitylog.action_type in PLUGIN_ACTION_TYPES:
+        return f"{PLUGIN_ACTION_TYPES[activitylog.action_type]} ({activitylog.data})"
+
