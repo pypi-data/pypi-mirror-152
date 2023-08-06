5094adfddadb1ae96605e5e9a80e0df12bc1bfa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:21 +0000

    Add package VorGemeinde/0.0.1/VorGemeinde-0.0.1.tar.gz

---
+++ packages/VorGemeinde/VorGemeinde-0.0.1.tar.gz/VorGemeinde-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup , find_packages
+
+setup(
+    name='VorGemeinde',
+    version='0.0.1',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='Michael',
+    author_email='michael.mayer@student.htldornbirn.at',
+    description='a Vorarlberg Python Package',
+    keywords = ['python', 'Vorarlberg', 'Gemeinde'],
+    classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Education",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 3",
+    "Operating System :: MacOS :: MacOS X",
+    "Operating System :: Microsoft :: Windows",
+]
+)
