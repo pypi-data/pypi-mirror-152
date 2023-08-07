5768cd2c3a0c2022809c3ebab9c7d71e8645dd75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:44:44 +0000

    Add package EasyUtils-SPeak/0.0.4/EasyUtils_SPeak-0.0.4-py3-none-any.whl

---
+++ packages/EasyUtils-SPeak/EasyUtils_SPeak-0.0.4-py3-none-any.whl/test/__init__.py
@@ -0,0 +1,11 @@
+'''
+Author: SPeak
+Date: 2022-05-03 17:43:15
+LastEditors: SPeak
+LastEditTime: 2022-05-03 17:52:18
+FilePath: /EasyUtils/src/test/__init__.py
+Description: 
+
+'''
+
+from .MyTestTask import WTask, RTask, RWTask
