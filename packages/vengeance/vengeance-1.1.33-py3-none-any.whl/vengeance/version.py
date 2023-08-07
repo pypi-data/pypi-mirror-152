9425c1b0ea9052fad2e23d5c147dfbc3a6315056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:44:45 +0000

    Add package vengeance/1.1.33/vengeance-1.1.33-py3-none-any.whl

---
+++ packages/vengeance/vengeance-1.1.33-py3-none-any.whl/vengeance/version.py
@@ -0,0 +1,7 @@
+
+""" https://github.com/michael-ross-ven/vengeance/blob/master/releases.log """
+__version__ = '1.1.33'
+__release__ = '$release 70'
+
+__all__ = ['__version__',
+           '__release__']
