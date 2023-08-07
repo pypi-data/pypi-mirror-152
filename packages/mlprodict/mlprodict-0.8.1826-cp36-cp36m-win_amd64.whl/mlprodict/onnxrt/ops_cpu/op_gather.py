9bf10d1337a5a05c1d24a696786ac01f72627b55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:40:47 +0000

    Add package mlprodict/0.8.1826/mlprodict-0.8.1826-cp36-cp36m-win_amd64.whl

---
+++ packages/mlprodict/mlprodict-0.8.1826-cp36-cp36m-win_amd64.whl/mlprodict/onnxrt/ops_cpu/op_gather.py
@@ -0,0 +1,43 @@
+# -*- encoding: utf-8 -*-
+# pylint: disable=E0203,E1101,C0111
+"""
+@file
+@brief Runtime operator.
+"""
+import numpy
+from ._op import OpRun
+from ..shape_object import ShapeObject
+from .op_gather_ import (  # pylint: disable=E0611,E0401
+    GatherFloat, GatherDouble, GatherInt64)
+
+
+class Gather(OpRun):
+
+    atts = {'axis': 0}
+
+    def __init__(self, onnx_node, desc=None, **options):
+        OpRun.__init__(self, onnx_node, desc=desc,
+                       expected_attributes=Gather.atts,
+                       **options)
+        self.rt_ = {
+            'float32': GatherFloat(self.axis),
+            'float64': GatherDouble(self.axis),
+            'int64': GatherInt64(self.axis)}
+
+    def _run(self, x, indices, verbose=0, fLOG=None):  # pylint: disable=W0221
+        if not x.flags['C_CONTIGUOUS']:
+            x = numpy.ascontiguousarray(x)
+        if not indices.flags['C_CONTIGUOUS']:
+            indices = indices.ascontiguousarray()
+        if indices.size == 0:
+            return (numpy.empty((0, ), dtype=x.dtype), )
+        try:
+            return (self.rt_[str(x.dtype)].compute(x, indices), )
+        except (KeyError, ValueError):
+            return (numpy.take(x, indices, axis=self.axis), )
+
+    def _infer_shapes(self, x, indices):  # pylint: disable=E0202,W0221
+        return (ShapeObject.gather_shape(x, indices, self.axis), )
+
+    def _infer_types(self, data, indices):  # pylint: disable=W0221
+        return (data, )
---
+++ packages/mlprodict/mlprodict-0.8.1826-cp36-cp36m-win_amd64.whl/mlprodict/onnxrt/ops_cpu/op_squeeze.py
@@ -0,0 +1,88 @@
+# -*- encoding: utf-8 -*-
+# pylint: disable=E0203,E1101,C0111
+"""
+@file
+@brief Runtime operator.
+"""
+import numpy
+from onnx.defs import onnx_opset_version
+from ..shape_object import ShapeObject
+from ._op import OpRunUnaryNum, OpRun
+
+
+class Squeeze_1(OpRunUnaryNum):
+
+    atts = {'axes': [], 'keepdims': 1}
+
+    def __init__(self, onnx_node, desc=None, **options):
+        OpRunUnaryNum.__init__(self, onnx_node, desc=desc,
+                               expected_attributes=Squeeze_1.atts,
+                               **options)
+        if isinstance(self.axes, numpy.ndarray):
+            self.axes = tuple(self.axes)
+        elif self.axes in [[], tuple()]:
+            self.axes = None
+        elif isinstance(self.axes, list):
+            self.axes = tuple(self.axes)
+
+    def _run(self, data, verbose=0, fLOG=None):  # pylint: disable=W0221
+        if isinstance(self.axes, (tuple, list)):
+            sq = data
+            for a in reversed(self.axes):
+                sq = numpy.squeeze(sq, axis=a)
+        else:
+            sq = numpy.squeeze(data, axis=self.axes)
+        return (sq, )
+
+    def _infer_shapes(self, x):  # pylint: disable=W0221
+        return (x.squeeze(axis=self.axes), )
+
+    def _infer_types(self, x):  # pylint: disable=W0221
+        return (x, )
+
+    def _infer_sizes(self, *args, **kwargs):
+        res = self.run(*args, **kwargs)
+        return (dict(temp=0), ) + res
+
+
+class Squeeze_11(Squeeze_1):
+    pass
+
+
+class Squeeze_13(OpRun):
+
+    atts = {'keepdims': 1}
+
+    def __init__(self, onnx_node, desc=None, **options):
+        OpRun.__init__(self, onnx_node, desc=desc,
+                       expected_attributes=Squeeze_13.atts,
+                       **options)
+        self.axes = None
+
+    def _run(self, data, axes=None, verbose=0, fLOG=None):  # pylint: disable=W0221
+        if axes is not None:
+            if hasattr(axes, '__iter__'):
+                sq = numpy.squeeze(data, axis=tuple(axes))
+            else:
+                sq = numpy.squeeze(data, axis=axes)
+        else:
+            sq = numpy.squeeze(data)
+        return (sq, )
+
+    def _infer_shapes(self, x, axes=None):  # pylint: disable=W0221
+        return (ShapeObject(None, dtype=x.dtype), )
+
+    def _infer_types(self, x, axes=None):  # pylint: disable=W0221
+        return (x, )
+
+    def _infer_sizes(self, *args, **kwargs):
+        res = self.run(*args, **kwargs)
+        return (dict(temp=0), ) + res
+
+
+if onnx_opset_version() >= 13:
+    Squeeze = Squeeze_13
+elif onnx_opset_version() >= 11:  # pragma: no cover
+    Squeeze = Squeeze_11
+else:  # pragma: no cover
+    Squeeze = Squeeze_1
