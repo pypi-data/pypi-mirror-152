992d377be1b41309e79d7cf2f201853e3e6b4cb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:45:29 +0000

    Add package StructEngPy/0.1.10/StructEngPy-0.1.10.tar.gz

---
+++ packages/StructEngPy/StructEngPy-0.1.10.tar.gz/StructEngPy-0.1.10/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+VERSION = '0.1.10'
+
+setup(
+    name='StructEngPy', 
+    version=VERSION, 
+    author='Zhuoju Huang',
+    author_email='zhuoju36@hotmail.com',
+    license='MIT',
+    description='package for structural engineering', 
+    packages=find_packages(),
+    zip_safe=False,
+    python_requires=">=3.6, <=3.8",
+    install_requires=[
+        'numpy>=1.21',
+        'scipy>=1.7',
+        'quadpy>=0.16',
+        'mpmath==1.2.1',
+    ],
+)
