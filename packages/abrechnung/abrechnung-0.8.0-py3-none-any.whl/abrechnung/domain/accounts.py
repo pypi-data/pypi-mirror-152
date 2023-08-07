6fb701a5c5a36321c35279a9a21b125e7916f61c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:40:39 +0000

    Add package abrechnung/0.8.0/abrechnung-0.8.0-py3-none-any.whl

---
+++ packages/abrechnung/abrechnung-0.8.0-py3-none-any.whl/abrechnung/domain/accounts.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+from enum import Enum
+from typing import Optional
+
+
+class AccountType(Enum):
+    personal = "personal"
+    clearing = "clearing"
+
+
+@dataclass
+class AccountDetails:
+    name: str
+    description: str
+    priority: int
+    owning_user_id: Optional[int]
+    deleted: bool
+
+    changed_by: int
+    committed_at: Optional[datetime]
+
+    clearing_shares: Optional[dict[int, float]] = field(default=None)
+
+
+@dataclass
+class Account:
+    id: int
+    group_id: int
+    type: str
+    is_wip: bool
+    last_changed: datetime
+    version: int
+    committed_details: Optional[AccountDetails]
+    pending_details: Optional[AccountDetails]
