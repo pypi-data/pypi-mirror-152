a231cef5c02d7d5ca1fcb85bb3a3050f5b9ab5bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:08:45 +0000

    Add package fwhunt-scan/2.0.0/fwhunt_scan-2.0.0-py3-none-any.whl

---
+++ packages/fwhunt-scan/fwhunt_scan-2.0.0-py3-none-any.whl/fwhunt_scan/__init__.pyi
@@ -0,0 +1,3 @@
+from .uefi_analyzer import UefiAnalyzer as UefiAnalyzer, UefiAnalyzerError as UefiAnalyzerError
+from .uefi_scanner import UefiRule as UefiRule, UefiScanner as UefiScanner
+from .uefi_te import TerseExecutableParser as TerseExecutableParser
