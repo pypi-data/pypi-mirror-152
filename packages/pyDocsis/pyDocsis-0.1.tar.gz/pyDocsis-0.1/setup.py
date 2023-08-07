8ce55f0ce0785b36c585a957ae53478bb6f6a492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:32 +0000

    Add package pyDocsis/0.1/pyDocsis-0.1.tar.gz

---
+++ packages/pyDocsis/pyDocsis-0.1.tar.gz/pyDocsis-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyDocsis',
+    version='0.1',
+    packages=['pydocsis'],
+    url='https://github.com/botheredtodd/pyDocsis',
+    license='LGPL-3.0 license',
+    author='Todd Balsley',
+    author_email='todd.balsley@cox.com',
+    description='A library for working with docsis files'
+)
