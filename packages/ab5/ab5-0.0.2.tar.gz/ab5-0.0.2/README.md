1cef72634cfbe95d5ee4630b8e1943882526e8c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:15 +0000

    Add package ab5/0.0.2/ab5-0.0.2.tar.gz

---
+++ packages/ab5/ab5-0.0.2.tar.gz/ab5-0.0.2/README.md
@@ -0,0 +1,7 @@
+UwU
+
+example:
+```py
+from ab5 import gratient
+print(gratient(('A'*100+'\n')*10,[0,223,50],[0,25,222]))
+```
---
+++ packages/ab5/ab5-0.0.2.tar.gz/ab5-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+__name__ = "ab5"
+__version__ = "0.0.2"
+
+setup(
+    name=__name__,
+    version=__version__,
+    author="Ab.5#3363",
+    author_email="<later@later.com>",
+    description="cool things",
+    long_description_content_type="text/markdown",
+    long_description=open("README.md", encoding="utf-8").read(),
+    url = "https://github.com/xxa2005/",
+    install_requires=[],
+    keywords=['color', 'python', 'colors', 'cool', 'gratient', 'fade', 'shadow'],
+    classifiers=[
+      "Intended Audience :: Developers",
+      "Programming Language :: Python :: 3",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+    ]
+)
