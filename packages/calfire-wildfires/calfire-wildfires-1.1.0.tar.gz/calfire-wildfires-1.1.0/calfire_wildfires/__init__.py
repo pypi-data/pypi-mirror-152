f7488a2c2d51707e4539c89c4bc52272f7dfcb36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:01:00 +0000

    Add package calfire-wildfires/1.1.0/calfire-wildfires-1.1.0.tar.gz

---
+++ packages/calfire-wildfires/calfire-wildfires-1.1.0.tar.gz/calfire-wildfires-1.1.0/calfire_wildfires/__init__.py
@@ -0,0 +1,30 @@
+"""
+Download wildfires data from CalFire
+"""
+import requests
+
+
+def get_active_fires():
+    """Get the latest ative fires from CalFire.
+
+    Returns GeoJSON with point geometry
+    """
+    # Request data
+    r = requests.get("https://www.fire.ca.gov/umbraco/api/IncidentApi/GeoJsonList?inactive=false")
+    if r.status_code != 200:
+        raise Exception(f"Request for data failed with {r.status_code} status code")
+    # Return it
+    return r.json()
+
+
+def get_all_fires():
+    """Get all active and inactive fires year to date from CalFire.
+
+    Returns GeoJSON with point geometry
+    """
+    # Request data
+    r = requests.get("https://www.fire.ca.gov/umbraco/api/IncidentApi/GeoJsonList")
+    if r.status_code != 200:
+        raise Exception(f"Request for data failed with {r.status_code} status code")
+    # Return it
+    return r.json()
