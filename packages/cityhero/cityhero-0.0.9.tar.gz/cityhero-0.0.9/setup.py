bec78f1e1ee3be25c88ebf862c2927f6023d8346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:03:49 +0000

    Add package cityhero/0.0.9/cityhero-0.0.9.tar.gz

---
+++ packages/cityhero/cityhero-0.0.9.tar.gz/cityhero-0.0.9/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import setuptools
+
+setuptools.setup(
+    name='cityhero',
+    version='0.0.9',
+    author='WildboarG',
+    author_email='mm62633482@gmail.com',
+    url='https://github.com/WildboarG/City_Hero-sign',
+    license="MIT",
+    description='Based on the script of the Erqi City Hero Infinite City sign in and collect coins',
+    long_description_content_type="text/markdown",
+    long_description = "test Module",
+    packages=["cityhero"],
+    install_requires=["requests","rich"],
+    keywords=["sign", "City_Hero"],
+    python_requires=">=3.6"
+    )
