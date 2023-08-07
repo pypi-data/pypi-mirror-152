8e2e3e348fa0bad07f6f83ef39b336f4f5739ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:40:00 +0000

    Add package file-tree-ds/1.0.1/file-tree-ds-1.0.1.tar.gz

---
+++ packages/file-tree-ds/file-tree-ds-1.0.1.tar.gz/file-tree-ds-1.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="file-tree-ds",
+    version="1.0.1",
+    author="Lawrence M",
+    author_email="lm3263658@gmail.com",
+    description="A tool to create directories for data science project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/particlemontecarlo/FileTree",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
