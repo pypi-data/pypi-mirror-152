0960c4a2bbd3266ca2224fee623f8db9bb1982de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:42:46 +0000

    Add package Goordinates/1.0.1/Goordinates-1.0.1.tar.gz

---
+++ packages/Goordinates/Goordinates-1.0.1.tar.gz/Goordinates-1.0.1/README.md
@@ -0,0 +1,9 @@
+# Goordinates
+
+### Goordinates is a Python package that allows you to easily get latitude and longitude information from a Google Map URL.  
+### Usage
+```
+from Goordinates import Gmap
+
+lat, lng = Gmap.get_cordinate(map_url)
+```
---
+++ packages/Goordinates/Goordinates-1.0.1.tar.gz/Goordinates-1.0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Goordinates",
+    version="1.0.1",
+    author="Duoslow",
+    author_email="heniugur@gmail.com",
+    description="Outputs latitude/longitude coordinates from any Google Map URL.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Duoslow/Goordinates",
+    project_urls={
+        "Bug Tracker": "https://github.com/Duoslow/Goordinates/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=['urllib3','requests']
+)
