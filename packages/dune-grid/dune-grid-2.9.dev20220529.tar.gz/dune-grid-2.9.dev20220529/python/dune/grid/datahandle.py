1ab1f4239c6afbd59e92261e8e1febe703bf41ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:05:10 +0000

    Add package dune-grid/2.9.dev20220529/dune-grid-2.9.dev20220529.tar.gz

---
+++ packages/dune-grid/dune-grid-2.9.dev20220529.tar.gz/dune-grid-2.9.dev20220529/python/dune/grid/datahandle.py
@@ -0,0 +1,15 @@
+from dune.generator.generator import SimpleGenerator
+
+from dune.common.hashit import hashIt
+
+generator = SimpleGenerator("DataHandle", "Dune::Python")
+
+def load(includes, typeName, *args):
+    includes = includes + ["dune/python/common/numpycommdatahandle.hh"]
+    moduleName = "numpycommdatahandle_" + hashIt(typeName)
+    return generator.load(includes, typeName, moduleName, *args)
+
+def dataHandle(mapper,array,function):
+    typeName = "Dune::Python::NumPyCommDataHandle< " + mapper.cppTypeName + ", double, std::function<double(double,double)> >"
+    includes = mapper.cppIncludes
+    return load(includes, typeName).DataHandle(mapper,array,function)
