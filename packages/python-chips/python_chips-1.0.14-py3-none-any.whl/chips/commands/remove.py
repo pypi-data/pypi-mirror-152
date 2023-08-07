9c68f1d5a919a9c9bd4ba15ef0774190c272c611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:05 +0000

    Add package python-chips/1.0.14/python_chips-1.0.14-py3-none-any.whl

---
+++ packages/python-chips/python_chips-1.0.14-py3-none-any.whl/chips/commands/remove.py
@@ -0,0 +1,20 @@
+from ..services.main import ChipService
+from ..common.messages_service import MessagesService
+
+
+def remove(path: str = None, result_type: str = "count"):
+    service = ChipService()
+    messages = MessagesService()
+    chipping_result, chipped_failure_files_list, chipped_skipped_files_list = \
+        service.remove_chips(result_type=result_type, path=path)
+    if chipping_result:
+
+        # list skipped files
+        if chipped_skipped_files_list:
+            messages.default("Removing chips skipped for next files: %s" % ", ".join(chipped_skipped_files_list))
+
+        # list failed files
+        if chipped_failure_files_list:
+            messages.warning("Removing chips failed for next files: %s" % ", ".join(chipped_failure_files_list))
+
+        messages.result(chipping_result)
