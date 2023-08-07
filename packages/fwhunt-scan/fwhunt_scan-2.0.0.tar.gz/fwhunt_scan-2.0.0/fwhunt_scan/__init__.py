febf4023023e5402ae35bac8e414d3154a5c374e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:08:45 +0000

    Add package fwhunt-scan/2.0.0/fwhunt_scan-2.0.0.tar.gz

---
+++ packages/fwhunt-scan/fwhunt_scan-2.0.0.tar.gz/fwhunt_scan-2.0.0/fwhunt_scan/__init__.py
@@ -0,0 +1,21 @@
+# SPDX-License-Identifier: GPL-3.0+
+
+"""
+Tools for analyzing UEFI firmware and checking UEFI modules with FwHunt rules
+"""
+
+__author__ = "FwHunt team"
+__email__ = "fwhunt@binarly.io"
+__version__ = "2.0.0"
+
+from .uefi_analyzer import UefiAnalyzer, UefiAnalyzerError
+from .uefi_scanner import UefiRule, UefiScanner
+from .uefi_te import TerseExecutableParser
+
+__all__ = [
+    "UefiAnalyzer",
+    "UefiRule",
+    "UefiScanner",
+    "TerseExecutableParser",
+    "UefiAnalyzerError",
+]
