13108cc8dc8e06e02911e36b0faa49849146f543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:53:45 +0000

    Add package tsc-auto/0.16/tsc-auto-0.16.tar.gz

---
+++ packages/tsc-auto/tsc-auto-0.16.tar.gz/tsc-auto-0.16/tsc_auto/__init__.py
@@ -0,0 +1,9 @@
+'''
+### 暂时无用的用于超算的文件
+- info.py
+- query.py
+- srun.sh
+'''
+from .set_gpu import set_gpu
+from .kill import get_nvidia_processes, get_ps_processes, get_user_processes, kill_processes
+from .notice import send_wechat, get_time_diff
