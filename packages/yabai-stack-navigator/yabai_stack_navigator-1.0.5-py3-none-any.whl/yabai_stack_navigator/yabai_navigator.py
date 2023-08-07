78ec01dd8fc4848db8b331dea9c165aa2842fb9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:05:07 +0000

    Add package yabai-stack-navigator/1.0.5/yabai_stack_navigator-1.0.5-py3-none-any.whl

---
+++ packages/yabai-stack-navigator/yabai_stack_navigator-1.0.5-py3-none-any.whl/yabai_stack_navigator/yabai_navigator.py
@@ -0,0 +1,25 @@
+import os
+import logging
+from yabai_stack_navigator.yabai_provider import YabaiProvider
+
+
+class YabaiNavigator:
+    def __init__(self, yabai_provider=YabaiProvider()):
+        self.yabai_provider = yabai_provider
+
+    def focus_on_stacked_window(self, window_id):
+        logging.debug("Focusing on stacked window")
+        args = ["-m", "window", "--focus", str(window_id)]
+        return self.yabai_provider.call_yabai(args)
+
+    def focus_on_window(self, next=True):
+        if next:
+            logging.debug("Focusing on next window")
+            os.system(
+                "yabai -m window --focus stack.next || yabai -m window --focus next || yabai -m window --focus first"
+            )
+        else:
+            logging.debug("Focusing on previous window")
+            os.system(
+                "yabai -m window --focus stack.prev || yabai -m window --focus prev || yabai -m window --focus last"
+            )
