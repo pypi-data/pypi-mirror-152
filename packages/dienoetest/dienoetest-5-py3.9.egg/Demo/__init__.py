14e793572b11fa7e5a632dd93c6935b513299746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:07:30 +0000

    Add package dienoetest/5/dienoetest-5-py3.9.egg

---
+++ packages/dienoetest/dienoetest-5-py3.9.egg/Demo/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+from demo_test import *
+def main():
+    print('main')
+def test():
+    print('test')
+def dienoe():
+    print("dienoe")
+demo_print()
---
+++ packages/dienoetest/dienoetest-5-py3.9.egg/Demo/demo_test.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+def demo_print():
+    print("demo_test")
