d3373e44c7c6b9032a575a53b5a16ec638e491e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:05:30 +0000

    Add package quake3rcon/1.0.0/quake3rcon-1.0.0.tar.gz

---
+++ packages/quake3rcon/quake3rcon-1.0.0.tar.gz/quake3rcon-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='quake3rcon',
+    version='1.0.0',
+    url='https://github.com/hellyet/quake3rcon',
+    license='MIT',
+    author='hellyet',
+    author_email='leonid.tosterovski@gmail.com',
+    description='A tiny library for using Quake 3\'s RCON protocol feature for some game servers like FiveM.',
+    packages=find_packages(exclude=['tests']),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    zip_safe=False
+)
